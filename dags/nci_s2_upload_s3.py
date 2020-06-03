3eb62ed0c410e004fb435e896ac728d0fd99f0bd
Sachit Rajbhandari <sachit.rajbhandari@ga.gov.au>
2020-06-03 14:07:11 +1000

    Implemented data mover

--- dags/nci_s2_upload_s3.py
+++ dags/nci_s2_upload_s3.py
@@ -9,7 +9,7 @@ data from NCI to AWS S3 bucket. It:
  * Executes uploaded rolling script to upload `Sentinel-2` data to AWS `S3` bucket. The upload takes
  place for yesterday of the DAG's execution date. E.g. For execution date `"2020-05-15"`, the
  upload process will take place for `"2020-05-14"` (a day before execution date).
- * Checks logs for any errors during upload.
+ * Cleans working folder at `NCI` after upload completion.
 
 This DAG takes following input parameters from `"nci_s2_upload_s3_config"` variable:
 
@@ -46,21 +46,16 @@ This DAG takes following input parameters from `"nci_s2_upload_s3_config"` varia
         }
     }
 """
-from base64 import b64decode
 from datetime import datetime, timedelta
 from textwrap import dedent
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 
-from airflow import DAG, configuration, AirflowException
+from airflow import DAG, configuration
 from airflow.models import Variable
 from airflow.contrib.hooks.aws_hook import AwsHook
-from airflow.contrib.hooks.sftp_hook import SFTPHook
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.contrib.operators.sftp_operator import SFTPOperator, SFTPOperation
-from airflow.operators.python_operator import PythonOperator
 
-from sensors.pbs_job_complete_sensor import PBSJobSensor
 
 # Read and load variable into dict
 VARIABLE_NAME = "nci_s2_upload_s3_config"
@@ -83,32 +78,6 @@ def get_parameter_value_from_variable(_dag_config, parameter_name, default_value
         raise Exception("Missing necessary parameter '{}' in "
                         "variable '{}'".format(parameter_name, VARIABLE_NAME))
     return parameter_value
-
-
-def _check_log(log_path, pbs_id, conn_id):
-    """
-    Check and raise exception if errors exist in log file
-    :param log_path: Absolute path to log file
-    :param pbs_id: PBS ID for the queued task
-    :param conn_id: sftp connection id
-    """
-    # The SSHOperator will base64 encode output stored in XCOM, if pickling of xcom vals is disabled
-    pbs_id = b64decode(pbs_id).decode('utf8').strip()
-    remote_file = Path(log_path).joinpath(pbs_id+".ER").as_posix()
-    with NamedTemporaryFile() as local_file:
-        sftp_hook = SFTPHook(ftp_conn_id=conn_id)
-        sftp_hook.retrieve_file(remote_file, local_file.name)
-
-        # List of search keyword(s) in log file. Currently checking only log level "ERROR"
-        log_error_text = '- s3_to_s3_rolling - ERROR -'
-        with open(local_file.name, "r") as file:
-            raise_exception = False
-            for line in file:
-                if log_error_text in line:
-                    raise_exception = True
-                    print(line.rstrip("\n"))
-            if raise_exception:
-                raise AirflowException("Error found in Sentinel-2 data upload to AWS S3 bucket")
 
 
 default_args = {
@@ -174,33 +143,8 @@ with dag:
         command=dedent("""
             cd {{ ti.xcom_pull(key='work_dir') }}
 
-            qsub -N s2nbar-rolling-archive \
-            -o "{{ ti.xcom_pull(key='work_dir') }}" \
-            -e "{{ ti.xcom_pull(key='work_dir') }}" <<EOF
-            #!/bin/bash
-
-            # Set up PBS job variables
-            #PBS -P v10
-            #PBS -q copyq
-
-            #PBS -l wd
-
-            ## Resource limits
-            #PBS -l mem=1GB
-            #PBS -l ncpus=1
-            #PBS -l walltime=10:00:00
-
-            ## The requested job scratch space.
-            #PBS -l jobfs=1GB
-            #PBS -l storage=gdata/if87+gdata/v10
-            #PBS -m abe -M nci.monitor@dea.ga.gov.au
-
             # echo on and exit on fail
-            set -ex
-
-            # Set up our environment
-            # shellcheck source=/dev/null
-            source "$HOME"/.bashrc
+            set -eux
 
             # Load the latest stable DEA module
             module use /g/data/v10/public/modules/modulefiles
@@ -215,28 +159,20 @@ with dag:
                     -d '{{ yesterday_ds }}' \
                     -b '{{ params.s3bucket }}' \
                     -u '{{ params.doupdate }}'
-
-            EOF
         """),
+        remote_host='gadi-dm.nci.org.au',
         params={'aws_conn': aws_conn.get_credentials()},
-        timeout=60 * 5,
-        do_xcom_push=True
+        timeout=60 * 5
     )
-    # Wait and tell PBS jobs completed
-    wait_for_script_execution = PBSJobSensor(
-        task_id='wait_for_script_execution',
-        pbs_job_id="{{ ti.xcom_pull(task_ids='execute_s2_to_s3_script') }}",
-        timeout=60 * 60 * 24 * 7,
+    # Deletes working folder and uploaded script file
+    clean_nci_work_dir = SSHOperator(
+        task_id='clean_nci_work_dir',
+        # Remove work dir after aws s3 sync
+        command=dedent("""
+            set -eux
+            rm -vrf "{{ ti.xcom_pull(key='work_dir') }}"
+        """)
     )
-    # Check for error in PBS job logs
-    check_log = PythonOperator(
-        task_id='check_log',
-        python_callable=_check_log,
-        op_kwargs={
-            "log_path": "{{ ti.xcom_pull(key='work_dir') }}",
-            "pbs_id": "{{ ti.xcom_pull(task_ids='execute_s2_to_s3_script') }}",
-            "conn_id": "{{ dag.default_args.ssh_conn_id }}"
-        },
-    )
-    create_nci_work_dir >> sftp_s2_to_s3_script >> execute_s2_to_s3_script
-    execute_s2_to_s3_script >> wait_for_script_execution >> check_log
+    create_nci_work_dir >> sftp_s2_to_s3_script
+    sftp_s2_to_s3_script >> execute_s2_to_s3_script
+    execute_s2_to_s3_script >> clean_nci_work_dir
