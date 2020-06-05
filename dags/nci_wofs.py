825dcb1ceeefc2c6686d155410277893f996bd1b
Damien Ayers <damien@omad.net>
2020-06-05 15:22:40 +1000

    Bump NCI WOfS module

--- dags/nci_wofs.py
+++ dags/nci_wofs.py
@@ -18,7 +18,7 @@ default_args = {
     'params': {
         'project': 'v10',
         'queue': 'normal',
-        'module': 'dea/20200604',
+        'module': 'dea/20200605',
         'year': '2020'
     }
 }
