068b32638b559498e726f49efc346d5dddea83e1
Sachit Rajbhandari <sachit.rajbhandari@ga.gov.au>
2020-06-10 15:40:09 +1000

    Added Airflow Variable to configure DAG

--- dags/k8s_index_s2_s3.py
+++ dags/k8s_index_s2_s3.py
@@ -3,37 +3,79 @@
 
 DAG to run indexing for Sentinel-2 data on S3 into AWS DB.
 
-This DAG uses k8s executors and pre-existing pods in cluster with relevant tooling
-and configuration installed.
+This DAG takes following input parameters from `"k8s_index_s2_s3_config"` variable:
 
-The DAG has to be parameterized with index date as below.
+ * `start_date`: Start date for DAG run.
+ * `end_date`: End date for DAG run.
+ * `catchup`: Set to "True" for back fill else "False".
+ * `schedule_interval`: Set "" for no schedule else provide schedule cron or preset (@once, @daily).
+ * `db_hostname`: Provide DB Host Name.
+ * `db_database`: Provide DB Name.
+ * `aws_iam_role`: Provide AWS IAM ROLE.
+ * `s3bucket`: Name of the S3 bucket.
+
+**EXAMPLE** *- Variable in JSON format with key name "k8s_index_s2_s3_config":*
 
     {
-        "index_date": "2020-05-01"
+        "k8s_index_s2_s3_config":
+        {
+            "start_date": "2020-4-1",
+            "end_date": "2020-4-30",
+            "catchup": "False",
+            "schedule_interval" : "@daily",
+            "db_hostname": "database-write.local",
+            "db_database": "ows-index",
+            "aws_iam_role": "svc-dea-dev-eks-processing-index",
+            "s3bucket": "dea-public-data-dev"
+        }
     }
 """
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import Variable
 from airflow.contrib.kubernetes.secret import Secret
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+
+# Read and load variable into dict
+VARIABLE_NAME = "k8s_index_s2_s3_config"
+dag_config = Variable.get(VARIABLE_NAME, deserialize_json=True)
+
+
+def get_parameter_value_from_variable(_dag_config, parameter_name, default_value=''):
+    """
+    Return parameter value from variable
+    :param _dag_config: Name of the variable
+    :param parameter_name: Parameter name
+    :param default_value: Default value if parameter is empty
+    :return: Parameter value for provided parameter name
+    """
+    if parameter_name in _dag_config and _dag_config[parameter_name]:
+        parameter_value = _dag_config[parameter_name]
+    elif default_value != '':
+        parameter_value = default_value
+    else:
+        raise Exception("Missing necessary parameter '{}' in "
+                        "variable '{}'".format(parameter_name, VARIABLE_NAME))
+    return parameter_value
 
 
 DEFAULT_ARGS = {
     "owner": "Sachit Rajbhandari",
     "depends_on_past": False,
-    "start_date": datetime(2020, 2, 21),
+    "start_date": get_parameter_value_from_variable(dag_config, 'start_date', datetime.today()),
+    "end_date": get_parameter_value_from_variable(dag_config, 'end_date', None),
     "email": ["sachit.rajbhandari@ga.gov.au"],
-    "email_on_failure": False,
+    "email_on_failure": True,
     "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=5),
     "env_vars": {
         "AWS_DEFAULT_REGION": "ap-southeast-2",
-        # TODO: Pass these via templated params in DAG Run
-        "DB_HOSTNAME": "database-write.local",
-        "DB_DATABASE": "ows-index",
+        "DB_HOSTNAME": get_parameter_value_from_variable(dag_config, 'db_hostname'),
+        "DB_DATABASE": get_parameter_value_from_variable(dag_config, 'db_database'),
     },
     # Use K8S secrets to send DB Creds
     # Lift secrets into environment variables for datacube
@@ -41,8 +83,11 @@ DEFAULT_ARGS = {
         Secret("env", "DB_USERNAME", "ows-db", "postgres-username"),
         Secret("env", "DB_PASSWORD", "ows-db", "postgres-password"),
     ],
+    "params": {
+        "s3bucket": get_parameter_value_from_variable(dag_config, 's3bucket'),
+        "aws_iam_role": get_parameter_value_from_variable(dag_config, 'aws_iam_role'),
+    }
 }
-
 
 INDEXER_IMAGE = "opendatacube/datacube-index:0.0.5"
 EXPLORER_IMAGE = "opendatacube/dashboard:2.1.6"
@@ -51,14 +96,13 @@ dag = DAG(
     "k8s_index_s2_s3",
     doc_md=__doc__,
     default_args=DEFAULT_ARGS,
-    schedule_interval=None,
-    catchup=False,
+    catchup=get_parameter_value_from_variable(dag_config, 'catchup', False),
+    schedule_interval=get_parameter_value_from_variable(dag_config, 'schedule_interval', None),
     tags=['k8s', 'sentinel_2']
 )
 
 
 with dag:
-    START = DummyOperator(task_id="s3_index_publish")
 
     # TODO: Bootstrap if targeting a Blank DB
     # TODO: Initialize Datacube
@@ -94,10 +138,11 @@ with dag:
         cmds=["s3-to-dc"],
         # Assume kube2iam role via annotations
         # TODO: Pass this via DAG parameters
-        annotations={"iam.amazonaws.com/role": "svc-dea-dev-eks-processing-index"},
+        annotations={"iam.amazonaws.com/role": "{{ params.aws_iam_role }}"},
         # TODO: Collect form JSON used to trigger DAG
         arguments=[
-            "s3://dea-public-data-dev/L2/sentinel-2-nbar/S2MSIARD_NBAR/{{ dag_run.conf.index_date }}/*/*.yaml",
+            "s3://{{ params.s3bucket }}"
+            "/L2/sentinel-2-nbar/S2MSIARD_NBAR/{{ yesterday_ds }}/*/*.yaml",
             "ga_s2a_ard_nbar_granule",
             "ga_s2b_ard_nbar_granule",
         ],
@@ -107,9 +152,5 @@ with dag:
         get_logs=True,
     )
 
-    COMPLETE = DummyOperator(task_id="all_done")
-
-    START >> BOOTSTRAP
     BOOTSTRAP >> ADD_PRODUCT
     ADD_PRODUCT >> INDEXING
-    INDEXING >> COMPLETE
