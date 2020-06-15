77ed37a24b44d34baadf2569c84af173271c6cb6
Sachit Rajbhandari <sachit.rajbhandari@ga.gov.au>
2020-06-16 06:56:13 +1000

    Removed create dir task and XCom usage

--- dags/nci_s2_upload_s3.py
+++ dags/nci_s2_upload_s3.py
@@ -4,30 +4,21 @@
 This DAG runs tasks on Gadi at the NCI. This DAG routinely sync Sentinel-2
 data from NCI to AWS S3 bucket. It:
 
- * Creates necessary working folder at `NCI`.
  * Uploads `Sentinel-2 to S3 rolling` script to NCI work folder.
  * Executes uploaded rolling script to upload `Sentinel-2` data to AWS `S3` bucket.
  * Cleans working folder at `NCI` after upload completion.
 
-This DAG takes following input parameters:
+This DAG takes following configuration:
 
- * `start_date`: Start date for DAG run `datetime(2019, 12, 6)`.
+ * `start_date`: Start date for DAG run `datetime(2020, 12, 6)`.
  * `end_date`: End date for DAG run `datetime(2020, 5, 31)`.
- * `catchup`: Set to `[True]` for back fill else `[False]`.
- * `schedule_interval`: Set `None` for no schedule else provide schedule cron or preset `"@daily"`.
+ * `catchup`: Set to `[True]` for back fill.
+ * `schedule_interval`: Set `@daily` to schedule upload daily.
  * `ssh_conn_id`: Provide SSH Connection ID `"lpgs_gadi"`.
  * `aws_conn_id`: Provide AWS Conection ID `"dea_public_data_upload"`.
  * `s3bucket`: Name of the S3 bucket. `"dea-public-data"`
- * `numdays`: Number of days to process before the execution date. `"0"`
- * `doupdate`: Select update option as below to replace granules and metadata.
-               If update option is not provided, granules and metadata are not
-               synced if they already exists in S3 bucket.
-    * `'granule_metadata'` to update granules and metadata;
-    * `'granule' to update'` granules without metadata;
-    * `'metadata'` to update only metadata;
-    * `'no'` or don't set to avoid update.
-
-
+ * `numdays`: Number of days to process before the execution date selected is `"0"`
+ * `doupdate`: Selected `'no'` to avoid update of existing granules/metadata .
 """
 from datetime import datetime, timedelta
 from textwrap import dedent
@@ -41,16 +32,16 @@ from airflow.contrib.operators.sftp_operator import SFTPOperator, SFTPOperation
 
 default_args = {
     'owner': 'Sachit Rajbhandari',
-    'start_date': datetime(2019, 12, 6),
+    'start_date': datetime(2020, 4, 21),
     'end_date': datetime(2020, 5, 31),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     'email_on_failure': True,
     'email': 'sachit.rajbhandari@ga.gov.au',
     'ssh_conn_id': 'lpgs_gadi',
-    'aws_conn_id': 'dea_public_data_upload',
+    'aws_conn_id': 'dea_public_data_dev_upload',
     'params': {
-        's3bucket': 'dea-public-data',
+        's3bucket': 'dea-public-data-dev',
         'numdays': '0',
         'doupdate': 'no'
     }
@@ -70,28 +61,18 @@ dag = DAG(
 
 
 with dag:
-    # Creating working directory
-    # '/g/data/v10/work/s2_nbar_rolling_archive/<current_datetime>_<end_date>_<num_days>'
+    WORK_DIR = "/g/data/v10/work/s2_nbar_rolling_archive/{{ ds }}_{{ params.numdays }}"
     COMMON = """
             {% set work_dir = '/g/data/v10/work/s2_nbar_rolling_archive/' 
-            + execution_date.strftime('%FT%H%M') + '_'+ ds +'_' + params.numdays  -%}
+            + ds +'_' + params.numdays -%}
             """
-    create_nci_work_dir = SSHOperator(
-        task_id='create_nci_work_dir',
-        command=dedent(COMMON + """
-            set -eux
-            mkdir -p {{work_dir}}
-            echo "{{ ti.xcom_push(key="work_dir", value=work_dir) }}"
-        """),
-        do_xcom_push=True
-    )
     # Uploading s2_to_s3_rolling.py script to NCI
     sftp_s2_to_s3_script = SFTPOperator(
         task_id="sftp_s2_to_s3_script",
         local_filepath=Path(
             Path(configuration.get('core', 'dags_folder')).parent
         ).joinpath("scripts/s2_to_s3_rolling.py").as_posix(),
-        remote_filepath="{{ti.xcom_pull(key='work_dir') }}/s2_to_s3_rolling.py",
+        remote_filepath="{}/s2_to_s3_rolling.py".format(WORK_DIR),
         operation=SFTPOperation.PUT,
         create_intermediate_dirs=True
     )
@@ -99,8 +80,8 @@ with dag:
     aws_conn = AwsHook(aws_conn_id=dag.default_args['aws_conn_id'])
     execute_s2_to_s3_script = SSHOperator(
         task_id='execute_s2_to_s3_script',
-        command=dedent("""
-            cd {{ ti.xcom_pull(key='work_dir') }}
+        command=dedent(COMMON + """
+            cd {{ work_dir }}
 
             # echo on and exit on fail
             set -eux
@@ -113,7 +94,7 @@ with dag:
             export AWS_ACCESS_KEY_ID={{params.aws_conn.access_key}}
             export AWS_SECRET_ACCESS_KEY={{params.aws_conn.secret_key}}
 
-            python3 '{{ ti.xcom_pull(key='work_dir') }}/s2_to_s3_rolling.py' \
+            python3 '{{ work_dir }}/s2_to_s3_rolling.py' \
                     -n '{{ params.numdays }}' \
                     -d '{{ ds }}' \
                     -b '{{ params.s3bucket }}' \
@@ -127,11 +108,10 @@ with dag:
     clean_nci_work_dir = SSHOperator(
         task_id='clean_nci_work_dir',
         # Remove work dir after aws s3 sync
-        command=dedent("""
+        command=dedent(COMMON + """
             set -eux
-            rm -vrf "{{ ti.xcom_pull(key='work_dir') }}"
+            rm -vrf "{{ work_dir }}"
         """)
     )
-    create_nci_work_dir >> sftp_s2_to_s3_script
     sftp_s2_to_s3_script >> execute_s2_to_s3_script
     execute_s2_to_s3_script >> clean_nci_work_dir
