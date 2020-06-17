3652c7cf23d2b3057337e4c4a349d4bd0ba687c6
Damien Ayers <damien@omad.net>
2020-06-17 15:28:06 +1000

    Fix linting errors and run black formatter

--- dags/check_qstat_nci.py
+++ dags/check_qstat_nci.py
@@ -8,51 +8,46 @@ from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
 
 default_args = {
-    'depends_on_past': False,
-    'start_date': datetime(2020, 2, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "depends_on_past": False,
+    "start_date": datetime(2020, 2, 1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 
 def ensure_table(postgres_conn_id, database):
-    ssh_hook = SSHHook(ssh_conn_id='lpgs_gadi')
-    ssh_hook.get_tunnel().start()
-    hook = PostgresHook(postgres_conn_id=postgres_conn_id,
-                        schema=database)
-    hook.run(
-        """CREATE TABLE ..."""
+    ssh_hook = SSHHook(ssh_conn_id="lpgs_gadi")
+
+
+dag = DAG(
+    "check_qstat_nci",
+    default_args=default_args,
+    catchup=False,
+    schedule_interval=timedelta(days=1),
+)
+
+with dag:
+    ensure_database_table = PythonOperator(
+        task_id="ensure_database_table", python_callable=ensure_table
     )
 
 
-dag = DAG('check_qstat_nci',
-          default_args=default_args,
-          catchup=False,
-          schedule_interval=timedelta(days=1))
-
-ensure_database_table = PythonOperator(
-    task_id='ensure_database_table',
-    python_callable=ensure_table)
+    get_qstat_output = SSHOperator(
+        task_id="get_qstat_output",
+        ssh_conn_id="lpgs_gadi",
+        command="qstat -xf -F json",
+        do_xcom_push=True,
+    )
 
 
-get_qstat_output = SSHOperator(task_id='get_qstat_output',
-                               ssh_conn_id='lpgs_gadi',
-                               command='qstat -xf -F json',
-                               do_xcom_push=True,
-                               dag=dag)
-
-
-# t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag)
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
 get_qstat_output >> t1
