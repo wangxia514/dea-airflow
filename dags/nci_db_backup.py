ddf5c6848f094076de5f95a22dec79000d95d8a2
Damien Ayers <damien@omad.net>
2020-07-07 06:49:52 +1000

    Be firm when creating temp dirs

--- dags/nci_db_backup.py
+++ dags/nci_db_backup.py
@@ -105,7 +105,7 @@ with DAG('nci_db_backup',
             )
 
             output_dir=$TMPDIR/pg_csvs_{{ ds_nodash }}
-            mkdir ${output_dir}
+            mkdir -p ${output_dir}
             cd ${output_dir}
 
             for table in ${tables[@]}; do
