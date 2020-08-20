e759ebdf46e28297d3839d339a723360cfe07255
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-08-20 01:58:06 +0000

    Use V1VolumeMount

--- dags/k8s_s3_orchestrate_pin.py
+++ dags/k8s_s3_orchestrate_pin.py
@@ -58,10 +58,10 @@ OWS_CONFIG_IMAGE = "geoscienceaustralia/dea-datakube-config:1.5.1"
 OWS_CFG_PATH = "/env/config/ows_cfg.py"
 OWS_CFG_IMAGEPATH = "/opt/dea-config/dev/services/wms/ows/ows_cfg.py"
 
-cfg_image_mount = VolumeMount('ows-config-image',
-                        mount_path='/opt',
-                        sub_path=None,
-                        read_only=True)
+cfg_image_mount = k8s.V1VolumeMount('ows-config-image',
+                                    mount_path='/opt',
+                                    sub_path=None,
+                                    read_only=True)
 
 ows_cfg_mount = VolumeMount('ows-config-volume',
                         mount_path='/env/config',
@@ -81,7 +81,7 @@ config_container = k8s.V1Container(
         image=OWS_CONFIG_IMAGE,
         command=["cp"],
         args=["-f", OWS_CFG_IMAGEPATH, OWS_CFG_PATH],
-        # volume_mounts=[cfg_image_mount, ows_cfg_mount],
+        volume_mounts=[cfg_image_mount],
         name="mount-ows-config",
         working_dir="/opt"
     )
