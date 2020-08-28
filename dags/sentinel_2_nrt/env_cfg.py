20b7eab3bbd52f81d539c312814b5d3d21706103
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-08-28 02:13:48 +0000

    Test project folder based dag

---
+++ dags/sentinel_2_nrt/env_cfg.py
@@ -0,0 +1,16 @@
+# ENVIRONMENT CONFIGURATION
+OWS_CFG_PATH = "/env/config/ows_cfg.py"
+INDEXING_PRODUCTS = "s2a_nrt_granule s2b_nrt_granule"
+ARCHIVE_PRODUCTS = INDEXING_PRODUCTS
+ARCHIVE_CONDITION = "[$(date -d '-365 day' +%F), $(date -d '-91 day' +%F)]"
+UPDATE_EXTENT_PRODUCTS = "s2_nrt_granule_nbar_t"
+SQS_QUEUE_NAME = "dea-dev-eks-ows-dag"
+INDEXING_ROLE = "dea-dev-eks-orchestration"
+SECRET_AWS_NAME = "processing-aws-creds-dev"
+SECRET_EXPLORER_NAME = "explorer-db"
+SECRET_OWS_NAME = "ows-db"
+DB_DATABASE = "ows"
+
+
+OWS_CFG_MOUNT_PATH = "/env/config"
+OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
