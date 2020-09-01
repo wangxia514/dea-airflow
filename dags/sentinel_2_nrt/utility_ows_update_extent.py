0e97e1822f1ae16853595444cd9f63f87a57e239
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-01 00:23:43 +0000

    add ows_update_extent utility

---
+++ dags/sentinel_2_nrt/utility_ows_update_extent.py
@@ -0,0 +1,62 @@
+"""
+# Explorer cubedash-gen refresh-stats
+"""
+
+from airflow import DAG
+from textwrap import dedent
+from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from airflow.operators.subdag_operator import SubDagOperator
+from sentinel_2_nrt.subdag_ows_views import ows_update_extent_subdag
+
+from sentinel_2_nrt.env_cfg import DB_DATABASE, SECRET_EXPLORER_NAME, SECRET_AWS_NAME
+
+DAG_NAME = "utility_ows-update-extent"
+
+# DAG CONFIGURATION
+DEFAULT_ARGS = {
+    "owner": "Pin Jin",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 6, 14),
+    "email": ["pin.jin@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "env_vars": {
+        # TODO: Pass these via templated params in DAG Run
+        "DB_HOSTNAME": "db-writer",
+        "DB_DATABASE": DB_DATABASE,
+    },
+    # Lift secrets into environment variables
+    "secrets": [
+        Secret("env", "AWS_DEFAULT_REGION", SECRET_AWS_NAME, "AWS_DEFAULT_REGION"),
+    ],
+}
+
+
+# THE DAG
+dag = DAG(
+    dag_id=DAG_NAME,
+    doc_md=__doc__,
+    default_args=DEFAULT_ARGS,
+    schedule_interval="0 */1 * * *",
+    catchup=False,
+    tags=["k8s", "ows"],
+)
+
+with dag:
+    OWS_UPDATE_EXTENTS = SubDagOperator(
+        task_id="run-ows-update-ranges",
+        subdag=ows_update_extent_subdag(DAG_NAME, "run-ows-update-ranges", DEFAULT_ARGS),
+    )
+
+    START = DummyOperator(task_id="start_ows_update_ranges")
+
+    COMPLETE = DummyOperator(task_id="all_done")
+
+    START >> OWS_UPDATE_EXTENTS
+    OWS_UPDATE_EXTENTS >> COMPLETE
