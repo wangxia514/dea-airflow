456b07e3098394bc4739992dcd7b8c6ccdf27912
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-01 07:17:15 +0000

    Testing cmd;

--- dags/sentinel_2_nrt/test_subdag.py
+++ dags/sentinel_2_nrt/test_subdag.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from textwrap import dedent
 
 
-def subdag_test(parent_dag_name, child_dag_name, args, refresh_products):
-
+def subdag_test(parent_dag_name, child_dag_name, args):
 
     dag_subdag = DAG(
         dag_id="%s.%s" % (parent_dag_name, child_dag_name),
@@ -11,6 +11,19 @@ def subdag_test(parent_dag_name, child_dag_name, args, refresh_products):
         catchup=False,
     )
 
+    bash_cmd = [
+        "bash",
+        "-c",
+        dedent(
+            """
+            for product in %s; do
+                echo $product;
+            done;
+        """
+        )
+        % ("{{ task_instance.xcom_pull(dag_id='utility_explorer-refresh-stats', task_ids='parse_dagrun_conf') }}"),
+    ]
+
     # BashOperator(task_id='t2', bash_command="echo product is set to: %s" %(refresh_products), dag=dag_subdag)
-    BashOperator(task_id='t3', bash_command='echo {{ task_instance.xcom_pull(dag_id="utility_explorer-refresh-stats", task_ids="parse_dagrun_conf") }}', dag=dag_subdag)
+    BashOperator(task_id='t3', bash_command=bash_cmd, dag=dag_subdag)
     return dag_subdag
