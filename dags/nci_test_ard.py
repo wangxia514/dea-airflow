bf6b02303e21219734bdeb3c175641f74c7c51ba
Duncan Gray <duncan.gray@ga.gov.au>
2020-09-01 16:43:12 +1000

    change nci_test_ard to be like nci_ard so airflow dev is clean

--- dags/nci_test_ard.py
+++ dags/nci_test_ard.py
@@ -14,31 +14,6 @@ from airflow.operators.dummy_operator import DummyOperator
 
 from sensors.pbs_job_complete_sensor import PBSJobSensor
 
-# swap around set work_dir log_dir too
-#     params = {
-#         "project": "v10",
-#         "queue": "normal",
-#         "module_ass": "ard-scene-select-py3-dea/20200831",
-#         "index_arg": "--index-datacube-env "
-#         "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env",
-#         "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
-#         "config_arg": "",
-#         "scene_limit": "",
-#         "products_arg": "",
-#         "pkgdir_arg": "/g/data/xu18/ga",
-#     }
-params = {
-    "project": "u46",
-    "queue": "normal",
-    "module_ass": "ard-scene-select-py3-dea/20200831",
-    "index_arg": "--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/index-test-odc.env",
-    # "index_arg": "",  # no indexing
-    "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
-    "config_arg": "--config /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/dsg547_dev.conf",
-    "scene_limit": "--scene-limit 1",
-    "products_arg": """--products '["usgs_ls8c_level1_1"]'""",
-    "pkgdir_arg": "/g/data/v10/Landsat-Collection-3-ops/scene_select_test/",
-}
 default_args = {
     "owner": "Duncan Gray",
     "depends_on_past": False,  # Very important, will cause a single failure to propagate forever
@@ -47,7 +22,19 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
     "ssh_conn_id": "lpgs_gadi",
     # "ssh_conn_id': 'dsg547",
-    "params": params,
+    "params": {
+        "project": "v10",
+        "queue": "normal",
+        "module_ass": "ard-scene-select-py3-dea/20200831",
+        # "index_arg": "--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env",
+        "index_arg": "--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/index-test-odc.env",
+        # "index_arg": "",  # no indexing
+        "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
+        "config_arg": "--config /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/dsg547_dev.conf",
+        "products_arg": """--products '["usgs_ls8c_level1_1"]'""",
+        # "pkgdir_arg": "/g/data/xu18/ga"
+        "pkgdir_arg": "/g/data/v10/Landsat-Collection-3-ops/scene_select_test/",
+    },
 }
 
 # tags is in airflow >1.10.8
@@ -68,10 +55,12 @@ with dag:
 
     COMMON = """
         #  ts_nodash timestamp no dashes.
-        {% set log_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/logdir' %}
-        {% set work_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/workdir' %}
         {% set log_dir = '/g/data/v10/work/c3_ard/' + ts_nodash + '/logdir' %}
         {% set work_dir = '/g/data/v10/work/c3_ard/' + ts_nodash + '/workdir' %}
+        {% set log_dir = '/g/data/u46/users/dsg547/results_airflow/' + ts_nodash + '/logdir' %}
+        {% set work_dir = '/g/data/u46/users/dsg547/results_airflow/' + ts_nodash + '/workdir' %}
+        {% set log_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/logdir' %}
+        {% set work_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/workdir' %}
         """
 
     # An example of remotely starting a qsub job (all it does is ls)
@@ -102,7 +91,7 @@ with dag:
                   --project {{ params.project }} \
                   --walltime 02:30:00 \
                   {{ params.index_arg }} \
-                  {{ params.scene_limit }}\
+                  --scene-limit 1\
                   --run-ard "
         """,
         timeout=60 * 20,
