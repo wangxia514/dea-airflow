682bb36ab7614397bd8938b2b636ee6b164e706b
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-01 23:56:47 +0000

    Add sentinel_2_nrt indexing package

---
+++ dags/sentinel_2_nrt/sentinel_2_nrt_archive.py
@@ -0,0 +1,118 @@
+"""
+# Sentinel-2_nrt Archiving automation
+
+DAG to periodically archive Sentinel-2 NRT data.
+
+This DAG uses k8s executors and in cluster with relevant tooling
+and configuration installed.
+"""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.kubernetes.secret import Secret
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+from textwrap import dedent
+
+import kubernetes.client.models as k8s
+
+from sentinel_2_nrt.images import INDEXER_IMAGE, OWS_IMAGE
+from sentinel_2_nrt.subdag_ows_views import ows_update_extent_subdag
+from sentinel_2_nrt.env_cfg import DB_DATABASE, SECRET_OWS_NAME, SECRET_AWS_NAME
+from airflow.operators.subdag_operator import SubDagOperator
+from sentinel_2_nrt.subdag_explorer_summary import explorer_refresh_stats_subdag
+
+
+ARCHIVE_CONDITION = "[$(date -d '-365 day' +%F), $(date -d '-91 day' +%F)]"
+ARCHIVE_PRODUCTS = "s2a_nrt_granule s2b_nrt_granule"
+
+DAG_NAME = "sentinel_2_nrt_archive"
+
+ARCHIVE_BASH_COMMAND = [
+    "bash",
+    "-c",
+    dedent(
+        """
+        for product in %s; do
+            echo "Archiving product: $product"
+            datacube dataset search -f csv "product=$product time in %s" > /tmp/to_kill.csv;
+            cat /tmp/to_kill.csv | awk -F',' '{print $1}' | sed '1d' > /tmp/to_kill.list;
+            echo "Datasets count to be archived"
+            wc -l /tmp/to_kill.list;
+            cat /tmp/to_kill.list | xargs datacube dataset archive
+        done;
+    """
+    )
+    % (ARCHIVE_PRODUCTS, ARCHIVE_CONDITION),
+]
+
+# DAG CONFIGURATION
+DEFAULT_ARGS = {
+    "owner": "Pin Jin",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 6, 14),
+    "email": ["pin.jin@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "env_vars": {
+        # TODO: Pass these via templated params in DAG Run
+        "DB_HOSTNAME": "db-writer",
+        "DB_DATABASE": DB_DATABASE,
+    },
+    # Lift secrets into environment variables
+    "secrets": [
+        Secret("env", "DB_USERNAME", SECRET_OWS_NAME, "postgres-username"),
+        Secret("env", "DB_PASSWORD", SECRET_OWS_NAME, "postgres-password"),
+        Secret("env", "AWS_DEFAULT_REGION", SECRET_AWS_NAME, "AWS_DEFAULT_REGION"),
+    ],
+}
+
+# THE DAG
+dag = DAG(
+    dag_id=DAG_NAME,
+    doc_md=__doc__,
+    default_args=DEFAULT_ARGS,
+    schedule_interval="0 */1 * * *",
+    catchup=False,
+    tags=["k8s", "sentinel-2", "archive"],
+)
+
+with dag:
+
+    ARCHIVE_EXTRANEOUS_DS = KubernetesPodOperator(
+        namespace="processing",
+        image=INDEXER_IMAGE,
+        arguments=ARCHIVE_BASH_COMMAND,
+        labels={"step": "ds-arch"},
+        name="datacube-dataset-archive",
+        task_id="archive-nrt-datasets",
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
+
+    OWS_UPDATE_EXTENTS = SubDagOperator(
+        task_id="run-ows-update-ranges",
+        subdag=ows_update_extent_subdag(
+            DAG_NAME, "run-ows-update-ranges", DEFAULT_ARGS
+        ),
+    )
+
+    EXPLORER_SUMMARY = SubDagOperator(
+        task_id="run-cubedash-gen-refresh-stat",
+        subdag=explorer_refresh_stats_subdag(
+            DAG_NAME, "run-cubedash-gen-refresh-stat", DEFAULT_ARGS
+        ),
+    )
+
+    START = DummyOperator(task_id="start_sentinel_2_nrt_archive")
+
+    COMPLETE = DummyOperator(task_id="all_done")
+
+    START >> ARCHIVE_EXTRANEOUS_DS
+    ARCHIVE_EXTRANEOUS_DS >> OWS_UPDATE_EXTENTS
+    ARCHIVE_EXTRANEOUS_DS >> EXPLORER_SUMMARY
+    OWS_UPDATE_EXTENTS >> COMPLETE
+    EXPLORER_SUMMARY >> COMPLETE
---
+++ dags/sentinel_2_nrt/utility_ows_update_extent.py
@@ -0,0 +1,97 @@
+"""
+## Utility Tool
+### ows update ranges
+This is utility is to provide administrators the easy accessiblity to run ad-hoc --views and update-ranges
+
+##### commands to run
+    `datacube-ows-update --views`
+    `datacube-ows-update s2_nrt_granule_`
+"""
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from airflow.operators.subdag_operator import SubDagOperator
+from sentinel_2_nrt.subdag_ows_views import ows_update_extent_subdag
+
+from sentinel_2_nrt.env_cfg import (
+    DB_DATABASE,
+    DB_HOSTNAME,
+    SECRET_OWS_NAME,
+    UPDATE_EXTENT_PRODUCTS,
+    SECRET_AWS_NAME,
+)
+
+DAG_NAME = "utility_ows-update-extent"
+
+# DAG CONFIGURATION
+DEFAULT_ARGS = {
+    "owner": "Pin Jin",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 6, 14),
+    "email": ["pin.jin@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "env_vars": {
+        # TODO: Pass these via templated params in DAG Run
+        "DB_HOSTNAME": DB_HOSTNAME,
+        "DB_DATABASE": DB_DATABASE,
+    },
+    # Lift secrets into environment variables
+    "secrets": [
+        Secret("env", "DB_USERNAME", SECRET_OWS_NAME, "postgres-username"),
+        Secret("env", "DB_PASSWORD", SECRET_OWS_NAME, "postgres-password"),
+        Secret("env", "AWS_DEFAULT_REGION", SECRET_AWS_NAME, "AWS_DEFAULT_REGION"),
+    ],
+}
+
+
+# THE DAG
+dag = DAG(
+    dag_id=DAG_NAME,
+    doc_md=__doc__,
+    default_args=DEFAULT_ARGS,
+    schedule_interval="0 */1 * * *",
+    catchup=False,
+    tags=["k8s", "ows"],
+)
+
+
+def parse_dagrun_conf(products, **kwargs):
+    if products:
+        return products
+    else:
+        return UPDATE_EXTENT_PRODUCTS
+
+
+SET_REFRESH_PRODUCT_TASK_NAME = "parse_dagrun_conf"
+
+with dag:
+
+    SET_PRODUCTS = PythonOperator(
+        task_id=SET_REFRESH_PRODUCT_TASK_NAME,
+        python_callable=parse_dagrun_conf,
+        op_args=["{{ dag_run.conf.products }}"],
+        # provide_context=True,
+    )
+    OWS_UPDATE_EXTENTS = SubDagOperator(
+        task_id="run-ows-update-ranges",
+        subdag=ows_update_extent_subdag(
+            DAG_NAME, "run-ows-update-ranges", DEFAULT_ARGS
+        ),
+    )
+
+    START = DummyOperator(task_id="start_ows_update_ranges")
+
+    COMPLETE = DummyOperator(task_id="all_done")
+
+    START >> SET_PRODUCTS
+    SET_PRODUCTS >> OWS_UPDATE_EXTENTS
+    OWS_UPDATE_EXTENTS >> COMPLETE
