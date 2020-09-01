ad7dac1bb8c0abf4aceb6f480632e4d24604d3a0
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-01 05:32:30 +0000

    Add  dag_id to xcom_pull

--- dags/sentinel_2_nrt/utility_explorer_summary.py
+++ dags/sentinel_2_nrt/utility_explorer_summary.py
@@ -81,7 +81,7 @@ with dag:
 
     t2 = SubDagOperator(
         task_id="test_sub_dag",
-        subdag=subdag_test(DAG_NAME, "test_sub_dag", DEFAULT_ARGS, "{{ ti.xcom_pull(task_ids='parse_dagrun_conf') }}")
+        subdag=subdag_test(DAG_NAME, "test_sub_dag", DEFAULT_ARGS, "{{ ti.xcom_pull(task_ids='parse_dagrun_conf', dag_id='utility_explorer-refresh-stats') }}")
     )
 
 
