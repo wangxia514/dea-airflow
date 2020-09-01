c1e138c32f06c0b98ad740309f81cbdb03f5a82a
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-01 23:38:11 +0000

    Allow variable config

--- dags/sentinel_2_nrt/env_cfg.py
+++ dags/sentinel_2_nrt/env_cfg.py
@@ -2,22 +2,28 @@
 # Sentinel-2_nrt process env configs read from variables if environment specific
 """
 from airflow.models import Variable
-foo = Variable.get("db_hostname", "db-writer")
 
-# ENVIRONMENT CONFIGURATION
+# For testing
+foo = Variable.get("bar", "bar")
+
+# OWS pod specific configuration
 OWS_CFG_PATH = "/env/config/ows_cfg.py"
+OWS_CFG_MOUNT_PATH = "/env/config"
+OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
+
+# Process specific for sentinel indexing process
 INDEXING_PRODUCTS = "s2a_nrt_granule s2b_nrt_granule"
 ARCHIVE_PRODUCTS = INDEXING_PRODUCTS
 ARCHIVE_CONDITION = "[$(date -d '-365 day' +%F), $(date -d '-91 day' +%F)]"
 UPDATE_EXTENT_PRODUCTS = "s2_nrt_granule_nbar_t"
-SQS_QUEUE_NAME = "dea-dev-eks-ows-dag"
-INDEXING_ROLE = "dea-dev-eks-orchestration"
-SECRET_AWS_NAME = "processing-aws-creds-dev"
-SECRET_EXPLORER_NAME = "explorer-db"
-SECRET_OWS_NAME = "ows-db"
-DB_DATABASE = "ows"
-DB_HOSTNAME = "db-writer"
 
 
-OWS_CFG_MOUNT_PATH = "/env/config"
-OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
+# Environment shared variables read from airflow variable config
+SQS_QUEUE_NAME =  Variable.get("sqs_queue_name", "dea-dev-eks-ows-dag")
+INDEXING_ROLE = Variable.get("indexing_role", "dea-dev-eks-orchestration")
+
+SECRET_AWS_NAME = Variable.get("secret_aws_name", "processing-aws-creds-dev")
+SECRET_EXPLORER_NAME = Variable.get("secret_explorer_name", "explorer-db")
+SECRET_OWS_NAME = Variable.get("secret_ows_name", "ows-db")
+DB_DATABASE = Variable.get("db_database", "ows")
+DB_HOSTNAME = Variable.get("db_hostname", "db-writer")
--- dags/sentinel_2_nrt/test_dag.py
+++ dags/sentinel_2_nrt/test_dag.py
@@ -1,22 +1,6 @@
 """
 ## Utility Tool
-### explore refresh stats
-This is utility is to provide administrators the easy accessiblity to run ad-hoc --refresh-stats
-
-#### default run
-    `cubedash-gen --no-init-database --refresh-stats --force-refresh s2a_nrt_granule`
-    `cubedash-gen --no-init-database --refresh-stats --force-refresh s2b_nrt_granule`
-
-#### Utility customisation
-The DAG can be parameterized with run time configuration `products`
-
-To run with all, set `dag_run.conf.products` to `--all`
-otherwise provide products to be refreshed seperated by space, i.e. `s2a_nrt_granule s2b_nrt_granule`
-dag_run.conf format:
-
-#### example conf in json format
-    "products": "--all"
-    "products": "s2a_nrt_granule s2b_nrt_granule"
+### simpletesting dag
 """
 
 from airflow import DAG
