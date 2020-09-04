56105741efb8d8ad2de1caa9158a9ea9b26aa638
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-09-04 03:39:35 +0000

    align variable name with env

--- dags/env_var/infra.py
+++ dags/env_var/infra.py
@@ -6,12 +6,12 @@
 from airflow.models import Variable
 
 # role config
-INDEXING_ROLE = Variable.get("indexing_role", "dea-dev-eks-orchestration")
+INDEXING_ROLE = Variable.get("processing_indexing_role", "dea-dev-eks-orchestration")
 
 # secrets name available in processing namespace
-SECRET_AWS_NAME = Variable.get("secret_aws_name", "processing-aws-creds-dev")
-SECRET_EXPLORER_NAME = Variable.get("secret_explorer_name", "explorer-db")
-SECRET_OWS_NAME = Variable.get("secret_ows_name", "ows-db")
+SECRET_AWS_NAME = Variable.get("processing_user_secret", "processing-aws-creds-dev")
+SECRET_EXPLORER_NAME = Variable.get("explorer_db_secret", "explorer-db")
+SECRET_OWS_NAME = Variable.get("ows_db_secret", "ows-db")
 
 # DB config
 DB_DATABASE = Variable.get("db_database", "ows")
