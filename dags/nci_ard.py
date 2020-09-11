381c56fd68667fa0e7625da87d9538748234e885
Duncan Gray <duncan.gray@ga.gov.au>
2020-09-11 11:44:37 +1000

    updating the module used and making it easier to test (#70)
    * updating the module used and making it easier to test
    
    * removing code for testing

--- dags/nci_ard.py
+++ dags/nci_ard.py
@@ -14,56 +14,65 @@ from airflow.operators.dummy_operator import DummyOperator
 
 from sensors.pbs_job_complete_sensor import PBSJobSensor
 
+params = {
+    "project": "v10",
+    "queue": "normal",
+    "module_ass": "ard-scene-select-py3-dea/20200909",
+    "index_arg": "--index-datacube-env "
+    "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env",
+    "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
+    "config_arg": "",
+    "scene_limit": "",
+    "products_arg": "",
+    "pkgdir_arg": "/g/data/xu18/ga",
+    "base_dir": "/g/data/v10/work/c3_ard/",
+}
+ssh_conn_id = "lpgs_gadi"
+schedule_interval = "0 4 * * *"
+
+# Having the info above as variables and some empty values
+# means I can easily test by adding some test code here
+# without modifying the code below.
+
+
 default_args = {
-    'owner': 'Duncan Gray',
-    'depends_on_past': False,  # Very important, will cause a single failure to propagate forever
-    'start_date': datetime(2020, 8, 26),
-    'retries': 0,
-    'retry_delay': timedelta(minutes=1),
-    'ssh_conn_id': 'lpgs_gadi',
-    #'ssh_conn_id': 'dsg547',
-    'params': {
-        'project': 'v10',
-        'queue': 'normal',
-        'module_ass': 'ard-scene-select-py3-dea/20200821',
-        'index_arg': '--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env',
-        #'index_arg': '--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/index-test-odc.env',
-        #'index_arg': '',  # no indexing
-        'wagl_env': '/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env',
-        #'config_arg': '--config /g/data/v10/projects/c3_ard/dea-ard-scene-select/tests/scripts/airflow/dsg547_dev.conf',
-        'pkgdir_arg': '/g/data/xu18/ga'
-    }
+    "owner": "Duncan Gray",
+    "depends_on_past": False,  # Very important, will cause a single failure to propagate forever
+    "start_date": datetime(2020, 8, 31),
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "ssh_conn_id": ssh_conn_id,
+    "params": params,
 }
 
-# tags is in airflow >1.10.8
-# My local env is airflow 1.10.10...
 dag = DAG(
-    'nci_ard',
+    "nci_ard",
     doc_md=__doc__,
     default_args=default_args,
     catchup=False,
-    schedule_interval=None,
-    default_view='graph',
-    tags=['nci', 'landsat_c3'],
+    schedule_interval=schedule_interval,
+    default_view="graph",
+    tags=["nci", "landsat_c3"],
 )
 
 with dag:
-    start = DummyOperator(task_id='start')
-    completed = DummyOperator(task_id='completed')
+    start = DummyOperator(task_id="start")
+    completed = DummyOperator(task_id="completed")
 
     COMMON = """
         #  ts_nodash timestamp no dashes.
-        {% set log_dir = '/g/data/v10/work/c3_ard/' + ts_nodash + '/logdir' %}
-        {% set work_dir = '/g/data/v10/work/c3_ard/' + ts_nodash + '/workdir' %}
+        {% set log_ext = ts_nodash + '/logdir' %}
+        {% set work_ext = ts_nodash + '/workdir' %}
         """
 
     # An example of remotely starting a qsub job (all it does is ls)
-    submit_task_id = f'submit_ard'
+    submit_task_id = f"submit_ard"
     submit_ard = SSHOperator(
         task_id=submit_task_id,
-        command=COMMON + """
-        mkdir -p {{ log_dir }} 
-        mkdir -p {{ work_dir }} 
+        command=COMMON
+        + """
+        mkdir -p {{ params.base_dir }}{{ work_ext }}
+        mkdir -p {{ params.base_dir }}{{ log_ext }}
         qsub -N ard_scene_select \
               -q  {{ params.queue }}  \
               -W umask=33 \
@@ -75,13 +84,16 @@ with dag:
                   module use /g/data/v10/private/modules/modulefiles/; \
                   module load {{ params.module_ass }}; \
                   ard-scene-select \
-                  --workdir {{ work_dir }} \
+                {{ params.products_arg }} \
+                {{ params.config_arg }} \
+                  --workdir {{ params.base_dir }}{{ work_ext }} \
                   --pkgdir {{ params.pkgdir_arg }} \
-                  --logdir {{ log_dir }} \
+                  --logdir {{ params.base_dir }}{{ log_ext }} \
                   --env {{ params.wagl_env }}  \
                   --project {{ params.project }} \
                   --walltime 02:30:00 \
                   {{ params.index_arg }} \
+                  {{ params.scene_limit }}\
                   --run-ard "
         """,
         timeout=60 * 20,
@@ -89,9 +101,9 @@ with dag:
     )
 
     wait_for_completion = PBSJobSensor(
-        task_id=f'wait_for_completion',
+        task_id=f"wait_for_completion",
         pbs_job_id="{{ ti.xcom_pull(task_ids='%s') }}" % submit_task_id,
         timeout=60 * 60 * 24 * 7,
     )
 
-    start  >> submit_ard >> wait_for_completion >> completed
+    start >> submit_ard >> wait_for_completion >> completed
