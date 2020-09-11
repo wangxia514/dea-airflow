d7d260166f46939cf6b4d63b001bffc51e494b02
Tisham Dhar <whatnickd@gmail.com>
2020-09-11 12:10:47 +1000

    #80 Initial DAG

---
+++ dags/k8s_wagl_nrt.py
@@ -0,0 +1,57 @@
+"""
+Run WAGL NRT in Airflow ( migrate from datahub running in AWS Batch)
+"""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    "owner": "Imam Alam",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 9, 11),
+    "email": ["imam.alam@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+WAGL_IMAGE = (
+    "589363755468.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:rc-20190109-5"
+)
+
+pipeline = DAG(
+    "k8s_wagl_nrt",
+    doc_md=__doc__,
+    default_args=default_args,
+    description="DEA Sentinel-2 NRT Processing",
+    concurrency=2,
+    max_active_runs=1,
+    catchup=False,
+    params={},
+    schedule_interval=None,
+    tags=["k8s", "dea", "psc", "wagl", "nrt"],
+)
+
+with pipeline:
+    START = DummyOperator(task_id="start_wagl")
+
+    WAGL_RUN = KubernetesPodOperator(
+        namespace="processing",
+        name="dea-s2-wagl-nrt",
+        task_id="dea-s2-wagl-nrt",
+        image_pull_policy="IfNotPresent",
+        image=WAGL_IMAGE,
+        is_delete_operator_pod=True,
+        arguments=[],
+        labels={"runner": "airflow"},
+        get_logs=True,
+    )
+
+    END = DummyOperator(task_id="end_wagl")
+
+    START >> WAGL_RUN >> END
