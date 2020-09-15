a2394c056314267885d01485e75b96cf755b5b41
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-09-15 14:10:33 +1000

    use latest explorer image

--- dags/k8s_db_sync_new.py
+++ dags/k8s_db_sync_new.py
@@ -62,7 +62,7 @@ DEFAULT_ARGS = {
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
 S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.7.g66e5fe4"
-EXPLORER_IMAGE = "opendatacube/explorer:2.1.11-156-g17d840a"
+EXPLORER_IMAGE = "opendatacube/explorer:2.1.11-157-g6b143e0"
 
 dag = DAG(
     "k8s_db_sync_new",
