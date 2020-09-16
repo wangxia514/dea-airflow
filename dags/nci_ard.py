ee4aa6b41149fd6a887e0bdc1ab4e9d6a88276bd
Duncan Gray <duncan.gray@ga.gov.au>
2020-09-16 14:14:25 +1000

    Have airflow be the primary starter of ARD processing. (#83)

--- dags/nci_ard.py
+++ dags/nci_ard.py
@@ -28,7 +28,7 @@ params = {
     "base_dir": "/g/data/v10/work/c3_ard/",
 }
 ssh_conn_id = "lpgs_gadi"
-schedule_interval = "0 4 * * *"
+schedule_interval = "0 16 * * *"
 
 # Having the info above as variables and some empty values
 # means I can easily test by adding some test code here
