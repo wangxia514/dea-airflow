f79f8314d8dac8c45b501b15bd07082553559852
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-09-24 13:06:53 +1000

    prepare nci incremental db sync dag

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -26,11 +26,12 @@ from datetime import datetime, timedelta
 import pendulum
 
 # Templated DAG arguments
-DB_HOSTNAME = "db-writer"
-DB_DATABASE = "nci_20200917"
 local_tz = pendulum.timezone("Australia/Canberra")
-DATESTRING  = "{{ ds }}"
+DB_HOSTNAME = "db-writer"
+DB_DATABASE = "nci_20200918"
+DATESTRING = "{{ ds }}"
 S3_KEY = f"s3://nci-db-dump/csv-changes/{DATESTRING}/agdc.dataset_changes.csv.gz"
+BACKUP_PATH = "/scripts/backup"
 
 DEFAULT_ARGS = {
     "owner": "Nikita Gandhi",
@@ -47,6 +48,8 @@ DEFAULT_ARGS = {
         "DB_HOSTNAME": DB_HOSTNAME,
         "DB_DATABASE": DB_DATABASE,
         "DB_PORT": "5432",
+        "BACKUP_PATH": BACKUP_PATH,
+        "DATESTRING": DATESTRING
     },
     # Use K8S secrets to send DB Creds
     # Lift secrets into environment variables for datacube
@@ -57,7 +60,7 @@ DEFAULT_ARGS = {
 }
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
-S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.9.g05c505e"
+S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.14.g174ff27"
 EXPLORER_IMAGE = "opendatacube/explorer:2.1.11-157-g6b143e0"
 
 dag = DAG(
@@ -71,23 +74,79 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
+affinity = {
+    "nodeAffinity": {
+        "requiredDuringSchedulingIgnoredDuringExecution": {
+            "nodeSelectorTerms": [{
+                "matchExpressions": [{
+                    "key": "nodetype",
+                    "operator": "In",
+                    "values": [
+                        "ondemand",
+                    ]
+                }]
+            }]
+        }
+    }
+}
+
+s3_backup_volume_mount = VolumeMount(name="s3-backup-volume",
+                                     mount_path=BACKUP_PATH,
+                                     sub_path=None,
+                                     read_only=False)
+
+s3_backup_volume_config = {
+    "persistentVolumeClaim":
+        {
+            "claimName": "s3-backup-volume"
+        }
+}
+
+s3_backup_volume = Volume(name="s3-backup-volume", configs=s3_backup_volume_config)
 
 with dag:
     START = DummyOperator(task_id="nci_db_incremental_sync")
 
     # Wait for S3 Key
     S3_BACKUP_SENSE = S3KeySensor(
+        labels={"step": "s3-backup-sense"},
+        name="s3-backup-sense",
         task_id="s3_backup_sense",
         poke_interval=60 * 30,
         bucket_key=S3_KEY,
         aws_conn_id="aws_nci_db_backup",
     )
 
-    # Download PostgreSQL incremental backup from S3 and restore to RDS Aurora
-    RESTORE_NCI_INCREMENTAL_SYNC = DummyOperator(task_id="restore_nci_incremental_sync")
+    # Download NCI db incremental backup from S3 and restore to RDS Aurora
+    RESTORE_NCI_INCREMENTAL_SYNC = KubernetesPodOperator(
+        namespace="processing",
+        image=S3_TO_RDS_IMAGE,
+        annotations={"iam.amazonaws.com/role": "svc-dea-dev-eks-processing-dbsync"},  # TODO: Pass this via DAG parameters
+        cmds=["./import_from_s3.sh"],
+        image_pull_policy="Always",
+        labels={"step": "s3-to-rds"},
+        name="s3-to-rds",
+        task_id="s3_to_rds",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        affinity=affinity,
+        volumes=[s3_backup_volume],
+        volume_mounts=[s3_backup_volume_mount],
+    )
 
     # Run update summary
-    UPDATE_SUMMARY = DummyOperator(task_id="update_summary")
+    UPDATE_SUMMARY = KubernetesPodOperator(
+        namespace="processing",
+        image=EXPLORER_IMAGE,
+        cmds=["cubedash-gen"],
+        arguments=["--no-init-database", "--refresh-stats", "--force-refresh", "--all"],
+        labels={"step": "summarize-datacube"},
+        name="summarize-datacube",
+        task_id="summarize_datacube",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        affinity=affinity,
+    )
 
     # Task complete
     COMPLETE = DummyOperator(task_id="all_done")
