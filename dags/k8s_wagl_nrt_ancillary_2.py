c686394d0e7417b7062f41f0ed04250b3d3557b3
Imam Alam <imam.alam@gmail.com>
2020-09-29 00:51:39 +0000

    wagl_nrt_ancillary 2 vol sync cacheable

--- dags/k8s_wagl_nrt_ancillary_2.py
+++ dags/k8s_wagl_nrt_ancillary_2.py
@@ -15,8 +15,25 @@ S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.26.g5ffb384"
 
 SYNC_CMD = "aws s3 sync --only-show-errors"
 SYNC_JOB = f"""
+date &&
 echo synching ozone &&
-{SYNC_CMD} s3://ga-sentinel/ancillary/lookup_tables/ozone/ /ancillary/ozone
+{SYNC_CMD} s3://ga-sentinel/ancillary/lookup_tables/ozone/ /ancillary/ozone &&
+echo synching dsm &&
+{SYNC_CMD} s3://ga-sentinel/ancillary/elevation/tc_aus_3sec/ /ancillary/dsm &&
+echo synching elevation &&
+{SYNC_CMD} s3://ga-sentinel/ancillary/elevation/world_1deg /ancillary/elevation/world_1deg &&
+echo synching aerosol &&
+{SYNC_CMD} --exclude "*" --include "aerosol.h5" \
+        s3://ga-sentinel/ancillary/aerosol/AATSR/2.0/ /ancillary/aerosol &&
+echo synching invariant height &&
+{SYNC_CMD} s3://dea-dev-bucket/s2-wagl-nrt/invariant /ancillary/invariant &&
+echo synching land sea rasters &&
+{SYNC_CMD} --exclude "*" --include Land_Sea_Rasters.tar.z \
+        s3://dea-dev-bucket/s2-wagl-nrt/ /ancillary &&
+echo extracting land sea rasters &&
+tar --keep-newer-files --no-same-owner \
+        -xf /ancillary/Land_Sea_Rasters.tar.z -C /ancillary/
+date
 """
 
 default_args = {
@@ -74,6 +91,20 @@ with pipeline:
         volume_mounts=[ancillary_volume_mount],
     )
 
+    VERIFY = KubernetesPodOperator(
+        namespace="processing",
+        image=S3_TO_RDS_IMAGE,
+        annotations={"iam.amazonaws.com/role": "svc-dea-dev-eks-wagl-nrt"},
+        cmds=["find", "/ancillary"],
+        image_pull_policy="Always",
+        name="sync_ancillaries",
+        task_id="sync_ancillaries",
+        get_logs=True,
+        # TODO: affinity=affinity,
+        volumes=[ancillary_volume],
+        volume_mounts=[ancillary_volume_mount],
+    )
+
     END = DummyOperator(task_id="end")
 
-    START >> COPY >> END
+    START >> COPY >> VERIFY >> END
