2584277181c99e278588f6de480da1c317f65700
Imam Alam <imam.alam@gmail.com>
2020-09-30 04:05:24 +0000

    wagl_nrt: ancillary updates

--- dags/k8s_wagl_nrt_ancillary.py
+++ dags/k8s_wagl_nrt_ancillary.py
@@ -5,36 +5,66 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.kubernetes.secret import Secret
 from airflow.kubernetes.volume import Volume
 from airflow.kubernetes.volume_mount import VolumeMount
-from airflow.operators.dummy_operator import DummyOperator
 
+
+# TODO non-cacheable ancillaries
+# TODO cleanup non-cacheable ancillaries
+
+
+NOW = datetime.now()
 
 S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.26.g5ffb384"
 
-SYNC_CMD = "aws s3 sync --only-show-errors"
-SYNC_JOB = f"""
-date &&
-echo synching ozone &&
-{SYNC_CMD} s3://ga-sentinel/ancillary/lookup_tables/ozone/ /ancillary/ozone &&
-echo synching dsm &&
-{SYNC_CMD} s3://ga-sentinel/ancillary/elevation/tc_aus_3sec/ /ancillary/dsm &&
-echo synching elevation &&
-{SYNC_CMD} s3://ga-sentinel/ancillary/elevation/world_1deg/ /ancillary/elevation/world_1deg &&
-echo synching aerosol &&
-{SYNC_CMD} --exclude "*" --include "aerosol.h5" \
-        s3://ga-sentinel/ancillary/aerosol/AATSR/2.0/ /ancillary/aerosol &&
-echo synching invariant height &&
-{SYNC_CMD} s3://dea-dev-bucket/s2-wagl-nrt/invariant/ /ancillary/invariant &&
-echo synching land sea rasters &&
-{SYNC_CMD} --exclude "*" --include Land_Sea_Rasters.tar.z \
-        s3://dea-dev-bucket/s2-wagl-nrt/ /ancillary &&
-echo extracting land sea rasters &&
-tar -xvf /ancillary/Land_Sea_Rasters.tar.z -C /ancillary/ &&
-find /ancillary/ -type f
-date
-"""
+
+def sync(*args):
+    return "aws s3 sync --only-show-errors " + " ".join(args)
+
+
+SYNC_JOBS = [
+    "date",
+    "echo synching ozone",
+    sync("s3://ga-sentinel/ancillary/lookup_tables/ozone/", "/ancillary/ozone"),
+    "echo synching dsm",
+    sync("s3://ga-sentinel/ancillary/elevation/tc_aus_3sec/", "/ancillary/dsm"),
+    "echo synching elevation",
+    sync(
+        "s3://ga-sentinel/ancillary/elevation/world_1deg/",
+        "/ancillary/elevation/world_1deg",
+    ),
+    "echo synching aerosol",
+    sync(
+        '--exclude "*" --include "aerosol.h5"',
+        "s3://ga-sentinel/ancillary/aerosol/AATSR/2.0/",
+        "/ancillary/aerosol",
+    ),
+    "echo synching invariant height",
+    sync("s3://dea-dev-bucket/s2-wagl-nrt/invariant/", "/ancillary/invariant"),
+    "echo synching land sea rasters",
+    sync(
+        '--exclude "*" --include Land_Sea_Rasters.tar.z',
+        "s3://dea-dev-bucket/s2-wagl-nrt/",
+        "/ancillary",
+    ),
+    "echo extracting land sea rasters",
+    "tar -xvf /ancillary/Land_Sea_Rasters.tar.z -C /ancillary/",
+    "echo synching water vapour",
+    *[
+        sync(
+            f'--exclude "*" --include "*{year}*"',
+            "s3://ga-sentinel/ancillary/water_vapour",
+            "/ancillary/water_vapour",
+        )
+        for year in (
+            [NOW.year] + [NOW.year - 1] if NOW.month == 1 and NOW.day < 7 else []
+        )
+    ],
+    "find /ancillary/ -type f",
+    "date",
+]
 
 default_args = {
     "owner": "Imam Alam",
@@ -81,7 +111,7 @@ with pipeline:
         namespace="processing",
         image=S3_TO_RDS_IMAGE,
         annotations={"iam.amazonaws.com/role": "svc-dea-dev-eks-wagl-nrt"},
-        cmds=["bash", "-c", SYNC_JOB],
+        cmds=["bash", "-c", " &&\n".join(SYNC_JOBS)],
         image_pull_policy="Always",
         name="sync_ancillaries",
         task_id="sync_ancillaries",
