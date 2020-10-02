0c5658a8828d4d0d8b7b72a81107c524f92206ad
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-10-02 14:52:18 +1000

    typo

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -28,9 +28,9 @@ DATESTRING = "{{ ds }}"
 S3_IMPORT_DATE = "{{ dag_run.conf and dag_run.conf.get('s3importdate', '') }}"
 S3_BUCKET = "nci-db-dump"
 if S3_IMPORT_DATE == '':
-    S3_PREFIX=f"csv-changes/{S3_IMPORT_DATE}"
-else:
     S3_PREFIX=f"csv-changes/{DATESTRING}"
+else:
+    S3_PREFIX=f"csv-changes/{S3_IMPORT_DATE}"
 S3_KEY = f"s3://{S3_BUCKET}/{S3_PREFIX}/agdc.dataset_changes.csv.gz"
 BACKUP_PATH = "/scripts/backup"
 
