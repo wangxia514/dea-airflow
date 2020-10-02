8ae3c42010b6eda622547eabb9f26a061c229f3c
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-10-02 15:01:19 +1000

    issue with datestring

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -27,10 +27,12 @@ DB_DATABASE = "nci_20200925"
 DATESTRING = "{{ ds }}"
 S3_IMPORT_DATE = "{{ dag_run.conf and dag_run.conf.get('s3importdate', '') }}"
 S3_BUCKET = "nci-db-dump"
-if S3_IMPORT_DATE == '':
-    S3_PREFIX=f"csv-changes/{DATESTRING}"
-else:
+if bool(S3_IMPORT_DATE):
+    print("not empty")
     S3_PREFIX=f"csv-changes/{S3_IMPORT_DATE}"
+else:
+    print("empty")
+    S3_PREFIX=f"csv-changes/{DATESTRING}"
 S3_KEY = f"s3://{S3_BUCKET}/{S3_PREFIX}/agdc.dataset_changes.csv.gz"
 BACKUP_PATH = "/scripts/backup"
 
