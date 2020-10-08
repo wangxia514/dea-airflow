cd5c576e385ed75ff194baa5a48cca50ee0bd530
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-10-08 12:54:54 +1100

    add DAGs to perform NCI db incremental sync and update-summary

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """
-### NCI to DEA RDS Datacube DB migration
+### NCI to DEA RDS Datacube DB incremental sync
 
-DAG to periodically sync NCI datacube database to RDS mainly for the purpose of
-running [Explorer](https://github.com/opendatacube/datacube-explorer)
+Daily DAG to sync NCI datacube db CSVs' from S3 to RDS for the purpose of
+running [NCI Explorer](https://github.com/opendatacube/datacube-explorer)
 and [Resto](https://github.com/jjrom/resto).
 
+#### Docker image notes
+s3-to-rds: https://bitbucket.org/geoscienceaustralia/s3-to-rds/src/master/
+
+### Airflow dependencies
 [Waits for S3Key](https://gist.github.com/nehiljain/6dace5faccb680653f7ea4d5d5273946)
 for a day's backup to be available via
 [S3KeySensor](https://airflow.apache.org/docs/stable/_api/airflow/sensors/s3_key_sensor/index.html)
-and executes downstream tasks that restores, indexes and runs updating summary of newly created database
+and executes downstream task
 """
 
-from datetime import datetime, timedelta
-
+import pendulum
 from airflow import DAG
 from airflow.sensors.s3_key_sensor import S3KeySensor
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
@@ -22,56 +25,60 @@ from airflow.kubernetes.secret import Secret
 from airflow.kubernetes.volume import Volume
 from airflow.kubernetes.volume_mount import VolumeMount
 from airflow.operators.dummy_operator import DummyOperator
-# from env_var.infra import DB_HOSTNAME
+from datetime import datetime, timedelta
+
+local_tz = pendulum.timezone("Australia/Canberra")
 
 # Templated DAG arguments
-DATESTRING = "{{ ds_nodash }}"
 DB_HOSTNAME = "db-writer"
-DB_DATABASE = f"nci_{DATESTRING}"
-FILE_PREFIX = f"dea-db.nci.org.au-{DATESTRING}"
-S3_KEY = f"s3://nci-db-dump/prod/{FILE_PREFIX}-datacube.pgdump"
-S3_KEY = f"s3://nci-db-dump/prod/{FILE_PREFIX}-datacube.pgdump"
+DB_DATABASE = "nci_20201007"
+DATESTRING = "{{ ds }}"
+S3_BUCKET = "nci-db-dump"
+S3_PREFIX=f"csv-changes/{DATESTRING}"
+S3_KEY = f"s3://{S3_BUCKET}/{S3_PREFIX}/agdc.dataset_changes.csv.gz"
 BACKUP_PATH = "/scripts/backup"
 
 DEFAULT_ARGS = {
     "owner": "Nikita Gandhi",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 10, 8, tzinfo=local_tz),
     "email": ["nikita.gandhi@ga.gov.au"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "depends_on_past": False,
-    "start_date": datetime(2020, 10, 7),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
     "env_vars": {
         "AWS_DEFAULT_REGION": "ap-southeast-2",
-        "S3_KEY": S3_KEY,
         "DB_HOSTNAME": DB_HOSTNAME,
         "DB_DATABASE": DB_DATABASE,
         "DB_PORT": "5432",
         "BACKUP_PATH": BACKUP_PATH,
-        "DATESTRING": DATESTRING
+        "DATESTRING": DATESTRING,
+        "S3_BUCKET": S3_BUCKET,
+        "S3_PREFIX": S3_PREFIX,
+        "S3_KEY": S3_KEY
     },
     # Use K8S secrets to send DB Creds
     # Lift secrets into environment variables for datacube
     "secrets": [
-        Secret("env", "DB_USERNAME", "explorer-admin", "postgres-username"),
-        Secret("env", "DB_PASSWORD", "explorer-admin", "postgres-password"),
+        Secret("env", "DB_ADMIN_USER", "explorer-admin", "postgres-username"),  # To run import from s3
+        Secret("env", "DB_ADMIN_PASSWORD", "explorer-admin", "postgres-password"),
     ],
 }
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
 S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.37.gecc8ab3"
-EXPLORER_IMAGE = "opendatacube/explorer:2.2.1"
 
 dag = DAG(
-    "k8s_nci_db_sync",
+    "k8s_nci_db_incremental_sync",
     doc_md=__doc__,
     default_args=DEFAULT_ARGS,
     catchup=False,
     concurrency=1,
     max_active_runs=1,
     tags=["k8s"],
-    schedule_interval=timedelta(days=7),
+    schedule_interval='45 0 * * *',     # every day 0:45AM
+    dagrun_timeout=timedelta(minutes=60*3),
 )
 
 affinity = {
@@ -105,7 +112,7 @@ s3_backup_volume_config = {
 s3_backup_volume = Volume(name="s3-backup-volume", configs=s3_backup_volume_config)
 
 with dag:
-    START = DummyOperator(task_id="nci-db-sync")
+    START = DummyOperator(task_id="nci-db-incremental-sync")
 
     # Wait for S3 Key
     S3_BACKUP_SENSE = S3KeySensor(
@@ -117,12 +124,12 @@ with dag:
         aws_conn_id="aws_nci_db_backup",
     )
 
-    # Download PostgreSQL full backup from S3 and restore to RDS Aurora
-    RESTORE_RDS_S3 = KubernetesPodOperator(
+    # Download NCI db incremental backup from S3 and restore to RDS Aurora
+    RESTORE_NCI_INCREMENTAL_SYNC = KubernetesPodOperator(
         namespace="processing",
         image=S3_TO_RDS_IMAGE,
-        annotations={"iam.amazonaws.com/role": "svc-dea-sandbox-eks-processing-dbsync"},
-        cmds=["./s3_to_rds.sh"],
+        annotations={"iam.amazonaws.com/role": "svc-dea-dev-eks-processing-dbsync"},
+        cmds=["./import_from_s3.sh"],
         image_pull_policy="Always",
         labels={"step": "s3-to-rds"},
         name="s3-to-rds",
@@ -134,52 +141,10 @@ with dag:
         volume_mounts=[s3_backup_volume_mount],
     )
 
-    # Restore dynamic indices
-    DYNAMIC_INDICES = KubernetesPodOperator(
-        namespace="processing",
-        image=EXPLORER_IMAGE,
-        cmds=["datacube"],
-        arguments=["-v", "system", "init", "--lock-table"],
-        labels={"step": "odc-indices"},
-        name="odc-indices",
-        task_id="odc-indices",
-        get_logs=True,
-        is_delete_operator_pod=True,
-        affinity=affinity,
-    )
-
-    # Run summary
-    SUMMARIZE_DATACUBE = KubernetesPodOperator(
-        namespace="processing",
-        image=EXPLORER_IMAGE,
-        cmds=["cubedash-gen"],
-        arguments=["--init", "--all"],
-        labels={"step": "summarize-datacube"},
-        name="summarize-datacube",
-        task_id="summarize-datacube",
-        get_logs=True,
-        is_delete_operator_pod=True,
-        affinity=affinity,
-    )
-
-    # Setup DB user permissions
-    SETUP_DB_PERMISSIONS = KubernetesPodOperator(
-        namespace="processing",
-        image=S3_TO_RDS_IMAGE,
-        cmds=["./setup_db_permissions.sh"],
-        labels={"step": "setup-db-permissions"},
-        name="setup-db-permissions",
-        task_id="setup-db-permissions",
-        get_logs=True,
-        is_delete_operator_pod=True,
-        affinity=affinity,
-    )
-
+    # Task complete
     COMPLETE = DummyOperator(task_id="done")
 
+
     START >> S3_BACKUP_SENSE
-    S3_BACKUP_SENSE >> RESTORE_RDS_S3
-    RESTORE_RDS_S3 >> DYNAMIC_INDICES
-    DYNAMIC_INDICES >> SUMMARIZE_DATACUBE
-    SUMMARIZE_DATACUBE >> SETUP_DB_PERMISSIONS
-    SETUP_DB_PERMISSIONS >> COMPLETE
+    S3_BACKUP_SENSE >> RESTORE_NCI_INCREMENTAL_SYNC
+    RESTORE_NCI_INCREMENTAL_SYNC >> COMPLETE
---
+++ dags/k8s_nci_db_update_summary.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+"""
+### DEA NCI dev database - summarize datacube
+
+DAG to run DEA NCI datacube db update summary on weekly bases for the purpose of
+running [NCI Explorer](https://github.com/opendatacube/datacube-explorer)
+and [Resto](https://github.com/jjrom/resto).
+
+"""
+
+import pendulum
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+
+local_tz = pendulum.timezone("Australia/Canberra")
+
+# Templated DAG arguments
+DB_HOSTNAME = "db-writer"
+DB_DATABASE = "nci_20201007"
+
+DEFAULT_ARGS = {
+    "owner": "Nikita Gandhi",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 10, 8, tzinfo=local_tz),
+    "email": ["nikita.gandhi@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "env_vars": {
+        "AWS_DEFAULT_REGION": "ap-southeast-2",
+        "DB_HOSTNAME": DB_HOSTNAME,
+        "DB_DATABASE": DB_DATABASE,
+        "DB_PORT": "5432",
+
+    },
+    # Use K8S secrets to send DB Creds
+    # Lift secrets into environment variables for datacube
+    "secrets": [
+        Secret("env", "DB_USERNAME", "explorer-writer", "postgres-username"),   # To run cubedash-gen
+        Secret("env", "DB_PASSWORD", "explorer-writer", "postgres-password"),
+    ],
+}
+
+EXPLORER_IMAGE = "opendatacube/explorer:2.2.1"
+
+dag = DAG(
+    "k8s_nci_db_update_summary",
+    doc_md=__doc__,
+    default_args=DEFAULT_ARGS,
+    catchup=False,
+    concurrency=1,
+    max_active_runs=1,
+    tags=["k8s"],
+    schedule_interval="5 1 * * sat",    # every saturday 1:05AM
+    dagrun_timeout=timedelta(minutes=60*13),
+)
+
+affinity = {
+    "nodeAffinity": {
+        "requiredDuringSchedulingIgnoredDuringExecution": {
+            "nodeSelectorTerms": [{
+                "matchExpressions": [{
+                    "key": "nodetype",
+                    "operator": "In",
+                    "values": [
+                        "ondemand",
+                    ]
+                }]
+            }]
+        }
+    }
+}
+
+with dag:
+    START = DummyOperator(task_id="nci-db-update-summary")
+
+    # Run update summary
+    UPDATE_SUMMARY = KubernetesPodOperator(
+        namespace="processing",
+        image=EXPLORER_IMAGE,
+        cmds=["cubedash-gen"],
+        arguments=["--no-init-database", "--refresh-stats", "--force-refresh", "--all"],
+        labels={"step": "summarize-datacube"},
+        name="summarize-datacube",
+        task_id="summarize-datacube",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        affinity=affinity,
+        execution_timeout=timedelta(hours=15),
+    )
+
+    # Task complete
+    COMPLETE = DummyOperator(task_id="done")
+
+
+    START >> UPDATE_SUMMARY
+    UPDATE_SUMMARY >> COMPLETE
