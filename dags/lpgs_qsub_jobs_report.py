bda26e8f6f4eb9ce6b8218c79573009b602caa3c
Damien Ayers <damien@omad.net>
2020-10-09 15:37:55 +1100

    Misc cleanups

--- dags/lpgs_qsub_jobs_report.py
+++ dags/lpgs_qsub_jobs_report.py
@@ -2,14 +2,12 @@
 Another test DAG
 """
 from datetime import timedelta
+
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.hooks.ssh_hook import SSHHook
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-
-sshHook = SSHHook(ssh_conn_id='lpgs_gadi')
 
 default_args = {
     'owner': 'damien',
@@ -23,19 +21,6 @@ dag = DAG(
     schedule_interval=timedelta(days=1)
 )
 
-# TODO: This could filter out jobs completed outside this period
-t1 = SSHOperator(
-    task_id="task1",
-    command='qstat -x -f -F json',
-    # ssh_hook=sshHook,
-    ssh_conn_id='lpgs_gadi',
-    dag=dag)
-
-example_bash_task = BashOperator(
-    task_id='example_bash_task',
-    bash_command='echo {{ task_instance.xcom_pull(task_ids="get_files") }}',
-)
-
 
 def my_python_callable(*args, **kwargs):
     """
@@ -44,7 +29,19 @@ def my_python_callable(*args, **kwargs):
     print('foo')
 
 
-put_into_postgres = PythonOperator(
-    task_id="save_qstat_to_postgres",
-    python_callable=my_python_callable
-)
+with dag:
+    # TODO: This could filter out jobs completed outside this period
+    t1 = SSHOperator(
+        task_id="task1",
+        command='qstat -x -f -F json',
+        ssh_conn_id='lpgs_gadi')
+
+    example_bash_task = BashOperator(
+        task_id='example_bash_task',
+        bash_command='echo {{ task_instance.xcom_pull(task_ids="get_files") }}',
+    )
+
+    put_into_postgres = PythonOperator(
+        task_id="save_qstat_to_postgres",
+        python_callable=my_python_callable
+    )
