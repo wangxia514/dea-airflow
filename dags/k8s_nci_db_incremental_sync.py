51a4e2ceb5fc28002ce92e7b8b5eb8cd835c6b03
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-10-09 16:24:33 +1100

    i defeat

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -33,9 +33,6 @@ local_tz = pendulum.timezone("Australia/Canberra")
 DB_HOSTNAME = "db-writer"
 DB_DATABASE = "nci_20200925"
 DATESTRING = "{{ ds }}"
-S3_IMPORT_DATE = "{{ dag_run.conf.get('s3importdate') if dag_run.conf }}"
-if f"{S3_IMPORT_DATE}" is not None and bool(f"{S3_IMPORT_DATE}"):
-    DATESTRING = f"{S3_IMPORT_DATE}"
 # DATESTRING = "{{ macros.ds_add(ds, -1) }}"  # get s3 key for previous day
 # NOTE: uncomment if you want to run DAG manually to import for specific date -  {"s3importdate": "<import-date>"}
 S3_BUCKET = "nci-db-dump"
