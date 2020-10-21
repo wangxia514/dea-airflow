ab85fdd9167fe409ad20c4cdca1d6f98c5f7fd1e
Imam Alam <imam.alam@gmail.com>
2020-10-21 01:08:52 +0000

    wagl_nrt: new image

--- dags/k8s_wagl_nrt.py
+++ dags/k8s_wagl_nrt.py
@@ -30,7 +30,7 @@ default_args = {
 }
 
 WAGL_IMAGE = (
-    "451924316694.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20201020-1"
+    "451924316694.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20201021-1"
 )
 S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.36.g1347ee8"
 
@@ -214,6 +214,7 @@ with pipeline:
         image_pull_policy="IfNotPresent",
         image=WAGL_IMAGE,
         # TODO: affinity=affinity,
+        cmds=["/scripts/process-scenes.sh"],
         arguments=[
             "{{ task_instance.xcom_pull(task_ids='copy_cmd', key='args')['granule_url'] }}",
             "{{ task_instance.xcom_pull(task_ids='copy_cmd', key='args')['datastrip_url'] }}",
