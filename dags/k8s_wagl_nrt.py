70f5f0cbde1ff1ed0328ec358aa3cfaed6151f71
Imam Alam <imam.alam@gmail.com>
2020-10-21 04:26:42 +0000

    wagl_nrt: new image again

--- dags/k8s_wagl_nrt.py
+++ dags/k8s_wagl_nrt.py
@@ -30,7 +30,7 @@ default_args = {
 }
 
 WAGL_IMAGE = (
-    "451924316694.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20201021-3"
+    "451924316694.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20201021-4"
 )
 S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.36.g1347ee8"
 
