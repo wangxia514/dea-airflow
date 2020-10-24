138bfe88ddd53ef455a130c1034d2bc6dec74543
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-10-25 07:21:22 +1100

    update s3-to-rds image to 0.1.1-unstable.50.g9c7d952: incremental import fix for agdc.dataset_type

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -71,7 +71,7 @@ DEFAULT_ARGS = {
 }
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
-S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.49.gf1fbfbb"
+S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.1-unstable.50.g9c7d952"
 
 dag = DAG(
     "k8s_nci_db_incremental_sync",
