3fc5d7b66552f152923e4d2e007603121dd71128
Alex Leith <alexgleith@gmail.com>
2020-11-03 08:30:08 +1100

    Do less parallelisation for backlog WO and FC

--- dags/k8s_index_fc_wo_c3_backlog.py
+++ dags/k8s_index_fc_wo_c3_backlog.py
@@ -48,45 +48,38 @@ DEFAULT_ARGS = {
 INDEXER_IMAGE = "opendatacube/datacube-index:0.0.12"
 
 dag = DAG(
-    "Sentinel-2_indexing",
-    doc_md=__doc__,
-    default_args=DEFAULT_ARGS,
-    catchup=False,
-    tags=["k8s", "landsat_c3"],
-)
-
-dag = DAG(
-    "Sentinel-2-backlog-indexing",
+    "k8s_index_wo_fc_c3_backlog",
     default_args=DEFAULT_ARGS,
     schedule_interval=DEFAULT_ARGS["schedule_interval"],
-    tags=["Sentinel-2", "indexing"],
+    tags=["k8s", "Landsat_c3"],
     catchup=False,
 )
 
 with dag:
     # This needs to be updated in the future in case more zones have been added
     # Rows should be from 88 to 116, and paths from 67 to 91
-    paths = range(88, 117)
+    # paths = range(88, 117)
     rows = range(67, 92)
     products = ["ga_ls_fc_3", "ga_ls_wo_3"]
     utm_zones = range(26, 42)
-    for path in paths:
+
+    for product in products:
         for row in rows:
-            for product in products:
-                INDEXING = KubernetesPodOperator(
-                    namespace="processing",
-                    image=INDEXER_IMAGE,
-                    image_pull_policy="Always",
-                    arguments=[
-                        "s3-to-dc",
-                        "--stac",
-                        "--no-sign-request",
-                        f"s3://dea-public-data/derivative/{product}/{row}/{path}/**/*.json",
-                        " ".join(products),
-                    ],
-                    labels={"backlog": "s3-to-dc"},
-                    name="datacube-index",
-                    task_id=f"Sentinel-2-backlog-indexing-task-{product}-{path}-{row}",
-                    get_logs=True,
-                    is_delete_operator_pod=True,
-                )
+            # for path in paths:
+            INDEXING = KubernetesPodOperator(
+                namespace="processing",
+                image=INDEXER_IMAGE,
+                image_pull_policy="Always",
+                arguments=[
+                    "s3-to-dc",
+                    "--stac",
+                    "--no-sign-request",
+                    f"s3://dea-public-data/derivative/{product}/{row}/**/*.json",
+                    " ".join(products),
+                ],
+                labels={"backlog": "s3-to-dc"},
+                name="datacube-index",
+                task_id=f"{product}--Backlog-indexing-row--{row}",
+                get_logs=True,
+                is_delete_operator_pod=True,
+            )
