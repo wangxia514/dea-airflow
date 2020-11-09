4b68abf2d07f64724b36dd19a061cd5897dd185a
Imam Alam <imam.alam@gmail.com>
2020-11-09 05:40:37 +0000

    wagl_nrt: test dag please ignore 12

--- dags/k8s_test_dag.py
+++ dags/k8s_test_dag.py
@@ -36,9 +36,9 @@ resources = {
 
 with pipeline:
     with patch.object(
-        Resources,
+        "airflow.contrib.operators.kubernetes_pod_operator.Resources",
         "to_k8s_client_obj",
-        return_value={"requests": {"memory": "2G", "cpu": "1000m"}},
+        return_value={"requests": {"memory": "3G", "cpu": "1000m"}},
     ) as mock_resources:
         res = Resources(**resources)
         val = res.to_k8s_client_obj()
