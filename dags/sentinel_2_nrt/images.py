513ab4f02eb5c29618f98014b73d55388c779d03
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-11-09 00:00:36 +0000

    use latest datacube -index

--- dags/sentinel_2_nrt/images.py
+++ dags/sentinel_2_nrt/images.py
@@ -4,9 +4,9 @@
 
 
 # IMAGES USED FOR THIS DAG
-INDEXER_IMAGE = "opendatacube/datacube-index:0.0.9-9-g4aacd64"
+INDEXER_IMAGE = "opendatacube/datacube-index:0.0.13"
 
-OWS_IMAGE = "opendatacube/ows:1.8.1-128-g0b9ec12"
+OWS_IMAGE = "opendatacube/ows:1.8.2"
 OWS_CONFIG_IMAGE = "geoscienceaustralia/dea-datakube-config:1.5.4-unstable.2.g85ae96d"
 
 
