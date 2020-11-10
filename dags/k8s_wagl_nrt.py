7b5f1d882848520c3a72668c03663876fde7ff8c
Imam Alam <imam.alam@gmail.com>
2020-11-10 01:38:16 +0000

    wagl_nrt: turn on wagl

--- dags/k8s_wagl_nrt.py
+++ dags/k8s_wagl_nrt.py
@@ -245,7 +245,7 @@ pipeline = DAG(
     max_active_runs=MAX_ACTIVE_RUNS,
     catchup=False,
     params={},
-    schedule_interval=None,  # timedelta(minutes=30),
+    schedule_interval=timedelta(minutes=30),
     tags=["k8s", "dea", "psc", "wagl", "nrt"],
 )
 
