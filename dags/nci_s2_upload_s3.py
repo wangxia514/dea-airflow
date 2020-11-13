7aa7646d8926e41676e9c39457cc510a1feb8db1
Damien Ayers <damien@omad.net>
2020-11-13 16:43:18 +1100

    All awful

--- dags/nci_s2_upload_s3.py
+++ dags/nci_s2_upload_s3.py
@@ -11,8 +11,6 @@ data from NCI to AWS S3 bucket. It:
 This DAG takes following input parameters from `nci_s2_upload_s3_config` variable:
 
  * `s3bucket`: Name of the S3 bucket. `"dea-public-data"`
- * `numdays`: Number of days to process before the execution date selected is `"0"`
- * `enddate`: End date for process execution.`"2020-08-26"`
  * `doupdate`: Select update option as below to replace granules and metadata.
     * `'granule_metadata'` to update granules and metadata;
     * `'granule' to update'` granules without metadata;
@@ -20,20 +18,50 @@ This DAG takes following input parameters from `nci_s2_upload_s3_config` variabl
     * `'no'` or don't set to avoid update of existing granules/metadata..
 """
 from datetime import datetime, timedelta
-from textwrap import dedent
 from pathlib import Path
+from textwrap import dedent
 
+import pendulum
 from airflow import DAG, configuration
-from airflow.models import Variable
 from airflow.contrib.hooks.aws_hook import AwsHook
-from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.contrib.operators.sftp_operator import SFTPOperator, SFTPOperation
+from airflow.contrib.operators.ssh_operator import SSHOperator
 
+local_tz = pendulum.timezone("Australia/Canberra")
+
+# language=SQL
+SQL_QUERY = """SELECT dsl.uri_body, ds.archived, ds.added, 
+    to_timestamp({{ next_execution_date.timestamp() }}) at time zone 'Australia/Canberra' as exec_dt 
+    FROM agdc.dataset ds 
+    INNER JOIN agdc.dataset_type dst ON ds.dataset_type_ref = dst.id 
+    INNER JOIN agdc.dataset_location dsl ON ds.id = dsl.dataset_ref 
+    WHERE dst.name='{{ params.product }}' 
+        AND (ds.added BETWEEN 
+                (to_timestamp({{ execution_date.timestamp() }}) at time zone 'Australia/Canberra') 
+                AND (to_timestamp({{ next_execution_date.timestamp() }}) at time zone 'Australia/Canberra') 
+            OR ds.archived BETWEEN 
+                (to_timestamp({{ execution_date.timestamp() }}) at time zone 'Australia/Canberra') 
+                AND (to_timestamp({{ next_execution_date.timestamp() }}) at time zone 'Australia/Canberra') ) ;"""
+
+# language="Shell Script"
+COMMON = dedent("""
+        {% set work_dir = '/g/data/v10/work/s2_nbar_rolling_archive/' + ds  -%}
+        cd {{ work_dir }}
+
+        # echo on and exit on fail
+        set -eu
+
+        # Load the latest stable DEA module
+        module use /g/data/v10/public/modules/modulefiles
+        module load dea
+
+        # Be verbose and echo what we run
+        set -x
+""")
 
 default_args = {
     'owner': 'Damien Ayers',
-    'start_date': datetime(2019, 12, 6),
-    'end_date': datetime(2020, 8, 26),
+    'start_date': datetime(2019, 12, 6, tzinfo=local_tz),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     'email_on_failure': True,
@@ -42,29 +70,31 @@ default_args = {
     'aws_conn_id': 'dea_public_data_upload',
 }
 
-# Loading DAG's end_date from Variable
-var = Variable.get("nci_s2_upload_s3_config", {}, deserialize_json=True)
-if var.get("enddate"):
-    default_args["end_date"] = var.get("enddate")
-
 dag = DAG(
     'nci_s2_upload_s3',
     doc_md=__doc__,
     default_args=default_args,
-    catchup=True,
+    catchup=False,
     schedule_interval='@daily',
     max_active_runs=4,
     default_view='tree',
     tags=['nci', 'sentinel_2'],
 )
 
-
 with dag:
-    WORK_DIR = "/g/data/v10/work/s2_nbar_rolling_archive/{{ ds }}_{{ var.json.nci_s2_upload_s3_config.numdays }}"
-    COMMON = """
-            {% set work_dir = '/g/data/v10/work/s2_nbar_rolling_archive/' 
-            + ds +'_' + var.json.nci_s2_upload_s3_config.numdays -%}
-            """
+    # language=Python
+    PYTHON_SCRIPT = dedent("""
+            from datacube import Datacube
+            dc = Datacube()
+            engine = dc.index._db._engine
+            result = engine.execute('''""" + SQL_QUERY + """''')
+            ids = [id for id, in result]
+            datasets = dc.index.datasets.bulk_get(ids)
+            for dataset in datasets:
+                # return f"s3://dea-public-data/L2/sentinel-2-nbar/S2MSIARD_NBAR/{ds.key_time.strftime('%Y-%m-%d')}/{ds.metadata_doc['tile_id'].replace('L1C', 'ARD')}/ARD-METADATA.yaml"
+                # Don't trust the datetimes that are exposed!
+                print(f"s3://dea-public-data/L2/sentinel-2-nbar/S2MSIARD_NBAR/{ds.metadata_doc['extent']['center_dt'][:10]}/{ds.metadata_doc['tile_id'].replace('L1C', 'ARD')}/ARD-METADATA.yaml")
+    """)
     # Uploading s2_to_s3_rolling.py script to NCI
     sftp_s2_to_s3_script = SFTPOperator(
         task_id="sftp_s2_to_s3_script",
@@ -75,23 +105,28 @@ with dag:
         operation=SFTPOperation.PUT,
         create_intermediate_dirs=True
     )
+    # language="Shell Script"
+    generate_list = SSHOperator(
+        task_id='generate_list_of_s2_to_upload',
+        # language="Shell Script"
+        command=COMMON + dedent(f"""
+        
+            python - <<EOF > s3_paths_list.txt
+            {PYTHON_SCRIPT}
+            EOF
+        
+        """),
+        remote_host='gadi-dm.nci.org.au',
+        timeout=60 * 10
+    )
+
     # Execute script to upload sentinel-2 data to s3 bucket
     aws_hook = AwsHook(aws_conn_id=dag.default_args['aws_conn_id'])
     execute_s2_to_s3_script = SSHOperator(
         task_id='execute_s2_to_s3_script',
+        # language="Shell Script"
         command=dedent(COMMON + """
             {% set aws_creds = params.aws_hook.get_credentials() -%}
-            cd {{ work_dir }}
-
-            # echo on and exit on fail
-            set -eu
-
-            # Load the latest stable DEA module
-            module use /g/data/v10/public/modules/modulefiles
-            module load dea
-
-            # Be verbose and echo what we run
-            set -x
 
             # Export AWS Access key/secret from Airflow connection module
             export AWS_ACCESS_KEY_ID={{aws_creds.access_key}}
@@ -107,14 +142,4 @@ with dag:
         params={'aws_hook': aws_hook},
         timeout=60 * 10
     )
-    # Deletes working folder and uploaded script file
-    clean_nci_work_dir = SSHOperator(
-        task_id='clean_nci_work_dir',
-        # Remove work dir after aws s3 sync
-        command=dedent(COMMON + """
-            set -eux
-            rm -vrf "{{ work_dir }}"
-        """)
-    )
-    sftp_s2_to_s3_script >> execute_s2_to_s3_script
-    execute_s2_to_s3_script >> clean_nci_work_dir
+    generate_list >> execute_s2_to_s3_script
