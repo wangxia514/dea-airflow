eebe8c7cbd362caf103a18944f64f1a25e743128
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-11-16 07:31:19 +0000

    Add resources

--- dags/sentinel_2_nrt/sentinel_2_nrt_streamline_indexing.py
+++ dags/sentinel_2_nrt/sentinel_2_nrt_streamline_indexing.py
@@ -87,4 +87,10 @@ with dag:
         task_id="streamline-indexing-task",
         get_logs=True,
         is_delete_operator_pod=True,
+        resources={
+            "request_cpu": "250m",
+            "request_memory": "32Mi",
+            "limit_cpu": "500m",
+            "limit_memory": "64Mi",
+        },
     )
