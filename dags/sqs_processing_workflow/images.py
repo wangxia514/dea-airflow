ef215e65aa354c5f6338f590924f46facb7dd899
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-12-17 05:30:42 +0000

    update image

--- dags/sqs_processing_workflow/images.py
+++ dags/sqs_processing_workflow/images.py
@@ -3,10 +3,12 @@
 """
 
 # IMAGES USED FOR THIS DAG
-INDEXER_IMAGE = "opendatacube/datacube-index:0.0.14"
+INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/datacube-index:0.0.14"
 
-OWS_IMAGE = "opendatacube/ows:1.8.2"
-OWS_CONFIG_IMAGE = "geoscienceaustralia/dea-datakube-config:1.5.5"
+OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:1.8.2"
+OWS_CONFIG_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-datakube-config:1.5.5"
 
 
-EXPLORER_IMAGE = "opendatacube/explorer:2.2.1"
+EXPLORER_IMAGE = (
+    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/explorer:2.2.1"
+)
