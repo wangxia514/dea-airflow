85fb28ac4533ef4d34982739cb87abb727cb2cbd
Damien Ayers <damien@omad.net>
2021-01-22 12:41:28 +1100

    C3 download derivs

--- dags/nci_c3_download_derivs.py
+++ dags/nci_c3_download_derivs.py
@@ -12,7 +12,7 @@ from airflow.contrib.operators.ssh_operator import SSHOperator
 
 dag = DAG(
     'nci_c3_download_derivs',
-    mod_md=__doc__,
+    doc_md=__doc__,
     catchup=False,
     tags=['nci', 'landsat_c3'],
     default_view='tree',
@@ -26,14 +26,36 @@ with dag:
     #
     # 2. Save inventory from Athena/raw. Save NCI files from DB. Compare
 
+    setup = SSHOperator(task_id='setup',
+                        ssh_conn_id='lpgs_gadi',
+                        do_xcom_push=False,
+                        command=dedent("""
+                        mkdir -p /g/data/v10/work/c3_download_derivs/{{ ts_nodash }}
+
+                        """))
+
     # Sync WOfS directory
-    sync_wofs = SSHOperator()
+    sync_wofs = SSHOperator(task_id="sync_wofs",
+                            ssh_conn_id="lpgs_gadi",
+                            remote_host="gadi-dm.nci.org.au",
+                            do_xcom_push=False,
+                            command=dedent("""
+cd /g/data/jw04/ga/ga_ls_wo_3
+time ~/bin/s5cmd --stat cp --if-size-differ 's3://dea-public-data/derivative/ga_ls_wo_3/*' ." > /g/data/v10/work/c3_download_derivs/{{ts_nodash}}/ga_ls_wo_3.download.log
+    """))
 
     # Sync FC directory
-    sync_fc = SSHOperator()
+    sync_fc = SSHOperator(task_id="sync_fc",
+                            ssh_conn_id="lpgs_gadi",
+                            remote_host="gadi-dm.nci.org.au",
+                            do_xcom_push=False,
+                            command="""""")
 
 
     index_wofs = SSHOperator()
 
 
     index_fc = SSHOperator()
+
+    sync_wofs >> index_wofs
+    sync_fc >> index_fc
