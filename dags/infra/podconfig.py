9243fd41eac7cf272321b1794af20a9ba7ee9aa3
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-03-02 00:16:38 +0000

    initial commit

--- dags/infra/podconfig.py
+++ dags/infra/podconfig.py
@@ -21,7 +21,9 @@ NODE_AFFINITY = {
 }
 
 # OWS pod specific configuration
-OWS_CFG_PATH = "/env/config/ows_cfg.py"
+OWS_CFG_PATH = "/env/config/ows_refactored/ows_root_cfg.py"
 OWS_CFG_MOUNT_PATH = "/env/config"
 OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
-OWS_CFG_IMAGEPATH = "/opt/dea-config/dev/services/wms/ows/ows_cfg.py"
+OWS_CFG_IMAGEPATH = "/opt/dea-config/dev/services/wms/ows_refactored"
+OWS_PYTHON_PATH = "/env/config"
+OWS_DATACUBE_CFG = "config.ows_cfg.ows_cfg"
