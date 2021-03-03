8b31b39bd8410ea8d6faab8be67958a94a535c97
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-03-03 14:39:50 +1100

    NCI db sync - updated s3-to-rds to test sync

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -71,7 +71,7 @@ DEFAULT_ARGS = {
 }
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
-S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.2"
+S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.3-unstable.4.g7ddd67d"
 
 dag = DAG(
     "k8s_nci_db_incremental_sync",
