e70b252f59b0dbbbe30cfb9f3b509dd18cfb3583
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-03-03 15:22:20 +1100

    nci db sync - pull image from DockerHub for quick test

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -71,7 +71,8 @@ DEFAULT_ARGS = {
 }
 
 # Point to Geoscience Australia / OpenDataCube Dockerhub
-S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.3-unstable.4.g7ddd67d"
+S3_TO_RDS_IMAGE = "geoscienceaustralia/s3-to-rds:0.1.3-unstable.4.g7ddd67d"
+# S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.3-unstable.4.g7ddd67d"
 
 dag = DAG(
     "k8s_nci_db_incremental_sync",
