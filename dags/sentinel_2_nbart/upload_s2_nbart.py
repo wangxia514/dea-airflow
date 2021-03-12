e117980a35273e3dc621137dc26ba6ec8fdec587
kieranricardo <kieranricardo@hotmail.com>
2021-03-12 11:40:53 +1100

    add whitespace to command

--- dags/sentinel_2_nbart/upload_s2_nbart.py
+++ dags/sentinel_2_nbart/upload_s2_nbart.py
@@ -133,7 +133,7 @@ def upload_dataset_without_yaml(granule_id):
               "--only-show-errors " \
               "--delete " \
               "--exclude NBAR/* " \
-              "--exclude ARD-METADATA.yaml" \
+              "--exclude ARD-METADATA.yaml " \
               "--exclude *NBAR_CONTIGUITY.TIF"
 
     try:
