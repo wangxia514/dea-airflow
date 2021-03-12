d58dbfcefea8258f705cf9a97890f4fb95921cff
Kieran Ricardo <kieranricardo@hotmail.com>
2021-03-12 16:17:24 +1100

    DAG for bulk indexing of ls c3 (#221)
    This PR adds a DAG that performs bulk indexing of landsat C3 dataset into the sandbox odc database.
    
    * created dag for bulk indexing of ls c3
    
    * change dag owner
    
    * update README + change DAG name

--- dags/collection3/README.md
+++ dags/collection3/README.md
@@ -1,20 +1,28 @@
 # Collection 3 indexing
-This is for odc database, the dags for another database is managed seperately.
+This folder contains DAGs for indexing Collection 3 data into the `odc` database in the dea-sandbox-eks cluster. The dags for indexing into the `sandbox` database are managed seperately.
 
-## SQS Queues
-1 x index queue for baseline products
-1 x archive queue for baseline products
-1 x queue each for derivative products
+## Products
 
-## Iam Role
-1 x Iam role
+Currently there are DAGs for indexing the following products:
 
-## products
-### baseline
+#### Baseline
 - `ga_ls5t_ard_3`
 - `ga_ls7e_ard_3`
 - `ga_ls8c_ard_3`
 
-### derivative
+#### Derivative
 - `ga_ls_wo_3`
 - `ga_ls_fc_3`
+
+## DAGs
+
+There are currently 4 DAGs in total: 2 for indexing the baseline products and 2 for indexing the derivative products. The `k8s_index_*_odc` DAGs index from SQS queues, and the `k8s_index_*_backlog_odc` DAGs performing bulk indexing directly from S3.
+
+## AWS Setup
+
+Currently there is 1 IAM role used for the DAGs in this folder. The following SQS queues are used:
+
+- 1 x index queue for baseline products
+- 1 x archive queue for baseline products
+- 1 x queue each for derivative products
+
---
+++ dags/collection3/k8s_index_ls_c3_backlog_odc.py
@@ -0,0 +1,111 @@
+"""
+# Landsat Collection-3 indexing automation for odc db
+
+DAG to periodically index/archive Landsat Collection-3 data.
+
+This DAG uses k8s executors and in cluster with relevant tooling
+and configuration installed.
+
+"""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.secret import Secret
+from airflow.operators.subdag_operator import SubDagOperator
+from infra.variables import (
+    DB_DATABASE,
+    DB_HOSTNAME,
+    SECRET_ODC_WRITER_NAME,
+)
+from infra.images import INDEXER_IMAGE
+
+
+DEFAULT_ARGS = {
+    "owner": "KieranRIcardo",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 6, 14),
+    "email": ["kieran.ricardo@ga.gov.au"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "env_vars": {
+        "DB_HOSTNAME": DB_HOSTNAME,
+        "DB_DATABASE": DB_DATABASE,
+    },
+    "secrets": [
+        Secret(
+            "env",
+            "DB_USERNAME",
+            SECRET_ODC_WRITER_NAME,
+            "postgres-username",
+        ),
+        Secret(
+            "env",
+            "DB_PASSWORD",
+            SECRET_ODC_WRITER_NAME,
+            "postgres-password",
+        ),
+    ],
+}
+TASK_ARGS = {
+    "secrets": DEFAULT_ARGS["secrets"],
+    "start_date": DEFAULT_ARGS["start_date"],
+}
+
+
+def load_subdag(parent_dag_name, child_dag_name, product, rows, args):
+    """
+    Make us a subdag to hide all the sub tasks
+    """
+    subdag = DAG(
+        dag_id=f"{parent_dag_name}.{child_dag_name}", default_args=args, catchup=False
+    )
+
+    with subdag:
+        for row in rows:
+            # for path in paths:
+            INDEXING = KubernetesPodOperator(
+                namespace="processing",
+                image=INDEXER_IMAGE,
+                image_pull_policy="Always",
+                arguments=[
+                    "s3-to-dc",
+                    "--stac",
+                    "--no-sign-request",
+                    f"s3://dea-public-data/baseline/{product}/{row}/**/*.json",
+                    " ".join(products),
+                ],
+                labels={"backlog": "s3-to-dc"},
+                name="datacube-index",
+                task_id=f"{product}--Backlog-indexing-row--{row}",
+                get_logs=True,
+                is_delete_operator_pod=True,
+                dag=subdag,
+            )
+    return subdag
+
+
+DAG_NAME = "k8s_index_ls_c3_backlog_odc"
+
+dag = DAG(
+    dag_id=DAG_NAME,
+    default_args=DEFAULT_ARGS,
+    schedule_interval="@once",
+    tags=["k8s", "landsat_c3", "backlog"],
+    catchup=False,
+)
+
+with dag:
+    rows = range(88, 117)
+    products = ["ga_ls5t_ard_3", "ga_ls7e_ard_3","ga_ls8c_ard_3"]
+
+    for product in products:
+        TASK_NAME = f"{product}--backlog"
+        index_backlog = SubDagOperator(
+            task_id=TASK_NAME,
+            subdag=load_subdag(DAG_NAME, TASK_NAME, product, rows, TASK_ARGS),
+            default_args=TASK_ARGS,
+            dag=dag,
+        )
