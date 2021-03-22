b517523f2eaa4a861316127a152cfed37b58b5ce
Imam Alam <imam.alam@gmail.com>
2021-03-22 02:46:56 +0000

    wagl_nrt: tolerations

--- dags/k8s_wagl_nrt_ancillary.py
+++ dags/k8s_wagl_nrt_ancillary.py
@@ -122,6 +122,10 @@ affinity = {
 }
 
 
+tolerations = [
+    {"key": "dedicated", "operator": "Equal", "value": "wagl", "effect": "NoSchedule"}
+]
+
 default_args = {
     "owner": "Imam Alam",
     "depends_on_past": False,
@@ -175,6 +179,7 @@ with pipeline:
         get_logs=True,
         startup_timeout_seconds=300,
         affinity=affinity,
+        tolerations=tolerations,
         volumes=[ancillary_volume],
         volume_mounts=[ancillary_volume_mount],
         labels={
