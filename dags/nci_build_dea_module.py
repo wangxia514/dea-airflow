dca6608211308b4eeeedc0d1928ae0e1343706ca
kieranricardo <kieranricardo@hotmail.com>
2021-03-25 11:40:15 +1100

    use shell timezone

--- dags/nci_build_dea_module.py
+++ dags/nci_build_dea_module.py
@@ -5,7 +5,6 @@
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from datetime import datetime, timedelta
-import pytz
 
 default_args = {
     'owner': 'Damien Ayers',
@@ -23,14 +22,12 @@ dag = DAG(
     tags=['nci'],
 )
 
-timezone = pytz.timezone("Australia/Canberra")
-
 with dag:
     build_env_task = SSHOperator(
         task_id=f'build_dea_module',
         ssh_conn_id='lpgs_gadi',
         command=f"""
-        rm -r /g/data/v10/public/modules/dea/{datetime.datetime.now(datetime.datetime.now(timezone)).strftime("%Y%m%d")}
+        rm -r /g/data/v10/public/modules/dea/$(date +%Y%m%d)
         set -eux
         cd ~/dea-orchestration/
         git reset --hard
