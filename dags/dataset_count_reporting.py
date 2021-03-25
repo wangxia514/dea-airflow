1fee5dc212c083cad46dc2b8fc8c13c5ff1c5c54
Damien Ayers <damien@omad.net>
2021-03-25 13:06:48 +1100

    More querying

--- dags/dataset_count_reporting.py
+++ dags/dataset_count_reporting.py
@@ -21,27 +21,22 @@ Decisions:
 - From there, insert into Dashboard Postgres
 """
 import logging
-import requests
-from airflow.hooks.base_hook import BaseHook
-from airflow.hooks.dbapi_hook import DbApiHook
-from airflow.hooks.postgres_hook import PostgresHook
-from datetime import timedelta
-
 import psycopg2
+import requests
 from airflow import DAG
-from airflow import secrets
+from airflow.contrib.hooks.aws_athena_hook import AWSAthenaHook
 from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.contrib.operators.ssh_operator import SSHOperator
-from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 
 default_args = {
     'owner': 'Damien Ayers',
     'start_date': days_ago(2),
     'retry_delay': timedelta(minutes=5)
 }
-
 
 LOG = logging.getLogger(__name__)
 
@@ -100,6 +95,7 @@ dag = DAG(
     schedule_interval=timedelta(days=1)
 )
 
+
 def retrieve_explorer_counts(explorer_storage_csv_url='https://explorer.dea.ga.gov.au/audit/storage.csv'):
     with requests.get(explorer_storage_csv_url, stream=True) as r:
         lines = (line.decode('utf-8') for line in r.iter_lines())
@@ -110,36 +106,45 @@ def retrieve_explorer_counts(explorer_storage_csv_url='https://explorer.dea.ga.g
             for product, count, *rest in csv.reader(lines)
         }
     return product_counts
+
+
 s
+
+
 class SQLTemplatedPythonOperator(PythonOperator):
     template_ext = ('.sql',)
 
 
-def my_func(**context):
-    context['templates_dict']['query']
+def record_pg_datasets_count(ssh_conn_id, postgres_conn_id, templates_dict, *args, **kwargs):
+    ssh_hook = SSHHook(ssh_conn_id=ssh_conn_id)
+    src_db_hook = PostgresSSHHook(ssh_hook=ssh_hook,
+                                  postgres_conn_id=postgres_conn_id)
+
+    records = src_db_hook.get_records(templates_dict['query'])
+
+    dest_db_hook = PostgresHook(postgres_conn_id='aws-db')
+    dest_db_hook.insert_rows(table='dataset_counts', rows=records)
 
 
-def count_pg_datasets(templates_dict, *args, **kwargs):
-
-    ssh_hook = SSHHook(ssh_conn_id='lpgs_gadi')
-    dbhook = PostgresSSHHook(ssh_hook=ssh_hook, postgres_conn_id='nci-dea-db')
-
+def record_lustre_datasets_count(ssh_conn_id, postgres_conn_id):
+    ssh_hook = SSHHook(ssh_conn_id)
+    conn = ssh_hook.get_conn()
+    stdin, stdout, stderr = conn.exec_command('fd -e odc-metadata.yaml /g/data/xu18/ga/ga_ls8c_ard_3 | wc -l')
 
 with dag:
+    # Count number of dataset files on NCI
     t1 = SSHOperator(
         task_id="task1",
-        command='qstat -x -f -F json',
+        command='fd -e odc-metadata.yaml /g/data/xu18/ga/ga_ls8c_ard_3 | wc -l',
         ssh_conn_id='lpgs_gadi')
 
-    example_bash_task = BashOperator(
-        task_id='example_bash_task',
-        bash_command='echo {{ task_instance.xcom_pull(task_ids="get_files") }}',
-    )
-
     SQLTemplatedPythonOperator(
-        templates_dict={'query': 'my_template.sql'},
-        params={'my_var': 'my_value'},
-        python_callable=count_pg_datasets,
+        templates_dict={'query': 'sql/datasets_report.sql'},
+        params={
+            'ssh_conn_id': 'lpgs_gadi',
+            'pg_hook_id': 'nci_dea_prod'
+        },
+        python_callable=record_pg_datasets_count,
         provide_context=True,
     )
     put_into_postgres = PythonOperator(
@@ -147,3 +152,8 @@ with dag:
         python_callable=my_python_callable
     )
 
+    AWSAthenaHook(
+        task_id='count_s3_datasets',
+        aws_conn_id='aws_default',
+
+    )
--- dags/sql/datasets_report.sql
+++ dags/sql/datasets_report.sql
@@ -3,5 +3,13 @@ SELECT product.name, count(*), max(d.added)
 FROM agdc.dataset d
 LEFT JOIN agdc.dataset_type product on d.dataset_type_ref = product.id
 WHERE d.archived is null
-and product.name in ('ga_ls5t_ard_3', 'ga_ls7e_ard_3', 'ga_ls8c_ard_3', 'ga_ls_fc_3', 'ga_ls_wo_3')
-group by product.name;
+-- and product.name in ('ga_ls5t_ard_3', 'ga_ls7e_ard_3', 'ga_ls8c_ard_3', 'ga_ls_fc_3', 'ga_ls_wo_3')
+and product.name like '%_3'
+group by product.name
+UNION
+select 'ga_all_ard_3', count(*), max(d.added)
+from agdc.dataset d
+left join agdc.dataset_type product on d.dataset_type_ref = product.id
+where d.archived is null
+and product.name like '%ard_3'
+;
