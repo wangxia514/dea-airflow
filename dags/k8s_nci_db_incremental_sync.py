bade66ff0d3488ba6e0ebdea3b66f78a381930cc
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-03-26 15:59:20 +1100

    turn on email notification on failure

--- dags/k8s_nci_db_incremental_sync.py
+++ dags/k8s_nci_db_incremental_sync.py
@@ -48,7 +48,7 @@ DEFAULT_ARGS = {
     "depends_on_past": False,
     "start_date": datetime(2020, 9, 24, tzinfo=local_tz),
     "email": ["nikita.gandhi@ga.gov.au"],
-    "email_on_failure": False,
+    "email_on_failure": True,
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
