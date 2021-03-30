a7f121ebe73bf384a1477873da10b5d4c6eea98d
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-03-30 12:58:49 +1100

    update explorer stable and unstable images to latest revisions

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -6,9 +6,9 @@ INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/
 OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:latest"
 OWS_CONFIG_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-datakube-config:latest"
 
-EXPLORER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/explorer:2.5.0"
+EXPLORER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/explorer:2.5.1"
 
 S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.4"
 
 # UNSTABLE IMAGES
-EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.4.3-91-g46fed4e"
+EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.5.0-3-gd9f5a67"
