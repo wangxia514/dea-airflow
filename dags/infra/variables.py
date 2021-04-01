55d55222b390f0e45e36b3b750cdec02933750cb
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-01 00:49:18 +0000

    add more pool

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -36,6 +36,7 @@ DEA_NEWDATA_PROCESSING_POOL = Variable.get(
     "newdata_indexing_pool", "NewDeaData_indexing_pool"
 )
 C3_INDEXING_POOL = Variable.get("c3_indexing_pool", "c3_indexing_pool")
+NBART_INDEXING_POOL = Variable.get("nbart_indexing_pool", "nbart_indexing_pool")
 
 # NCI db sync
 NCI_DBSYNC_ROLE = Variable.get("nci_dbsync_role", "svc-dea-dev-eks-nci-dbsync")
