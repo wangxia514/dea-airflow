3557bc4d50e898acd61b2ecc35f62c8714b046c3
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-04-16 12:28:19 +1000

    fix import

--- dags/nci_aws/k8s_nci_db_incremental_sync.py
+++ dags/nci_aws/k8s_nci_db_incremental_sync.py
@@ -34,7 +34,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 from infra.podconfig import ONDEMAND_NODE_AFFINITY
 from infra.images import S3_TO_RDS_IMAGE, INDEXER_IMAGE
-from infra.variables import NCI_DBSYNC_ROLE
+from infra.iam_roles import NCI_DBSYNC_ROLE
 
 local_tz = pendulum.timezone("Australia/Canberra")
 
