2db3d9de4be248a28098f5ecc385157e7de922e2
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-21 03:13:19 +0000

    qa

---
+++ dags/infra/connections.py
@@ -0,0 +1,4 @@
+"""
+infra provided connids
+"""
+DB_ODC_READER_CONN = "db_odc_reader"
--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -18,15 +18,15 @@ SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer") #qa
 SECRET_ODC_WRITER_NAME = Variable.get("db_odc_writer_secret", "odc-writer") #qa
 SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba_admin") #qa
 
-# SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin")
-# SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-writer")
+SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin") #qa
+SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_writer_secret", "explorer-nci-writer") #qa
 
 # DB config
 DB_DATABASE = Variable.get("db_database", "odc") #qa
 DB_HOSTNAME = Variable.get("db_hostname", "db-writer") #qa
-# DB_PORT = Variable.get("db_port", "5432")
+DB_PORT = Variable.get("db_port", "5432") #qa
 
-# AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2")
+AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2") #qa
 
 # dea-access
 DEA_ACCESS_RESTO_API_ADMIN_SECRET = Variable.get("dea_access_resto_api_admin_secret", "dea-access-resto-api-admin") #qa
