3bf3cbf899de2bb64dfb75bea6b14146de09c2ef
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-21 03:48:10 +0000

    qa

--- dags/infra/s3_buckets.py
+++ dags/infra/s3_buckets.py
@@ -1,9 +1,10 @@
 """
 #
 # s3_buckets supplied by terraform or global
-#
+Audit check:
+    date: 21/04/2021
 """
 from airflow.models import Variable
 
 # S3 buckets
-DB_DUMP_S3_BUCKET = Variable.get("db_dump_s3_bucketname", "dea-dev-odc-db-dump")
+DB_DUMP_S3_BUCKET = Variable.get("db_dump_s3_bucketname", "dea-dev-odc-db-dump") #qa
--- dags/infra/sqs_queues.py
+++ dags/infra/sqs_queues.py
@@ -1,27 +1,28 @@
 """
 #
 # sqs_queues supplied by terraform
-#
+Audit check:
+    date: 21/04/2021
 """
 from airflow.models import Variable
 
 # AWS SQS
 NEWDEADATA_SQS_QUEUE_NAME = Variable.get(
     "newdeadata_sqs_queue_name_odc_db", "dea-sandbox-eks-ows-dag"
-)
+) #qa
 C3_ARCHIVAL_SQS_QUEUE_NAME = Variable.get(
     "landsat_c3_archival_sqs_queue_name_odc_db",
     "dea-sandbox-eks-landsat-3-archiving-odc-db",
-)
+) #qa
 C3_INDEXING_SQS_QUEUE_NAME = Variable.get(
     "landsat_c3_indexing_sqs_queue_name_odc_db",
     "dea-sandbox-eks-landsat-3-indexing-odc-db",
-)
+) #qa
 C3_FC_SQS_QUEUE_NAME = Variable.get(
     "landsat_c3_fc_indexing_sqs_queue_name_odc_db",
     "dea-sandbox-eks-alchemist-fc-indexing-wo-odc-db",
-)
+) #qa
 C3_WO_SQS_QUEUE_NAME = Variable.get(
     "landsat_c3_wo_indexing_sqs_queue_name_odc_db",
     "dea-sandbox-eks-alchemist-c3-indexing-wo-odc-db",
-)
+) #qa
