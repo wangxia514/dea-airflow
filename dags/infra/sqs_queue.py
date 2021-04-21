ff8a01aa23ae9d53290bfe6363a9452604e5d8b7
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-21 00:56:23 +0000

    add audit check

--- dags/infra/sqs_queue.py
+++ dags/infra/sqs_queue.py
@@ -1,7 +1,9 @@
 """
 # SQS queues required for dags
+Audit check:
+    date: 21/04/2021
 """
 from airflow.models import Variable
 
 # AWS SQS
-SQS_QUEUE_NAME = Variable.get("sqs_queue_name", "dea-dev-eks-ows-dag")
+SQS_QUEUE_NAME = Variable.get("sqs_queue_name", "dea-dev-eks-ows-dag") #qa
--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -2,29 +2,31 @@
 # Environment shared variables read from airflow variable config, provided by infrastracture
 # https://airflow.apache.org/docs/stable/concepts.html?highlight=variable#storing-variables-in-environment-variables
 # Variables set using Environment Variables would not appear in the Airflow UI but you will be able to use it in your DAG file
+Audit check:
+    date: 21/04/2021
 """
 from airflow.models import Variable
 
 # secrets name available in processing namespace
-SECRET_AWS_NAME = Variable.get("processing_user_secret", "indexing-aws-creds-sandbox")
+SECRET_AWS_NAME = Variable.get("processing_user_secret", "indexing-aws-creds-sandbox") #qa
 
 # DB Users / Roles
 SECRET_EXPLORER_WRITER_NAME = Variable.get(
     "db_explorer_writer_secret", "explorer-writer"
-)
-SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer")
-SECRET_ODC_WRITER_NAME = Variable.get("db_odc_writer_secret", "odc-writer")
-SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba_admin")
+)#qa
+SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer") #qa
+SECRET_ODC_WRITER_NAME = Variable.get("db_odc_writer_secret", "odc-writer") #qa
+SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba_admin") #qa
 
-SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin")
-SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-writer")
+# SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin")
+# SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-writer")
 
 # DB config
-DB_DATABASE = Variable.get("db_database", "odc")
-DB_HOSTNAME = Variable.get("db_hostname", "db-writer")
-DB_PORT = Variable.get("db_port", "5432")
+DB_DATABASE = Variable.get("db_database", "odc") #qa
+DB_HOSTNAME = Variable.get("db_hostname", "db-writer") #qa
+# DB_PORT = Variable.get("db_port", "5432")
 
-AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2")
+# AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2")
 
 # dea-access
-DEA_ACCESS_RESTO_API_ADMIN_SECRET = Variable.get("dea_access_resto_api_admin_secret", "dea-access-resto-api-admin")
+DEA_ACCESS_RESTO_API_ADMIN_SECRET = Variable.get("dea_access_resto_api_admin_secret", "dea-access-resto-api-admin") #qa
