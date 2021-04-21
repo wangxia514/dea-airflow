ec9b23bc3dd4ec8a86ab9589f939746f89271260
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-21 04:19:15 +0000

    update

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -8,7 +8,11 @@ Audit check:
 from airflow.models import Variable
 
 # secrets name available in processing namespace
-SECRET_AWS_NAME = Variable.get("processing_user_secret", "indexing-aws-creds-sandbox")
+C3_INDEXING_USER_SECRET = Variable.get("c3_indexing_user_secret", "processing-aws-creds-sandbox") #qa
+C3_ALCHEMIST_SECRET = Variable.get(
+    "alchemist_c3_indexing_user_secret", "alchemist-c3-user-creds"
+) #qa
+
 SECRET_EXPLORER_WRITER_NAME = Variable.get(
     "db_explorer_writer_secret", "explorer-writer"
 ) #qa
@@ -16,15 +20,15 @@ SECRET_OWS_WRITER_NAME = Variable.get("db_ows_writer_secret", "ows-writer") #qa
 SECRET_ODC_WRITER_NAME = Variable.get("db_odc_writer_secret", "odc-writer") #qa
 SECRET_DBA_ADMIN_NAME = Variable.get("db_dba_admin_secret", "dba-admin") #qa
 
-SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin")
-SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_writer_secret", "explorer-nci-writer")
+SECRET_EXPLORER_NCI_ADMIN_NAME = Variable.get("db_explorer_nci_admin_secret", "explorer-nci-admin") #qa
+SECRET_EXPLORER_NCI_WRITER_NAME = Variable.get("db_explorer_nci_writer_secret", "explorer-nci-writer") #qa
 
 # DB config
 DB_DATABASE = Variable.get("db_database", "odc") #qa
 DB_HOSTNAME = Variable.get("db_hostname", "db-writer") #qa
-DB_PORT = Variable.get("db_port", "5432")
+DB_PORT = Variable.get("db_port", "5432") #qa
 
-AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2")
+AWS_DEFAULT_REGION = Variable.get("region", "ap-southeast-2") #qa
 
 # sandbox db info
 DB_SANDBOX_DATABASE = Variable.get("db_sandbox", "sandbox") #qa
