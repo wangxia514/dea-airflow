bb07a73735e43650e6efca6a48c43c1e5afc3fc8
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-21 05:44:04 +0000

    fix typo

--- dags/infra/iam_roles.py
+++ dags/infra/iam_roles.py
@@ -7,7 +7,7 @@ Audit check:
 from airflow.models import Variable
 
 # role config
-INDEXING_ROLE = Variable.get("processing_indexing_role", "dea-dev-eks-orchestration") #qa
+INDEXING_ROLE = Variable.get("processing_indexing_role", "dea-sandbox-eks-orchestration") #qa
 DB_DUMP_S3_ROLE = Variable.get("db_dump_s3_role", "dea-dev-eks-db-dump-to-s3") # qa
 
 # NCI db sync
