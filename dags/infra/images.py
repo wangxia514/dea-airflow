38a7204bac2a81ca64547c0497a3da331e4ccf18
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-22 06:44:57 +0000

    update  ows to use refactored

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -5,8 +5,8 @@
 # IMAGES USED FOR THIS DAG
 INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/datacube-index:0.0.18"
 
-OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:1.8.5"
-OWS_CONFIG_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-datakube-config:1.5.5"
+OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:latest"
+OWS_CONFIG_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-datakube-config:latest"
 
 EXPLORER_IMAGE = (
     "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/explorer:2.5.1"
--- dags/subdags/podconfig.py
+++ dags/subdags/podconfig.py
@@ -1,5 +1,9 @@
+"""
 # OWS pod specific configuration
-OWS_CFG_PATH = "/env/config/ows_cfg.py"
+"""
+OWS_CFG_FOLDER_PATH = "/env/config/ows_refactored"
 OWS_CFG_MOUNT_PATH = "/env/config"
-OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_cfg.py"
-OWS_CFG_IMAGEPATH = "/opt/dea-config/prod/services/wms/ows/ows_cfg.py"
+OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_refactored/ows_root_cfg.py"
+OWS_CFG_IMAGEPATH = "/opt/dea-config/prod/services/wms/ows_refactored"
+OWS_PYTHON_PATH = "/env/config"
+OWS_DATACUBE_CFG = "ows_refactored.ows_root_cfg.ows_cfg"
