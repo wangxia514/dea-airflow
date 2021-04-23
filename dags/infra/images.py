e8b689f475129cf186e3348dc1109fb5e810433a
Imam Alam <imam.alam@gmail.com>
2021-04-23 02:10:00 +0000

    wagl_nrt: wagl docker image

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -14,7 +14,7 @@ EXPLORER_IMAGE = (
 S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.4"
 
 WAGL_IMAGE = (
-    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20210423-1"
+    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20210423-2"
 )
 
 # UNSTABLE IMAGES
