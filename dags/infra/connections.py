ab6761a22f44f868f658c511263e2210b748893d
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-27 06:58:41 +0000

    add terraform provided conn ids

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -2,3 +2,9 @@
 infra provided connids
 """
 DB_ODC_READER_CONN = "db_odc_reader"
+
+# AWS CONN Alignment
+AWS_NCI_DB_BACKUP_CONN = "aws_nci_db_backup_s3"
+AWS_DEA_PUBLIC_DATA_LANDSAT_SYNC_CONN = "aws_dea_public_data_landsat_3_sync"
+AWS_DEA_PUBLIC_DATA_UPLOAD_CONN = "aws_dea_public_data_upload"
+AWS_WAGL_NRT_CONN = "aws_wagl_nrt"
