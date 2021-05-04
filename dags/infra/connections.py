5d2153d64cb52944871b07d19a91e2fa12470c1e
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-05-04 04:04:26 +0000

    add terraform provided conn

--- dags/infra/connections.py
+++ dags/infra/connections.py
@@ -6,8 +6,5 @@ DB_ODC_READER_CONN = "db_odc_reader"  # qa
 AWS_SENTINEL_2_ARD_SYNC_CONN = "sentinel_2_ard_sync_user"  # qa
 
 # AWS CONN Alignment
-# AWS_NCI_DB_BACKUP_CONN = "aws_nci_db_backup_s3"
-AWS_DEA_PUBLIC_DATA_LANDSAT_3_SYNC_CONN = "aws_dea_public_data_landsat_3_sync"
-AWS_DEA_PUBLIC_DATA_UPLOAD_CONN = "aws_dea_public_data_upload"
-AWS_WAGL_NRT_CONN = "aws_wagl_nrt"
-AWS_DEAD_LETTER_QUEUE_CHECKER_CONN = "aws_dead_letter_sqs_queue_checker"
+AWS_WAGL_NRT_CONN = "aws_wagl_nrt"  # qa
+AWS_DEAD_LETTER_QUEUE_CHECKER_CONN = "aws_dead_letter_sqs_queue_checker"  # qa
