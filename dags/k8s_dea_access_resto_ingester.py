3f205b9b6aede1860b49cfeb7e68421b005b9c3e
Ubuntu <ubuntu@ip-10-0-15-207.ap-southeast-2.compute.internal>
2021-05-05 05:07:08 +0000

    Change the STACTORESTO image to the one in ECR

--- dags/k8s_dea_access_resto_ingester.py
+++ dags/k8s_dea_access_resto_ingester.py
@@ -80,7 +80,7 @@ default_args = {
 
 # Docker images
 CURL_SVC_IMAGE = "curlimages/curl:7.71.1"
-STAC2RESTO_IMAGE = "cyrusbt5/resto-stac2resto"
+STAC2RESTO_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/dea-access/stac-to-resto:latest"
 
 # Set kubernetes secret
 SECRET_ENV = Secret(
