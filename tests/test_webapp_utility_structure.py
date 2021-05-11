d4a3a33ab1270e7bb101b2a0e0bfa7cd071566c5
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-05-11 01:42:41 +0000

    update test code

--- tests/test_webapp_utility_structure.py
+++ tests/test_webapp_utility_structure.py
@@ -4,11 +4,17 @@ from utility.utility_ows_update_extent import dag
 
 class testClass(unittest.TestCase):
     def assertDagDictEqual(self, source, dag):
-        assert dag.task_dict.keys() == source.keys()
+        self.assertEqual(dag.task_dict.keys(), source.keys())
         for task_id, downstream_list in source.items():
-            assert dag.has_task(task_id)
+            self.assertTrue(
+                dag.has_task(task_id), msg="Missing task_id: {} in dag".format(task_id)
+            )
             task = dag.get_task(task_id)
-            assert task.downstream_task_ids == set(downstream_list)
+            self.assertEqual(
+                task.downstream_task_ids,
+                set(downstream_list),
+                msg="unexpected downstream link in {}".format(task_id),
+            )
 
     def test_dag(self):
         self.assertDagDictEqual(
