73eb5035f7bf20b2e0f7aff069c3acbdb90277b6
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-05-13 04:32:15 +0000

    fix

--- dags/test_email.py
+++ dags/test_email.py
@@ -8,12 +8,12 @@ from airflow.operators.email_operator import EmailOperator
 
 default_args = {
     "owner": "Pin Jin",
-    "depends_on_past": False,
+    # "depends_on_past": False,
     "start_date": datetime(2020, 6, 14),
-    "email": ["pin.jin@ga.gov.au"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": None,
+    "email": ["nikita.gandhi@ga.gov.au"],
+    # "email_on_failure": False,
+    # "email_on_retry": False,
+    # "retries": 1,
 }
 
 dag = DAG(
@@ -25,10 +25,12 @@ dag = DAG(
 )
 
 email = EmailOperator(
+    mime_charset="utf-8",
     task_id="send_email",
-    to="pin.jin@ga.gov.au",
-    subject="Airflow Alert",
-    html_content=None,
+    to="nikita.gandhi@ga.gov.au",
+    subject="Templated Subject: start_date {{ ds }}",
+    params={"content1": "random"},
+    html_content="Templated Content: content1 - {{ params.content1 }}  task_key - {{ task_instance_key_str }} test_mode - {{ test_mode }} task_owner - {{ task.owner}} hostname - {{ ti.hostname }}",
     dag=dag,
 )
 
