c98212112916b356464f48e38359a9230b1faed4
Imam Alam <imam.alam@gmail.com>
2021-05-25 11:16:04 +0000

    upgrade docker image: aws s3 sync -> aws s3 cp in upload

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -13,9 +13,7 @@ EXPLORER_IMAGE = (
 
 S3_TO_RDS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/s3-to-rds:0.1.4"
 
-WAGL_IMAGE = (
-    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:patch-20210428-1"
-)
+WAGL_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacubepipelines/legacy-nrt:release-20210525"
 
 # UNSTABLE IMAGES
 EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.5.0-3-gd9f5a67"
