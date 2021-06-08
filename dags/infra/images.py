bbe70664bee8909e6de7f4289d5cb36da6f43a7f
Kieran Ricardo <kieranricardo@hotmail.com>
2021-06-08 15:05:46 +1000

    update tag

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -2,7 +2,7 @@
 # IMAGES USED FOR DAGs
 """
 ## STABLE IMAGES
-INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/datacube-index:0.0.20"
+INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/datacube-index:latest"
 
 OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:latest"
 OWS_CONFIG_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-datakube-config:latest"
