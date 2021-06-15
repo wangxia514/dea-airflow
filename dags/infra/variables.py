97a136e07e7fde79b933360a09d39721253753b5
Tom McAdam <mcadam1980@gmail.com>
2021-06-15 14:02:36 +1000

    carsa_reporting: modified copernicus api variable name to obscure

--- dags/infra/variables.py
+++ dags/infra/variables.py
@@ -56,8 +56,8 @@ SENTINEL_2_ARD_INDEXING_AWS_USER_SECRET = Variable.get(
 
 S2_NRT_AWS_CREDS = "wagl-nrt-aws-creds"
 
-COP_API_REP_CREDS = "copernicus_api_creds"
+COP_API_REP_CREDS = "copernicus_api_password"
 
 WATERBODIES_DEV_USER_SECRET = Variable.get(
-    'waterbodies_dev_user_secret', 'waterbodies-dev-user-creds'
+    "waterbodies_dev_user_secret", "waterbodies-dev-user-creds"
 )
