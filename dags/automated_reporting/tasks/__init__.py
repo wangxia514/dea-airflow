be1bca69db07958aa78e33d33d88a5d89113cd14
Tom McAdam <mcadam1980@gmail.com>
2021-07-09 13:05:16 +1000

    acs_reporting: add  pylint skip-file

--- dags/automated_reporting/tasks/__init__.py
+++ dags/automated_reporting/tasks/__init__.py
@@ -1,16 +1,11 @@
+# pylint: skip-file
 """
 Pull all tasks under namespace tasks.####
 """
 from .check_db import task as check_db_task
-from .s2_completeness import (
-    task_ard as s2_completeness_ard_task,
-)  # pylint: disable-msg=E0401
-from .s2_completeness import (
-    task_wo as s2_completeness_wo_task,
-)  # pylint: disable-msg=E0401
+from .s2_completeness import task_ard as s2_completeness_ard_task
+from .s2_completeness import task_wo as s2_completeness_wo_task
 from .simple_latency import task as simple_latency_task
 from .expire_completeness import task as expire_completeness_task
-from .usgs_completeness import (
-    task as usgs_completeness_task,
-)  # pylint: disable-msg=E0401
+from .usgs_completeness import task as usgs_completeness_task
 from .latency_from_completeness import task as latency_from_completeness_task
