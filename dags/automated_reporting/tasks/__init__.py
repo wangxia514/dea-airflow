62b10ec2f4b5b0669587cc515f642a06d9df5198
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-07-14 03:54:09 +0000

    add noqa to multiline imports

--- dags/automated_reporting/tasks/__init__.py
+++ dags/automated_reporting/tasks/__init__.py
@@ -7,6 +7,6 @@ from .s2_completeness import task_wo as s2_completeness_wo_task  # noqa: F401
 from .simple_latency import task as simple_latency_task  # noqa: F401
 from .expire_completeness import task as expire_completeness_task  # noqa: F401
 from .usgs_completeness import task as usgs_completeness_task  # noqa: F401
-from .latency_from_completeness import (
-    task as latency_from_completeness_task,
+from .latency_from_completeness import (  # noqa: F401
+    task as latency_from_completeness_task,  # noqa: F401
 )  # noqa: F401
