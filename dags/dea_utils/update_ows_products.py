d9009cc8bda7704c699e1eb633e401d371f64f18
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-07-15 02:38:34 +0000

    test

--- dags/dea_utils/update_ows_products.py
+++ dags/dea_utils/update_ows_products.py
@@ -84,8 +84,8 @@ def ows_update_operator(products, dag=None):
         "-c",
         dedent(
             f"""
-            # datacube-ows-update --version
-            datacube-ows-update --views
+            datacube-ows-update --version
+            # datacube-ows-update --views
             for product in {products}; do
                 if [ $product == "--all" ]; then
                     datacube-ows-update
