2e257e7beef86f84d88a4fdfcb82b24a1b775fbb
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-07-15 02:43:45 +0000

    fix image path for ows

--- dags/dea_utils/update_ows_products.py
+++ dags/dea_utils/update_ows_products.py
@@ -18,7 +18,7 @@ from webapp_update.update_list import OWS_UPDATE_LIST
 OWS_CFG_FOLDER_PATH = "/env/config/ows_refactored"
 OWS_CFG_MOUNT_PATH = "/env/config"
 OWS_CFG_PATH = OWS_CFG_MOUNT_PATH + "/ows_refactored/ows_root_cfg.py"
-OWS_CFG_IMAGEPATH = "/opt/dea-config/prod/services/wms/ows_refactored"
+OWS_CFG_IMAGEPATH = "/opt/dea-config/dev/services/wms/ows_refactored"
 OWS_PYTHON_PATH = "/env/config"
 OWS_DATACUBE_CFG = "ows_refactored.ows_root_cfg.ows_cfg"
 
