f97315cf91d506c5def6faeb5f59e98d59841e76
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-07-15 02:34:07 +0000

    update ows image

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -4,7 +4,7 @@
 ## STABLE IMAGES
 INDEXER_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/datacube-index:latest"
 
-OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:1.8.11"
+OWS_IMAGE = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/opendatacube/ows:1.8.14"
 OWS_CONFIG_IMAGE = "geoscienceaustralia/dea-datakube-config:latest"  # do not pull from ECR, to avoid delay in ecr sync
 
 EXPLORER_IMAGE = (
