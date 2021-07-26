12b1f9f7b9e9e5d9aaa273e86656d8ad1eaae35f
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-07-26 06:50:27 +0000

    add default_var=

--- dags/infra/sqs_queues.py
+++ dags/infra/sqs_queues.py
@@ -6,10 +6,10 @@ Audit check:
 from airflow.models import Variable
 
 # AWS SQS
-SQS_QUEUE_NAME = Variable.get("sqs_queue_name", "dea-dev-eks-ows-dag")  # qa
+SQS_QUEUE_NAME = Variable.get("sqs_queue_name", default_var="dea-dev-eks-ows-dag")  # qa
 SENTINEL_2_ARD_INDEXING_SQS_QUEUE_NAME_ODC_DB = Variable.get(
     "sentinel_2_ard_indexing_sqs_queue_name_odc_db",
-    "dea-dev-eks-sentinel-2-ard-indexing-odc-db",
+    default_var="dea-dev-eks-sentinel-2-ard-indexing-odc-db",
 )  # qa
 
 
