190683f76b3f61f676bf97b2528c4a64feaeec06
Imam Alam <imam.alam@gmail.com>
2021-08-13 00:21:23 +0000

    ard: publish Landsat SNS

--- dags/infra/sns_notifications.py
+++ dags/infra/sns_notifications.py
@@ -1,2 +1,6 @@
 """ SNS notification services. """
 PUBLISH_S2_NRT_SNS = "arn:aws:sns:ap-southeast-2:451924316694:dea-dev-eks-wagl-s2-nrt"
+
+PUBLISH_ARD_NRT_LS_SNS = (
+    "arn:aws:sns:ap-southeast-2:451924316694:dea-dev-eks-wagl-ls-nrt"
+)
