f6635719d4c2e63a0fbbaff4878868941fc0d209
Damien Ayers <damien@omad.net>
2021-08-19 11:29:38 +1000

    PBSJobOperator: handle ssh args and passing around pbs_job_id

--- plugins/operators/pbs_job_operator.py
+++ plugins/operators/pbs_job_operator.py
@@ -22,51 +22,63 @@ Extra Features
 import json
 from logging import getLogger
 
-from airflow import AirflowException
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.exceptions import AirflowException
+from airflow.sensors.base import BaseSensorOperator
 
 from dea_airflow_common.ssh import SSHRunMixin
 
 log = getLogger(__name__)
 
+XCOM_KEY = "PBS_JOB_ID"
+
 
 # Putting the SSHMixin first, so that it hopefully consumes it's __init__ arguments
 class PBSJobOperator(SSHRunMixin, BaseSensorOperator):
+    """"""
 
-    # TODO, how do we handle work_dir?
-    # - Is it okay to ignore?
-    # - Is it another required argument?
+    template_fields = (
+        "pbs_args",
+        "pbs_command",
+    )
 
-    template_fields = ("pbs_args", "pbs_command",)
-    @apply_defaults
     def __init__(
-            self,
-
-            qsub_args: str,
-            qsub_command: str,
-            work_dir: str,
-            poke_interval: int = 5 * 60,
-            mode="reschedule",
-            timeout: int = 24 * 60 * 60,
-            *args,
-            **kwargs,
-
+        self,
+        qsub_args: str,
+        qsub_command: str,
+        work_dir: str,
+        poke_interval: int = 5 * 60,
+        mode="reschedule",
+        timeout: int = 24 * 60 * 60,
+        ssh_conn_id=None,
+        ssh_hook=None,
+        *args,
+        **kwargs,
     ):
-        super().__init__(
+        BaseSensorOperator.__init__(
+            self,
             mode=mode,
             poke_interval=poke_interval,
             timeout=timeout,
             *args,
-            **kwargs
+            **kwargs,
         )
+
+        self.ssh_hook = ssh_hook
+        self.ssh_conn_id = ssh_conn_id
+
         self.log.info("Inside PBSJobOperator __init__")
         self.qsub_args = qsub_args
         self.qsub_command = qsub_command
         self.work_dir = work_dir
-        self.pbs_job_id = None
 
     def execute(self, context):
-        if self.pbs_job_id:
+        # Because this Operator is a 'reschedule' type Airflow Sensor, the Python class will be
+        # created fresh each time it is attempted. We need a persistent storage location, and Airflow XCom is a
+        # good choice.
+        # TODO: Put pbs_job_id into the context
+        pbs_job_id = self.xcom_pull(context, task_ids=[self.task_id], key=XCOM_KEY)
+        context["pbs_job_id"] = pbs_job_id
+        if pbs_job_id:
             # If we have submitted a job already, delegate to the poke() function via BaseSensorOperator
             # Which handles rescheduling and re-executing as required.
             super().execute(context)
@@ -74,16 +86,17 @@ class PBSJobOperator(SSHRunMixin, BaseSensorOperator):
             # Running for the first time, submit the job
             ret_val, output = self.run_ssh_command_and_return_output(
                 f"""
-          mkdir -p { self.work_dir };
-          cd { self.work_dir };
-          qsub \
-          {self.qsub_args} \
-          -o { self.work_dir } -e { self.work_dir }
-          -- {self.qsub_command}
+                  mkdir -p {self.work_dir};
+                  cd {self.work_dir};
+                  qsub \
+                  {self.qsub_args} \
+                  -o {self.work_dir} -e {self.work_dir}
+                  -- {self.qsub_command}
                 """
             )
             if ret_val == 0:
-                self.pbs_job_id = output.strip()
+                pbs_job_id = output.strip()
+                self.xcom_push(context, key=XCOM_KEY, value=pbs_job_id)
             else:
                 raise AirflowException("Error submitting PBS Job")
 
@@ -91,9 +104,10 @@ class PBSJobOperator(SSHRunMixin, BaseSensorOperator):
         # qstat json output incorrectly attempts to escape single quotes
         # This can be fixed with sed, or within python, sed  "s/\\\'/'/g"
 
+        pbs_job_id = context["pbs_job_id"]
         try:
             ret_val, output = self.run_ssh_command_and_return_output(
-                f"qstat -fx -F json {self.pbs_job_id}"
+                f"qstat -fx -F json {pbs_job_id}"
             )
         except EOFError:
             # Sometimes qstat hangs and doesn't complete it's output. Be accepting of this,
@@ -109,9 +123,9 @@ class PBSJobOperator(SSHRunMixin, BaseSensorOperator):
             self.log.exception("Error parsing qstat output: ", exc_info=e)
             return False
 
-        job_state = result["Jobs"][self.pbs_job_id]["job_state"]
+        job_state = result["Jobs"][pbs_job_id]["job_state"]
         if job_state == "F":
-            pbs_result = result["Jobs"][self.pbs_job_id]
+            pbs_result = result["Jobs"][pbs_job_id]
             exit_status = pbs_result["Exit_status"]
 
             self.xcom_push(context, "return_value", pbs_result)
@@ -124,6 +138,6 @@ class PBSJobOperator(SSHRunMixin, BaseSensorOperator):
                 # https://github.com/apache/airflow/pull/7133 isn't implemented yet.
                 # The only way to /not/ retry is by setting the `task_instance.max_tries = 0`
                 # as seen here: https://gist.github.com/robinedwards/3f2ec4336e1ced084547d24d7e7ead3a
-                raise AirflowException("PBS Job Failed %s", self.pbs_job_id)
+                raise AirflowException("PBS Job Failed %s", pbs_job_id)
         else:
             return False
