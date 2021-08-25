772cfb744fd25e2f78a2c3bbbf1c446e081af99c
Imam Alam <imam.alam@gmail.com>
2021-08-25 07:33:20 +0000

    [ard] update and fix ARD docker image

--- dags/infra/images.py
+++ dags/infra/images.py
@@ -18,7 +18,7 @@ WAGL_IMAGE = (
     "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/dev/wagl:release-20210526a"
 )
 
-WAGL_IMAGE_POC = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-wagl-docker:0.1.10"
+WAGL_IMAGE_POC = "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/geoscienceaustralia/dea-wagl-docker:0.1.11"
 
 # UNSTABLE IMAGES
 EXPLORER_UNSTABLE_IMAGE = "opendatacube/explorer:2.5.0-3-gd9f5a67"
