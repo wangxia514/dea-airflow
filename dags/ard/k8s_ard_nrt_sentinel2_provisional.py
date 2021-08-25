d6ee88e7a18f9022b009c5f7158e0ccca7a4ac35
Imam Alam <imam.alam@gmail.com>
2021-08-25 07:30:48 +0000

    [ard] fix typo

--- dags/ard/k8s_ard_nrt_sentinel2_provisional.py
+++ dags/ard/k8s_ard_nrt_sentinel2_provisional.py
@@ -85,7 +85,7 @@ ancillary_volume = V1Volume(
 )
 
 pipeline = DAG(
-    "k8s_ard_nrt_sentinel2_provisional_alt",
+    "k8s_ard_nrt_sentinel2_provisional",
     doc_md=__doc__,
     default_args=default_args,
     description="DEA Sentinel-2 ARD NRT processing (provisional)",
