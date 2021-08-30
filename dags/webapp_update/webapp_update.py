44d440b5cb4f09e15df510bcfe845d61f0e53263
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-08-30 07:08:50 +0000

    update web app dag docstring

--- dags/webapp_update/webapp_update.py
+++ dags/webapp_update/webapp_update.py
@@ -3,37 +3,11 @@
 This DAG runs every 6 hours to update for fresh index datasets
 The current product list is available from https://github.com/GeoscienceAustralia/dea-airflow/blob/develop/dags/webapp_update/update_list.py
 
-The list as at the time this docstring is created is:
+## OWS UPDATE
+the observed time takes for ows to update is between `1hour` to `2hours`
 
-```python
-    OWS_UPDATE_LIST = (
-        "s2_nrt_granule_nbar_t",
-        "wofs_albers",
-        "fc_albers_combined",
-        "s2_ard_granule_nbar_t",
-        "ga_ls_wo_3",
-        "ga_s2_wo_3",
-        "ga_ls_fc_3",
-        "ga_ls_ard_3",
-    )
-
-    EXPLORER_UPDATE_LIST = (
-        "s2a_nrt_granule",
-        "s2b_nrt_granule",
-        "s2a_ard_granule",
-        "s2b_ard_granule",
-        "wofs_albers",
-        "ls5_fc_albers",
-        "ls7_fc_albers",
-        "ls8_fc_albers",
-        "ga_ls_wo_3",
-        "ga_s2_wo_3",
-        "ga_ls_fc_3",
-        "ga_ls5t_ard_3",
-        "ga_ls7e_ard_3",
-        "ga_ls8c_ard_3",
-    )
-```
+## Explorer update
+the observed time takes for explorer to update is between `10mins` to `30mins`
 """
 
 from datetime import datetime, timedelta
