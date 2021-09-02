29ec17d5cecd3f1dafa13c8b305947888d3e2544
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-09-02 01:04:38 +0000

    fix typo

--- dags/utility/utility_s3_move_copy.py
+++ dags/utility/utility_s3_move_copy.py
@@ -50,13 +50,12 @@ DEFAULT_ARGS = {
     },
 }
 
-S3_COPY_COMMAND = [
-    "{% if dag_run.conf.get('src_bucket_folder') and dag_run.conf.get('dest_bucket_folder') %}cp --acl bucket-owner-full-control '{{ dag_run.conf.src_bucket_folder }}' {{ dag_run.conf.dest_bucket_folder }}{% endif %}"
-]
+# S3_COPY_COMMAND = [
+#     "{% if dag_run.conf.get('src_bucket_folder') and dag_run.conf.get('dest_bucket_folder') %}cp --acl bucket-owner-full-control '{{ dag_run.conf.src_bucket_folder }}' {{ dag_run.conf.dest_bucket_folder }}{% endif %}"
+# ]
 
 S3_CHECK_ARGS = [
-    "ls",
-    "s3://dea-public-data-dev",
+    "ls s3://dea-public-data-dev",
 ]
 
 # THE DAG
@@ -73,8 +72,8 @@ with dag:
     S3_COPY = KubernetesPodOperator(
         namespace="processing",
         image=S5CMD_IMAGE,
-        # cmds=["./s5cmd"],
-        arguments=S3_COPY_COMMAND,
+        cmds=["./s5cmd"],
+        arguments=S3_CHECK_ARGS,
         annotations={"iam.amazonaws.com/role": UTILITY_S3_COPY_MOVE_ROLE},
         labels={"step": "utility-s5cmd-copy"},
         name="s5cmd-copy",
