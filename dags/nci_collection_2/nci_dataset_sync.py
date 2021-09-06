454093c2db0822c4310b6f2b341aecd5993af414
Damien Ayers <damien@omad.net>
2021-09-06 16:17:08 +1000

    Change approach for indexing new C2 data
    This Collection 2 'sync' DAG has been failing regularly since April,
    I've tried various combinations of lengthening walltime, and increasing
    parallelisation, but `dea-sync` in combination with the NCI Lustre
    /g/data filesystems and NCI DEA Postgres Server are not performing well
    enough to complete successfully.
    
    Collection 2 is being decommissioned at the end of the year, so rather
    than trying to fix this 'properly', lets try indexing all datasets
    created in the last 30 days, each time the DAG runs, using GNU find, awk
    and xargs with `datacube dataset add`.

--- dags/nci_collection_2/nci_dataset_sync.py
+++ dags/nci_collection_2/nci_dataset_sync.py
@@ -11,60 +11,25 @@ from textwrap import dedent
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.operators.dummy import DummyOperator
-from sensors.pbs_job_complete_sensor import PBSJobSensor
 from nci_collection_2.nci_common import c2_default_args, c2_schedule_interval, MINUTES
 
-SYNCED_PRODUCTS = [
-    "ls8_nbar_scene",
-    "ls8_nbart_scene",
-    "ls8_pq_scene",
-    "ls8_pq_legacy_scene",
-    "ls7_nbar_scene",
-    "ls7_nbart_scene",
-    "ls7_pq_scene",
-    "ls7_pq_legacy_scene",
-]
 
-SYNC_PREFIX_PATH = {
-    "ls8_nbar_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls8/",
-    "ls7_nbar_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls7/",
-    "ls8_nbart_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls8/",
-    "ls7_nbart_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls7/",
-    "ls8_pq_scene": "/g/data/rs0/scenes/pq-scenes-tmp/ls8/",
-    "ls7_pq_scene": "/g/data/rs0/scenes/pq-scenes-tmp/ls7/",
-    "ls8_pq_legacy_scene": "/g/data/rs0/scenes/pq-legacy-scenes-tmp/ls8/",
-    "ls7_pq_legacy_scene": "/g/data/rs0/scenes/pq-legacy-scenes-tmp/ls7/",
-}
-
-SYNC_SUFFIX_PATH = {
-    "ls8_nbar_scene": "/??/output/nbar/",
-    "ls7_nbar_scene": "/??/output/nbar/",
-    "ls8_nbart_scene": "/??/output/nbart/",
-    "ls7_nbart_scene": "/??/output/nbart/",
-    "ls8_pq_scene": "/??/output/pqa/",
-    "ls7_pq_scene": "/??/output/pqa/",
-    "ls8_pq_legacy_scene": "/??/output/pqa/",
-    "ls7_pq_legacy_scene": "/??/output/pqa/",
+SYNC_PATHS = {
+    "ls8_nbar_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls8/2021/??/output/nbar/",
+    "ls7_nbar_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls7/2021/??/output/nbar/",
+    "ls8_nbart_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls8/2021/??/output/nbart/",
+    "ls7_nbart_scene": "/g/data/rs0/scenes/nbar-scenes-tmp/ls7/2021/??/output/nbart/",
+    "ls8_pq_scene": "/g/data/rs0/scenes/pq-scenes-tmp/ls8/2021/??/output/pqa/",
+    "ls7_pq_scene": "/g/data/rs0/scenes/pq-scenes-tmp/ls7/2021/??/output/pqa/",
+    "ls8_pq_legacy_scene": "/g/data/rs0/scenes/pq-legacy-scenes-tmp/ls8/2021/??/output/pqa/",
+    "ls7_pq_legacy_scene": "/g/data/rs0/scenes/pq-legacy-scenes-tmp/ls7/2021/??/output/pqa/",
 }
 
 SYNC_COMMAND = dedent(
     """
-  {% set work_dir = '/g/data/v10/work/sync/' + params.product + '/' + ds -%}
-  {% set sync_cache_dir = work_dir + '/cache' -%}
-  {% set sync_path = params.sync_prefix_path + params.year + params.sync_suffix_path -%}
-
-  mkdir -p {{ sync_cache_dir }};
-  qsub -N sync_{{ params.product}}_{{ params.year }} \
-  -q {{ params.queue }} \
-  -W umask=33 \
-  -l wd,walltime=10:00:00,mem=12GB,ncpus=4 -m abe \
-  -l storage=gdata/v10+gdata/fk4+gdata/rs0+gdata/if87 \
-  -P {{ params.project }} -o {{ work_dir }} -e {{ work_dir }} \
-  -- /bin/bash -l -c \
-      "source $HOME/.bashrc; \
       module use /g/data/v10/public/modules/modulefiles/; \
       module load {{ params.module }}; \
-      dea-sync -vvv --cache-folder {{sync_cache_dir}} -j 12 --update-locations --index-missing {{ sync_path }}"
+      find {{ sync_path }} -maxdepth 1 -mindepth 1 -newermt '30 days ago' | awk '{ print $1 "/ga-metadata.yaml"}' | xargs -j 4 datacube dataset add
 """
 )
 
@@ -79,29 +44,21 @@ with DAG(
     default_view="tree",
 ) as dag:
     START = DummyOperator(task_id="start")
-    for product in SYNCED_PRODUCTS:
+    for product, sync_path in SYNC_PATHS.items():
         submit_sync = SSHOperator(
             task_id=f"submit_sync_{product}",
             command=SYNC_COMMAND,
             params={
                 "product": product,
-                "sync_prefix_path": SYNC_PREFIX_PATH[product],
-                "sync_suffix_path": SYNC_SUFFIX_PATH[product],
-                "queue": "normal",
+                "sync_path": sync_path,
             },
-            do_xcom_push=True,
-            timeout=5 * MINUTES,  # For running SSH Commands
-            weight_rule="upstream",  # Prefer completing downstream tasks before upstream tasks. We want to wait for
-            # PBS Jobs to complete before scheduling more. They tend to timeout when running
-            # too many at once.
-            pool="nci_c2_sync",
+            timeout=120 * MINUTES,  # For running SSH Commands
+            remote_host="gadi-dm.nci.org.au",
         )
 
-        wait_for_completion = PBSJobSensor(
-            task_id=f"wait_for_{product}",
-            pbs_job_id="{{ ti.xcom_pull(task_ids='submit_sync_%s') }}" % product,
-            weight_rule="upstream",
-            pool="nci_c2_sync",
-        )
+        # Keep this task_id around to not break the DAG history
+        # Normally we would rename the DAG for a structure change, but, I don't want to
+        # have to change the DAG which depends on this one by name.
+        wait_for_completion = DummyOperator(task_id=f"wait_for_{product}")
 
         START >> submit_sync >> wait_for_completion
