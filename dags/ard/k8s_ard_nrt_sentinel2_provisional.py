254f5e41789bdb53b66b9825f04831175f0ba614
Imam Alam <imam.alam@gmail.com>
2021-09-06 01:43:18 +0000

    [ard] turn off scheduling for ARD dev DAGs

--- dags/ard/k8s_ard_nrt_sentinel2_provisional.py
+++ dags/ard/k8s_ard_nrt_sentinel2_provisional.py
@@ -92,7 +92,7 @@ pipeline = DAG(
     max_active_runs=MAX_ACTIVE_RUNS,
     catchup=False,
     params={},
-    schedule_interval=timedelta(minutes=1),
+    schedule_interval=None,
     tags=["k8s", "dea", "psc", "ard", "wagl", "nrt", "sentinel-2", "provisional"],
 )
 
