b0411fc6d02e776c187db7d928e62edd5f99ee39
Matthew Alger <Matthew.Alger@ga.gov.au>
2021-09-28 10:21:20 +1000

    Waterbodies conflux ready for NSW

--- dags/k8s_waterbodies_conflux.py
+++ dags/k8s_waterbodies_conflux.py
@@ -160,6 +160,7 @@ def k8s_job_task(dag):
                                 dedent(
                                     """
                                     echo Default region $AWS_DEFAULT_REGION
+                                    ip addr show
                                     dea-conflux run-from-queue -v \
                                             --plugin examples/waterbodies.conflux.py \
                                             --queue {queue} \
