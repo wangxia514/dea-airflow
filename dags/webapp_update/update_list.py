d047d169de59316b9429108ec767bd7ac94cb5f0
Erin Telfer <Erin.Telfer@ga.gov.au>
2021-10-08 11:34:30 +1300

    add ba to ows and explorer lists

--- dags/webapp_update/update_list.py
+++ dags/webapp_update/update_list.py
@@ -15,6 +15,7 @@ OWS_UPDATE_LIST = (
     "ga_ls_ard_3",
     "ga_ls_ard_provisional_3",
     "s2_nrt_provisional_granule_nbar_t",
+    "ga_s2_ba_provisional_3",
 )
 
 # Default Datacube Explorer Products to Update
@@ -35,4 +36,5 @@ EXPLORER_UPDATE_LIST = (
     "ga_ls8c_ard_provisional_3",
     "ga_s2am_ard_provisional_3",
     "ga_s2bm_ard_provisional_3",
+    "ga_s2_ba_provisional_3",
 )
