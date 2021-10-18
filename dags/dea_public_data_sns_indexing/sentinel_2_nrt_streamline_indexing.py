5b5fcf62468dc3b02bcb3cbeb9545a32f90f5321
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-18 01:47:19 +0000

    update dag name

--- dags/dea_public_data_sns_indexing/sentinel_2_nrt_streamline_indexing.py
+++ dags/dea_public_data_sns_indexing/sentinel_2_nrt_streamline_indexing.py
@@ -68,7 +68,7 @@ INDEXING_BASH_COMMAND = [
 
 # THE DAG
 dag = DAG(
-    "sentinel_2_nrt_streamline_indexing",
+    "dea_public_data_sns_streamline_indexing",
     doc_md=__doc__,
     default_args=DEFAULT_ARGS,
     schedule_interval="0 */1 * * *",  # hourly
