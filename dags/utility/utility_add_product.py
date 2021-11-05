042fc631bb8a32af344840c49646db1a5c6ca7e4
David Gavin <GypsyBojangles@users.noreply.github.com>
2021-11-06 08:20:21 +1100

    Change order of flags

--- dags/utility/utility_add_product.py
+++ dags/utility/utility_add_product.py
@@ -182,10 +182,10 @@ with dag:
             # "s3://dea-public-data/cemp_insar/insar/displacement/alos//**/*.yaml",
             # "cemp_insar_alos_displacement",
             # Jinja templates for arguments
-            "{{ dag_run.conf.s3_glob }}",
-            "{{ dag_run.conf.product_name }}",
             "--no-sign-request",
             "{% if dag_run.conf.get('skip_lineage') %} --skip_lineage {% endif %}",
+            "{{ dag_run.conf.s3_glob }}",
+            "{{ dag_run.conf.product_name }}",            
         ],
         name="datacube-index",
         task_id=INDEXING_TASK_ID,
