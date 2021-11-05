8007a86befb50d41b75a5db818da4bcd0ebde5e3
David Gavin <GypsyBojangles@users.noreply.github.com>
2021-11-06 08:23:52 +1100

    correct flag for no sign request

--- dags/utility/utility_add_product.py
+++ dags/utility/utility_add_product.py
@@ -182,7 +182,7 @@ with dag:
             # "s3://dea-public-data/cemp_insar/insar/displacement/alos//**/*.yaml",
             # "cemp_insar_alos_displacement",
             # Jinja templates for arguments
-            "--no-sign-request",
+            "--no_sign_request",
             "{% if dag_run.conf.get('skip_lineage') %} --skip_lineage {% endif %}",
             "{{ dag_run.conf.s3_glob }}",
             "{{ dag_run.conf.product_name }}",            
