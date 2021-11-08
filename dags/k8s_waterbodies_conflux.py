85daa7330d296fa2cee9548ef6a2da2658ab7d8c
Sai Ma <Sai.Ma@ga.gov.au>
2021-11-08 14:23:57 +1100

    Update the parallelism to 256

--- dags/k8s_waterbodies_conflux.py
+++ dags/k8s_waterbodies_conflux.py
@@ -136,7 +136,7 @@ def k8s_job_task(dag):
     mem = CONFLUX_POD_MEMORY_MB
     req_mem = "{}Mi".format(int(mem))
     lim_mem = "{}Mi".format(int(mem) * 2)
-    parallelism = 64
+    parallelism = 256
     yaml = {
         "apiVersion": "batch/v1",
         "kind": "Job",
