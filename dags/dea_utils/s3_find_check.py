48ca1e4f0fe2bb4a1d363426f59d1b48c3df8499
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-11-23 03:08:53 +0000

    update add product dag

---
+++ dags/dea_utils/s3_find_check.py
@@ -0,0 +1,49 @@
+"""
+A reusable Task for checking s3_glob
+"""
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+from textwrap import dedent
+
+from infra.images import INDEXER_IMAGE
+from infra.podconfig import ONDEMAND_NODE_AFFINITY
+
+from airflow.utils.trigger_rule import TriggerRule
+
+
+def s3_find_operator(s3_glob):
+    """
+    Sets up a Task to valid s3_glob
+
+    Expects to be run within the context of a DAG
+    """
+    S3_FIND_BASH_COMMAND = [
+        "bash",
+        "-c",
+        dedent(
+            f"""
+            COUNT=`s3-find --no-sign-request {s3_glob} | wc -l`
+            if [ $COUNT -eq 0 ]; then
+                echo no file found for s3_glob {s3_glob}
+                exit 1
+            else
+                echo $COUNT files found
+            fi
+            """
+        ),
+    ]
+
+    return KubernetesPodOperator(
+        namespace="processing",
+        image=INDEXER_IMAGE,
+        arguments=S3_FIND_BASH_COMMAND,
+        labels={"app": "s3-glob-validator"},
+        name="s3-glob-validator",
+        task_id="s3-glob-validator-task",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        affinity=ONDEMAND_NODE_AFFINITY,
+        log_events_on_failure=True,
+        trigger_rule=TriggerRule.NONE_FAILED_OR_SKIPPED,  # Needed in case add product was skipped
+    )
