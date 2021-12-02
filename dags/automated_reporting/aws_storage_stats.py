0ac0e1e74bfe575a5939b745874c11a3643935a1
Ramkumar Ramagopalan <ramkumar_gr@yahoo.com>
2021-12-02 02:18:10 +0000

    add metrics calculation tasks

--- dags/automated_reporting/aws_storage_stats.py
+++ dags/automated_reporting/aws_storage_stats.py
@@ -14,6 +14,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 from datetime import datetime as dt, timedelta
 from airflow.models import Variable
 from infra.variables import AWS_STATS_SECRET
+import json
 
 default_args = {
     "owner": "Ramkumar Ramagopalan",
@@ -38,6 +39,10 @@ dag = DAG(
     schedule_interval=None,
 )
 
+INVENTORY_FILES_JSON = (
+    "{{ task_instance.xcom_pull(task_ids='get_inventory_files', key='return_value') }}"
+)
+
 with dag:
     JOBS1 = [
         "echo AWS Storage job started: $(date)",
@@ -45,6 +50,13 @@ with dag:
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import downloadinventory; downloadinventory.task()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
+
+    JOBS2 = [
+        "echo AWS Storage job started: $(date)",
+        "pip install ga-reporting-etls==1.2.19",
+        "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import process; process.task()'`",
+    ]
+
     k8s_task_download_inventory = KubernetesPodOperator(
         namespace="processing",
         image="python:3.8-slim-buster",
@@ -59,4 +71,23 @@ with dag:
             "GOOGLE_ANALYTICS_CREDENTIALS": Variable.get("google_analytics"),
         },
     )
-    k8s_task_download_inventory
+    metrics_task = {}
+    json_dict = json.loads(INVENTORY_FILES_JSON)
+    for i in range(1, len(json_dict)):
+        task_id_key = f"calc_metrics_file{i}"
+        file = json_dict[f"file{i}"]
+        metrics_task[task_id_key] = KubernetesPodOperator(
+            namespace="processing",
+            image="python:3.8-slim-buster",
+            arguments=["bash", "-c", " &&\n".join(JOBS2)],
+            name="write-xcom",
+            do_xcom_push=True,
+            is_delete_operator_pod=True,
+            in_cluster=True,
+            task_id=task_id_key,
+            get_logs=True,
+            env_vars={
+                "INVENTORY_FILE": "{{ file }}",
+            },
+        )
+        k8s_task_download_inventory >> metrics_task[task_id_key] 
