c637a4d09ce1504ca171ad377fcc5deeb7652890
Ramkumar Ramagopalan <ramkumar_gr@yahoo.com>
2021-12-07 05:08:11 +0000

    add 3rd task with the full json

--- dags/automated_reporting/aws_storage_stats.py
+++ dags/automated_reporting/aws_storage_stats.py
@@ -72,7 +72,7 @@ with dag:
         do_xcom_push=True,
         is_delete_operator_pod=True,
         in_cluster=True,
-        task_id="metrics_collector1",
+        task_id="metrics_collector3",
         get_logs=True,
         env_vars={
             "INVENTORY_FILE" : "{{ task_instance.xcom_pull(task_ids='get_inventory_files', key='return_value')['metrics_collector_1'] }}",
@@ -92,4 +92,18 @@ with dag:
             "INVENTORY_FILE" : "{{ task_instance.xcom_pull(task_ids='get_inventory_files', key='return_value')['metrics_collector_2'] }}",
         },
     )
-    k8s_task_download_inventory >> [metrics_task1, metrics_task2]
+    metrics_task3 = KubernetesPodOperator(
+        namespace="processing",
+        image="python:3.8-slim-buster",
+        arguments=["bash", "-c", " &&\n".join(JOBS2)],
+        name="write-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="metrics_collector3",
+        get_logs=True,
+        env_vars={
+            "INVENTORY_FILE" : "{{ task_instance.xcom_pull(task_ids='get_inventory_files', key='return_value') }}",
+        },
+    )
+    k8s_task_download_inventory >> [metrics_task1, metrics_task2, metric_task3]
