7590fa0453278d3568939b4cab3187c3f658193a
Ramkumar Ramagopalan <ramkumar_gr@yahoo.com>
2021-12-08 05:24:43 +0000

    dynamic tasks

--- dags/automated_reporting/aws_storage_stats.py
+++ dags/automated_reporting/aws_storage_stats.py
@@ -131,7 +131,7 @@ with dag:
             do_xcom_push=True,
             is_delete_operator_pod=True,
             in_cluster=True,
-            task_id="metrics_collector3",
+            task_id=f"collection{i}",
             get_logs=True,
             env_vars={
                 "INVENTORY_FILE" : "{{ task_instance.xcom_pull(task_ids='get_inventory_files', key='return_value') }}",
