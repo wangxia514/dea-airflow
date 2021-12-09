decf7451ce04a94006167517a509261ee795eacc
Ramkumar Ramagopalan <ramkumar_gr@yahoo.com>
2021-12-09 01:57:07 +0000

    bump version to 41

--- dags/automated_reporting/aws_storage_stats.py
+++ dags/automated_reporting/aws_storage_stats.py
@@ -40,25 +40,25 @@ dag = DAG(
 with dag:
     JOBS1 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.40",
+        "pip install ga-reporting-etls==1.2.41",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import downloadinventory; downloadinventory.task()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
     JOBS2 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.40",
+        "pip install ga-reporting-etls==1.2.41",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import process; process.printvar()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
     JOBS3 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.40",
+        "pip install ga-reporting-etls==1.2.41",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import process; process.printvarv2()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
     JOBS4 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.40",
+        "pip install ga-reporting-etls==1.2.41",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import process; process.calc_size_and_count()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
