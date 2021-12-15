aaa9ab35fb00189d1bdac3605cfe4b62d36e5129
Ramkumar Ramagopalan <ramkumar_gr@yahoo.com>
2021-12-15 10:03:44 +0000

    bump version to 1.2.49

--- dags/automated_reporting/aws_storage_stats.py
+++ dags/automated_reporting/aws_storage_stats.py
@@ -101,19 +101,19 @@ def aggregate_metrics_from_collections(task_instance):
 with dag:
     JOBS1 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.48",
+        "pip install ga-reporting-etls==1.2.49",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import downloadinventory; downloadinventory.task()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
     JOBS2 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.48",
+        "pip install ga-reporting-etls==1.2.49",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import process; process.calc_size_and_count()'`",
         "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
     ]
     JOBS3 = [
         "echo AWS Storage job started: $(date)",
-        "pip install ga-reporting-etls==1.2.48",
+        "pip install ga-reporting-etls==1.2.49",
         "jsonresult=`python3 -c 'from nemo_reporting.aws_storage_stats import etl; etl.task()'`",
         "mkdir -p /airflow/xcom/; echo '{\"status\": \"success\"}' > /airflow/xcom/return.json",
     ]
