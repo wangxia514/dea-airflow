7ece34046f0dc78a5d14be13e5dae169ff6b973f
Sai Ma <Sai.Ma@ga.gov.au>
2022-02-18 15:13:49 +1100

    Add allow-unsafe to elvis update metadat-type

--- dags/k8s_index_elvis_lidar_dem_test_loop.py
+++ dags/k8s_index_elvis_lidar_dem_test_loop.py
@@ -154,7 +154,7 @@ ADD_METADATA_TYPE_CMD = [
     "-c",
     dedent(
         """
-        datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/feature/add-elvis-dem_1m/product_metadata/eo3_dem.odc-type.yaml
+        datacube metadata update https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/feature/add-elvis-dem_1m/product_metadata/eo3_dem.odc-type.yaml --allow-unsafe
         """
     ),
 ]
