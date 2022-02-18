eca12305226f8e43748fea4228cf864ca6133a9e
Sai Ma <Sai.Ma@ga.gov.au>
2022-02-18 13:52:34 +1100

    Add adding products step to elvis indexing

--- dags/k8s_index_elvis_lidar_dem_test_loop.py
+++ dags/k8s_index_elvis_lidar_dem_test_loop.py
@@ -3,10 +3,12 @@ This DAG aims speed up the ELVIS Lidar (Digital Elevation Model) data indexing t
 
 It includes following main steps:
 
-1. load metadata files from s3://elvis-stac bucket (belongs to NLI team, we better not modify files in that bucket)
-2. modify the metadata content (will pass this change to NLI team once everyone happy with the DEM products)
-3. dump the updated metadata to s3://dea-public-data-dev/projects/elvis-lidar
-4. index DEM datasets from s3://dea-public-data-dev/projects/elvis-lidar
+1. add ELVIS Lidar product metadata-type configures to odc
+2. add ELVIS Lidar product configures (dem_1m, dem_5m) to odc
+3. load metadata files from s3://elvis-stac bucket (belongs to NLI team, we better not modify files in that bucket)
+4. modify the metadata content (will pass this change to NLI team once everyone happy with the DEM products)
+5. dump the updated metadata to s3://dea-public-data-dev/projects/elvis-lidar
+6. index DEM datasets from s3://dea-public-data-dev/projects/elvis-lidar
 
 To run this Airflow DAG, we need value:
 
@@ -14,6 +16,10 @@ script_path
     Default "https://raw.githubusercontent.com/GeoscienceAustralia/dea-airflow/develop/scripts/elvis_lidar_metadata_changing.py"
 
 We force the user to provide script path before DAG run. In case the Airlfow repo had been hacked and someone can pass any script to run in GA network.
+
+Note: this DAG does not include auto-refresh existing product defintions (because of opendatacube limitations). If user try to
+modify the ELVIS Lidar product defintions (e.g. add one more querable field), please use relative deletion_utility_datasets DAG
+to remove products. Then this DAG can add products back by updated configurations, then index datasets.
 
 """
 
@@ -143,6 +149,59 @@ S3_TO_DC_CMD = [
 ]
 
 
+ADD_METADATA_TYPE_CMD = [
+    "bash",
+    "-c",
+    dedent(
+        """
+        datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/feature/add-elvis-dem_1m/product_metadata/eo3_dem.odc-type.yaml
+        """
+    ),
+]
+
+
+ADD_PRODUCTS_CMD = [
+    "bash",
+    "-c",
+    dedent(
+        """
+        datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/feature/add-elvis-dem_1m/products/others/elvis/dem_1m.yaml
+        datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/feature/add-elvis-dem_1m/products/others/elvis/dem_5m.yaml
+        """
+    ),
+]
+
+
+def add_metadata_type(dag):
+    return KubernetesPodOperator(
+        namespace="processing",
+        image=INDEXER_IMAGE,
+        image_pull_policy="IfNotPresent",
+        labels={"step": "add-metadata-type"},
+        arguments=ADD_METADATA_TYPE_CMD,
+        name="elvis-lidar-add-metadata-type",
+        task_id="elvis-lidar-add-metadata-type",
+        get_logs=True,
+        affinity=ONDEMAND_NODE_AFFINITY,
+        is_delete_operator_pod=True,
+    )
+
+
+def add_products(dag):
+    return KubernetesPodOperator(
+        namespace="processing",
+        image=INDEXER_IMAGE,
+        image_pull_policy="IfNotPresent",
+        labels={"step": "add-elvis-dem-products"},
+        arguments=ADD_PRODUCTS_CMD,
+        name="elvis-lidar-add-products",
+        task_id="elvis-lidar-add-products",
+        get_logs=True,
+        affinity=ONDEMAND_NODE_AFFINITY,
+        is_delete_operator_pod=True,
+    )
+
+
 def index_dataset(dag):
     return KubernetesPodOperator(
         namespace="processing",
@@ -159,7 +218,8 @@ def index_dataset(dag):
 
 
 with dag:
-
+    add_metadata_type = add_metadata_type(dag)
+    add_products = add_products(dag)
     update_metadata = update_metadata(dag)
     index_dataset = index_dataset(dag)
-    update_metadata >> index_dataset
+    add_metadata_type >> add_products >> update_metadata >> index_dataset
