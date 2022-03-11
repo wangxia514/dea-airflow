b49f92071c65266c5b7fa9a6aab9fc2080428277
Tom McAdam <mcadam1980@gmail.com>
2022-03-11 14:55:52 +1000

    automated_reporting: added completeness task to esa_monitoring

--- dags/automated_reporting/rep_esa_monitoring_prod.py
+++ dags/automated_reporting/rep_esa_monitoring_prod.py
@@ -6,16 +6,26 @@ Operational monitoring of ESA production systems
 
 # The DAG object; we'll need this to instantiate a DAG
 from datetime import datetime, timedelta
+import json
+
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
-
+from airflow.hooks.base_hook import BaseHook
 from airflow.models import Variable
+
+from automated_reporting.utilities import helpers
+from infra import connections as infra_connections
 
 REP_CONN_STR = Variable.get("db_rep_secret")
 SCIHUB_CREDENTIALS_STR = Variable.get("copernicus_api_password")
 S3_CREDENTIALS_STR = Variable.get("reporting_s3_secret")
+ODC_CONN_STR = json.dumps(
+    helpers.parse_connection(
+        BaseHook.get_connection(infra_connections.DB_ODC_READER_CONN)
+    )
+)
 
 default_args = {
     "owner": "Tom McAdam",
@@ -68,8 +78,7 @@ with dag:
     INSERT_ACQS_TASK = [
         "echo Insert S2 acquisitions: $(date)",
         "pip install ga-reporting-etls",
-        "jsonresult=`python3 -c 'from nemo_reporting.esa_monitoring import s2_inserts; s2_inserts.task_env()'`",
-        "mkdir -p /airflow/xcom/; echo $jsonresult > /airflow/xcom/return.json",
+        "python3 -c 'from nemo_reporting.esa_monitoring import s2_inserts; s2_inserts.task_env()'",
     ]
     insert_s2_acquisitions = KubernetesPodOperator(
         namespace="processing",
@@ -87,4 +96,74 @@ with dag:
         },
     )
 
-    scihub_s2_acquisitions >> insert_s2_acquisitions
+    L1_CONFIG = {
+        "title": "AWS L1 SQS",
+        "source": "sqs",
+        "use_identifier": True,
+        "days": 30,
+        "sensors": [
+            {"id": "s2a", "pipeline": "S2A_MSIL1C", "rep_code": "esa_s2a_msi_l1c"},
+            {"id": "s2b", "pipeline": "S2B_MSIL1C", "rep_code": "esa_s2b_msi_l1c"},
+        ],
+    }
+    ARD_CONFIG = {
+        "title": "AWS ARD ODC",
+        "source": "odc-nrt",
+        "use_identifier": True,
+        "days": 30,
+        "sensors": [
+            {
+                "id": "s2a",
+                "odc_code": "s2a_nrt_granule",
+                "rep_code": "ga_s2a_msi_ard_c3",
+            },
+            {
+                "id": "s2b",
+                "odc_code": "s2b_nrt_granule",
+                "rep_code": "ga_s2b_msi_ard_c3",
+            },
+        ],
+    }
+    COMPUTE_COMPLETENESS_TASK = [
+        "echo Compute S2 L1 Completeness: $(date)",
+        "pip install ga-reporting-etls",
+        "python3 -c 'from nemo_reporting.esa_monitoring import s2_completeness; s2_completeness.task_env()'",
+    ]
+    compute_s2_l1_completeness = KubernetesPodOperator(
+        namespace="processing",
+        image="python:3.8-slim-buster",
+        arguments=["bash", "-c", " &&\n".join(COMPUTE_COMPLETENESS_TASK)],
+        name="compute_s2_l1_completeness",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="compute_s2_l1_completeness",
+        get_logs=True,
+        env_vars={
+            "COMPLETENESS_CONFIG": json.dumps(L1_CONFIG),
+            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
+            "DB_CREDS": REP_CONN_STR,
+            "ODC_CREDS": ODC_CONN_STR,
+        },
+    )
+    compute_s2_ard_completeness = KubernetesPodOperator(
+        namespace="processing",
+        image="python:3.8-slim-buster",
+        arguments=["bash", "-c", " &&\n".join(COMPUTE_COMPLETENESS_TASK)],
+        name="compute_s2_ard_completeness",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="compute_s2_ard_completeness",
+        get_logs=True,
+        env_vars={
+            "COMPLETENESS_CONFIG": json.dumps(ARD_CONFIG),
+            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
+            "DB_CREDS": REP_CONN_STR,
+            "ODC_CREDS": ODC_CONN_STR,
+        },
+    )
+
+    (
+        scihub_s2_acquisitions
+        >> insert_s2_acquisitions
+        >> [compute_s2_l1_completeness, compute_s2_ard_completeness]
+    )
