cdeaff09f752603348fc9a89c6be1a732de5e8a2
Tom McAdam <mcadam1980@gmail.com>
2022-03-23 09:39:56 +1000

    automated_reporting: elvis version bump

--- dags/automated_reporting/developement_dags/rep_elvis_dev.py
+++ dags/automated_reporting/developement_dags/rep_elvis_dev.py
@@ -50,7 +50,7 @@ dag = DAG(
 with dag:
     JOBS7 = [
         "echo Elvis ingestion processing: $(date)",
-        "pip install ga-reporting-etls==1.10.1",
+        "pip install ga-reporting-etls==1.10.2",
         "marine-elvis-ingestion",
     ]
     START = DummyOperator(task_id="marine-monthly-stats")
