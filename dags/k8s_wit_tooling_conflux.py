7234d448865d3928af26171606bbae8a74e20ede
Sai Ma <Sai.Ma@ga.gov.au>
2022-05-03 02:18:43 +0000

    Change the msg queue names to run qld sample

--- dags/k8s_wit_tooling_conflux.py
+++ dags/k8s_wit_tooling_conflux.py
@@ -136,16 +136,16 @@ dag = DAG(
     tags=["k8s", "landsat", "wit", "conflux"],
 )
 
-WIT_INPUTS = [{"product": "ga_ls5t_ard_3", "plugin": "wit_ls5", "queue": "wit_conflux_ls5_sqs"},
-              {"product": "ga_ls7e_ard_3", "plugin": "wit_ls7", "queue": "wit_conflux_ls7_sqs"},
-              {"product": "ga_ls8c_ard_3", "plugin": "wit_ls8", "queue": "wit_conflux_ls8_sqs"}]
+WIT_INPUTS = [{"product": "ga_ls5t_ard_3", "plugin": "wit_ls5", "queue": "wit_conflux_qld_ls5_sqs"},
+              {"product": "ga_ls7e_ard_3", "plugin": "wit_ls7", "queue": "wit_conflux_qld_ls7_sqs"},
+              {"product": "ga_ls8c_ard_3", "plugin": "wit_ls8", "queue": "wit_conflux_qld_ls8_sqs"}]
 
 
 def k8s_job_task(dag, queue_name, plugin, product):
     mem = CONFLUX_POD_MEMORY_MB
     req_mem = "{}Mi".format(int(mem) * 2)
     lim_mem = "{}Mi".format(int(mem) * 2)
-    parallelism = 50
+    parallelism = 32
 
     yaml = {
         "apiVersion": "batch/v1",
