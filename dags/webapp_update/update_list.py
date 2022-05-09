d67461f9e73bb68314fdc4a3e4006c13efef253a
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-05-09 04:13:58 +0000

    remove c2 product from webapp update

--- dags/webapp_update/update_list.py
+++ dags/webapp_update/update_list.py
@@ -4,8 +4,6 @@ Lists of default products to update in the OWS and Explorer Web Application
 # Default OWS product layers to be updated
 OWS_UPDATE_LIST = (
     "s2_nrt_granule_nbar_t",
-    "wofs_albers",
-    "fc_albers_combined",
     "s2_ard_granule_nbar_t",
     "ga_ls_wo_3",
     "ga_ls_fc_3",
@@ -24,9 +22,6 @@ EXPLORER_UPDATE_LIST = (
     "s2b_nrt_granule",
     "s2a_ard_granule",
     "s2b_ard_granule",
-    "wofs_albers",
-    "ls7_fc_albers",
-    "ls8_fc_albers",
     "ga_ls_wo_3",
     "ga_ls_fc_3",
     "ga_ls5t_ard_3",
