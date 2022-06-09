04538da86a2ae7c6d819d55f9481b73cd425d79f
Sai Ma <Sai.Ma@ga.gov.au>
2022-06-09 05:50:10 +0000

    Change printout format to meet jinja2 request

--- dags/k8s_wit_tooling_conflux_dev.py
+++ dags/k8s_wit_tooling_conflux_dev.py
@@ -605,12 +605,12 @@ with dag:
     )
 
     bash_commands = """
-        echo "shapefile: {{ dag_run.conf["shapefile"] if dag_run else "" }}";
-        echo "cmd: {{ dag_run.conf["cmd"] if dag_run else "" }}";
-        echo "intermediatedir: {{ dag_run.conf["intermediatedir"] if dag_run else "" }}";
-        echo "csvdir: {{ dag_run.conf["csvdir"] if dag_run else "" }}";
-        echo "flags: {{ dag_run.conf["flags"] if dag_run else "" }}";
-        echo "use_id: {{ dag_run.conf["use_id"] if dag_run else "" }}"
+        echo "shapefile: {{{{ dag_run.conf["shapefile"] if dag_run else "" }}}}";
+        echo "cmd: {{{{ dag_run.conf["cmd"] if dag_run else "" }}}}";
+        echo "intermediatedir: {{{{ dag_run.conf["intermediatedir"] if dag_run else "" }}}}";
+        echo "csvdir: {{{{ dag_run.conf["csvdir"] if dag_run else "" }}}}";
+        echo "flags: {{{{ dag_run.conf["flags"] if dag_run else "" }}}}";
+        echo "use_id: {{{{ dag_run.conf["use_id"] if dag_run else "" }}}}"
         """
 
     print_dag_run_cfg = BashOperator(
