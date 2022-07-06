626f35dd000741f6ce2c9efcc7eb8be4c145a5fa
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-07-06 03:55:14 +0000

    odc tasks and sns tasks as dict

--- dags/automated_reporting/rep_nrt_simple_latency_prod.py
+++ dags/automated_reporting/rep_nrt_simple_latency_prod.py
@@ -93,7 +93,7 @@ with dag:
         task_id="check-db",
         get_logs=True,
     )
-    odc_tasks = []
+    odc_tasks = {} 
     for i in range(1, len(products_list) + 1):
         odc_tasks[i] = KubernetesPodOperator(
             namespace="processing",
@@ -111,7 +111,7 @@ with dag:
             },
         )
         check_db >> odc_tasks[i]
-    sns_tasks = []
+    sns_tasks = {} 
     sns_list = [("S2A_MSIL1C", "esa_s2a_msi_l1c"), ("S2B_MSIL1C", "esa_s2b_msi_l1c")]
     for i in range(1, len(sns_list) + 1):
         sns_tasks[i] = KubernetesPodOperator(
