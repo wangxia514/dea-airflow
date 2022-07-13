21f0a7a754aed9d6940829ee03c18e2ed539533c
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-07-13 12:10:46 +0000

    xcom1

--- dags/automated_reporting/rep_check_nci_conn.py
+++ dags/automated_reporting/rep_check_nci_conn.py
@@ -31,15 +31,7 @@ with dag:
     print_ga_storage_task = SSHOperator(
         task_id="print_storage_file",
         ssh_conn_id="lpgs_gadi",
-        #command="cat /scratch/v10/usage_reports/ga_storage_usage_latest.csv",
         command=JOBS2,
         do_xcom_push=True,
     )
-    #run_lquota_task = SSHOperator(
-    #    task_id="run_lquota_task",
-    #    ssh_conn_id="lpgs_gadi",
-    #    command="which lquota",
-    #    do_xcom_push=True,
-    #)
     print_ga_storage_task
-    #>> run_lquota_task
