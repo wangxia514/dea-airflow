4c3d4fdbb215aaa309f032e8cc14bc9bd69bbc99
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-07-14 01:10:57 +0000

    custom ssh operator

--- dags/automated_reporting/rep_check_nci_conn.py
+++ dags/automated_reporting/rep_check_nci_conn.py
@@ -7,6 +7,23 @@ from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from datetime import datetime
 
+class MySSHOperator(SSHOperator):
+
+    def execute(self, context=None) -> Union[bytes, str]:
+        result: Union[bytes, str]
+        if self.command is None:
+            raise AirflowException("SSH operator error: SSH command not specified. Aborting.")
+
+        # Forcing get_pty to True if the command begins with "sudo".
+        self.get_pty = self.command.startswith('sudo') or self.get_pty
+
+        try:
+            with self.get_ssh_client() as ssh_client:
+                result = self.run_ssh_client_command(ssh_client, self.command)
+        except Exception as e:
+            raise AirflowException(f"SSH operator error: {str(e)}")
+        return result.decode('utf-8')
+
 default_args = {
     "owner": "Ramkumar Ramagopalan",
     "start_date": datetime(2020, 3, 12),
@@ -24,7 +41,7 @@ dag = DAG(
 )
 
 with dag:
-    print_ga_storage_task = SSHOperator(
+    print_ga_storage_task = MySSHOperator(
         task_id="print_storage_file",
         ssh_conn_id="lpgs_gadi",
         command="cat /scratch/v10/usage_reports/ga_storage_usage_latest.csv",
