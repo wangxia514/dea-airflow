c03c0de3310078e72e94a097fa80b37236e9afc5
Tom McAdam <tom.mcadam@ga.gov.au>
2022-07-14 23:10:46 +0000

    ar: added rapid currency dag

--- dags/automated_reporting/developement_dags/rep_dea_currency.py
+++ dags/automated_reporting/developement_dags/rep_dea_currency.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 """
-sara_history dag
+DEA Currency Dags
 """
 
 # The DAG object; we'll need this to instantiate a DAG
@@ -20,8 +19,10 @@ default_args = {
     "depends_on_past": False,
     "start_date": dt.now() - timedelta(hours=1),
     "email": ["tom.mcadam@ga.gov.au"],
-    "email_on_failure": True,
+    "email_on_failure": False,  ### UPDATE IN PROD ####
     "email_on_retry": False,
+    "retry_delay": timedelta(minutes=5),
+    "retries": 3,
     "secrets": [
         Secret("env", "DB_HOST", REPORTING_DB_DEV_SECRET, "DB_HOST"),
         Secret("env", "DB_NAME", REPORTING_DB_DEV_SECRET, "DB_NAME"),
@@ -29,30 +30,53 @@ default_args = {
         Secret("env", "DB_USER", REPORTING_DB_DEV_SECRET, "DB_USER"),
         Secret("env", "DB_PASSWORD", REPORTING_DB_DEV_SECRET, "DB_PASSWORD"),
         Secret("env", "ODC_DB_HOST", REPORTING_ODC_DB_SECRET, "DB_HOST"),
-        Secret("env", "ODC_DB_NAME", REPORTING_ODC_DB_SECRET, "DB_DB_NAME"),
-        Secret("env", "ODC_DB_PORT", REPORTING_ODC_DB_SECRET, "DB_DB_PORT"),
+        Secret("env", "ODC_DB_NAME", REPORTING_ODC_DB_SECRET, "DB_NAME"),
+        Secret("env", "ODC_DB_PORT", REPORTING_ODC_DB_SECRET, "DB_PORT"),
         Secret("env", "ODC_DB_USER", REPORTING_ODC_DB_SECRET, "DB_USER"),
         Secret("env", "ODC_DB_PASSWORD", REPORTING_ODC_DB_SECRET, "DB_PASSWORD"),
     ],
 }
 
-daily_args = default_args.copy()
-daily_args["retry_delay"] = timedelta(minutes=5)
-daily_args["retries"] = 3
-
 daily_dag = DAG(
     "rep_dea_currency_daily",
-    default_args=daily_args,
+    default_args=default_args,
     description="DAG for currency of dea products (run daily)",
     tags=["reporting_dev"],
     schedule_interval=timedelta(days=1)
 )
 
+rapid_dag = DAG(
+    "rep_dea_currency_rapid",
+    default_args=default_args,
+    description="DAG for currency of dea products (run daily)",
+    tags=["reporting_dev"],
+    schedule_interval=timedelta(minutes=15)
+)
+
 ODC_CURRENCY_JOB = [
     "echo DEA ODC Currency job started: $(date)",
     "pip install ga-reporting-etls==2.2.2",
-    "odc-currency",
+    "odc-currency"
 ]
+SNS_CURRENCY_JOB = [
+    "echo DEA ODC Currency job started: $(date)",
+    "pip install ga-reporting-etls==2.2.2",
+    "sns-currency"
+]
+
+def create_operator(dag, method, job, env_vars):
+    return  KubernetesPodOperator(
+                dag=dag,
+                namespace="processing",
+                image="python:3.8-slim-buster",
+                arguments=["bash", "-c", " &&\n".join(job)],
+                name=f"{method}_currency-{env_vars['PRODUCT_ID']}",
+                is_delete_operator_pod=True,
+                in_cluster=True,
+                task_id=f"{method}_currency-{env_vars['PRODUCT_ID']}",
+                get_logs=True,
+                env_vars=env_vars
+    )
 
 def create_odc_task(dag, product_id, days, product_suffix=None):
     """
@@ -65,19 +89,20 @@ def create_odc_task(dag, product_id, days, product_suffix=None):
     }
     if product_suffix:
         env_vars["PRODUCT_SUFFIX"] = product_suffix
+    return create_operator("odc", dag, ODC_CURRENCY_JOB, env_vars)
 
-    return  KubernetesPodOperator(
-                dag=dag,
-                namespace="processing",
-                image="python:3.8-slim-buster",
-                arguments=["bash", "-c", " &&\n".join(ODC_CURRENCY_JOB)],
-                name=f"odc_currency-{product_id}",
-                is_delete_operator_pod=True,
-                in_cluster=True,
-                task_id=f"odc_currency-{product_id}",
-                get_logs=True,
-                env_vars=env_vars
-    )
+def create_sns_task(dag, product_id, days, pipeline):
+    """
+    Function to generate KubernetesPodOperator tasks with id based on `product_id`
+    """
+    env_vars={
+        "DAYS": str(days),
+        "PRODUCT_ID": product_id,
+        "PIPELINE": pipeline,
+        "DATA_INTERVAL_END": "{{  dag_run.data_interval_end | ts  }}"
+    }
+    return create_operator("sns", dag, SNS_CURRENCY_JOB, env_vars)
+
 
 with daily_dag:
 
@@ -101,3 +126,23 @@ with daily_dag:
     ]
     daily_odc_tasks = [create_odc_task(daily_dag, product_id, 90, "aws") for product_id in products_list]
     START >> daily_odc_tasks
+
+with rapid_dag:
+
+    # Product list to extract the metric for, could potentially be part of dag configuration and managed in airflow UI?
+    odc_products_list = [
+        "s2a_nrt_granule",
+        "s2b_nrt_granule",
+        "ga_s2_wo_3",
+        "ga_ls7e_ard_provisional_3",
+        "ga_ls8c_ard_provisional_3",
+        "ga_s2am_ard_provisional_3",
+        "ga_s2bm_ard_provisional_3",
+        "ga_s2_ba_provisional_3",
+    ]
+    sns_products_list = [("S2A_MSIL1C", "esa_s2a_msi_l1c"), ("S2B_MSIL1C", "esa_s2b_msi_l1c")]
+
+    rapid_odc_tasks = [create_odc_task(rapid_dag, product_id, 30) for product_id in odc_products_list]
+    rapid_sns_tasks = [create_sns_task(rapid_dag, product_id, pipeline) for pipeline, product_id in sns_products_list]
+    START >> rapid_odc_tasks
+    START >> rapid_sns_tasks
