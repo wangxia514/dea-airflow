ccfdd90dc531e97273a51c521af4890f132c965a
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-07-17 06:42:42 +0000

    add cat statement

--- dags/automated_reporting/rep_test_secret_as_volume.py
+++ dags/automated_reporting/rep_test_secret_as_volume.py
@@ -34,7 +34,7 @@ dag = DAG(
 with dag:
     JOBS_CHECK_VOLUME = [
         "echo check tmp contents $(date)",
-        "while :; do echo 'Hit CTRL+C'; sleep 1; done",
+        "while :; do cat /var/secrets/google/DB_HOST; sleep 100; done",
     ]
     kubernetes_secret_vars_ex = KubernetesPodOperator(
         namespace="processing",
