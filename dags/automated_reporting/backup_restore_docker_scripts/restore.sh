8c4f32bd8fc08654a99e3317eff3400f62c984e6
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-08-08 02:10:37 +0000

    restore.sh updated

--- dags/automated_reporting/backup_restore_docker_scripts/restore.sh
+++ dags/automated_reporting/backup_restore_docker_scripts/restore.sh
@@ -5,4 +5,5 @@ then
     EXECUTION_DATE=`aws s3 ls s3://$REPORTING_BUCKET/ | tr -s ' ' ' ' | cut -f3 -d ' ' | cut -f1 -d '/' | tail -1`
     result=`aws s3 cp s3://$REPORTING_BUCKET/$EXECUTION_DATE/$SCHEMA-dump.sql $SCHEMA-dump.sql`
 fi
-pg_restore -h $DB_HOST -U $DB_USER -d $DB_NAME -1 $SCHEMA-dump.sql
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DROP SCHEMA $SCHEMA CASCADE;"
+psql -h $DB_HOST -U $DB_USER $DB_NAME < $SCHEMA-dump.sql
