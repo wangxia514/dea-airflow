86a99e7a5c8bb7d974fe917d07ba81c3933cf5d4
Ramkumar Ramagopalan <ramkumar.ramagopalan@ga.gov.au>
2022-08-25 02:05:37 +0000

    sentinel nrt downloader

---
+++ dags/automated_reporting/rep_check_sentinel_nrt_downloader.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+aws cost stats dag for ga-aws-dea
+"""
+# The DAG object; we'll need this to instantiate a DAG
+from datetime import datetime as dt
+from airflow import DAG
+from airflow.kubernetes.secret import Secret
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+from infra.variables import REPORTING_IAM_SQS_SECRET, REPORTING_DB_DEV_SECRET
+
+default_args = {
+    "owner": "Ramkumar Ramagopalan",
+    "depends_on_past": False,
+    "email": ["ramkumar.ramagopalan@ga.gov.au"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "start_date": dt(2022, 8, 17),
+    "secrets": [
+        Secret("env", "ACCESS_KEY", REPORTING_IAM_SQS_SECRET, "ACCESS_KEY"),
+        Secret("env", "SECRET_KEY", REPORTING_IAM_SQS_SECRET, "SECRET_KEY"),
+        Secret("env", "DB_HOST", REPORTING_DB_DEV_SECRET, "DB_HOST"),
+        Secret("env", "DB_NAME", REPORTING_DB_DEV_SECRET, "DB_NAME"),
+        Secret("env", "DB_PORT", REPORTING_DB_DEV_SECRET, "DB_PORT"),
+        Secret("env", "DB_USER", REPORTING_DB_DEV_SECRET, "DB_USER"),
+        Secret("env", "DB_PASSWORD", REPORTING_DB_DEV_SECRET, "DB_PASSWORD"),
+    ],
+}
+
+dag = DAG(
+    "rep_sentinel_l1_nrt_downloads",
+    description="DAG for sentinel_l1_nrt_downloads dev",
+    tags=["reporting_dev"],
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+ETL_IMAGE = (
+    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/ga-reporting-etls-dev:latest"
+)
+
+with dag:
+    JOBS1 = [
+        "echo sentinel-l1-nrt-downloads job started: $(date)",
+        "mkdir -p /airflow/xcom/",
+        "sentinel-l1-nrt-downloads /airflow/xcom/return.json",
+    ]
+    JOBS2 = [
+        "echo sentinel-l1-nrt-downloads job started: $(date)",
+        "sentinel-l1-nrt-ingestion",
+    ]
+    sentinel_l1_nrt_downloads = KubernetesPodOperator(
+        namespace="processing",
+        image=ETL_IMAGE,
+        arguments=["bash", "-c", " &&\n".join(JOBS1)],
+        name="sentinel_l1_nrt_downloads",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="sentinel_l1_nrt_downloads",
+        get_logs=True,
+        task_concurrency=1,
+        do_xcom_push=True,
+        env_vars={
+            "QUEUE_NAME": "sentinel-sqs-test",
+        }
+    )
+    sentinel_l1_nrt_ingestion = KubernetesPodOperator(
+        namespace="processing",
+        image=ETL_IMAGE,
+        arguments=["bash", "-c", " &&\n".join(JOBS2)],
+        name="sentinel_l1_nrt_ingestion",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="sentinel_l1_nrt_ingestion",
+        get_logs=True,
+        task_concurrency=1,
+        do_xcom_push=False,
+        env_vars={
+            "METRICS": "{{ task_instance.xcom_pull(task_ids='sentinel_l1_nrt_downloads') }}",
+        }
+    )
+    sentinel_l1_nrt_downloads >> sentinel_l1_nrt_ingestion
