4361000ce9f34d060cd5668cecb90863102af6b1
Tom McAdam <tom.mcadam@ga.gov.au>
2022-08-30 12:00:11 +0000

    automated_reporting: puse k8s secrets for ea and asb google analytics

--- dags/automated_reporting/rep_esa_monitoring_prod.py
+++ dags/automated_reporting/rep_esa_monitoring_prod.py
@@ -9,34 +9,12 @@ from datetime import datetime, timedelta
 import json
 
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
-from airflow.hooks.base_hook import BaseHook
-from airflow.models import Variable
 
-from infra import connections as infra_connections
 from automated_reporting import k8s_secrets, utilities
 
-
-def parse_connection(conn_obj):
-    """
-    Parse an Airflow connection object into a dictionary
-    """
-    return dict(
-        user=conn_obj.login,
-        password=conn_obj.password,
-        host=conn_obj.host,
-        dbname=conn_obj.schema,
-        port=conn_obj.port,
-    )
-
-
-REP_CONN_STR = Variable.get("db_rep_secret")
-SCIHUB_CREDENTIALS_STR = Variable.get("copernicus_api_password")
-S3_CREDENTIALS_STR = Variable.get("reporting_s3_secret")
-ODC_CONN_STR = json.dumps(
-    parse_connection(BaseHook.get_connection(infra_connections.DB_ODC_READER_CONN))
+ENV = "prod"
+ETL_IMAGE = (
+    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/ga-reporting-etls:v2.11.0"
 )
 
 default_args = {
@@ -44,70 +22,58 @@ default_args = {
     "depends_on_past": False,
     "start_date": datetime(2022, 3, 1),
     "email": ["tom.mcadam@ga.gov.au"],
-    "email_on_failure": True,
+    "email_on_failure": True if ENV == "prod" else False,
     "email_on_retry": False,
     "retries": 2,
     "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    "rep_esa_monitoring_prod",
+    f"rep_esa_monitoring_{ENV}",
     description="DAG ESA production monitoring",
-    tags=["reporting"],
+    tags=["reporting"] if ENV == "prod" else ["reporting_dev"],
     default_args=default_args,
-    schedule_interval=timedelta(minutes=15),
-)
-
-ENV = "prod"
-ETL_IMAGE = (
-    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/ga-reporting-etls:v2.10.0"
+    schedule_interval="*/15 * * * *",
 )
 
 with dag:
 
     SCIHUB_ACQS_TASK = [
         "echo Get SCIHUB acquisitions: $(date)",
+        "parse-uri ${REP_DB_URI} /tmp/env; source /tmp/env",
         "mkdir -p /airflow/xcom/",
         "esa-acquisitions /airflow/xcom/return.json",
     ]
-    scihub_s2_acquisitions = KubernetesPodOperator(
-        namespace="processing",
+    scihub_s2_acquisitions = utilities.k8s_operator(
+        dag=dag,
         image=ETL_IMAGE,
-        arguments=["bash", "-c", " &&\n".join(SCIHUB_ACQS_TASK)],
-        name="scihub_s2_acquisitions",
-        is_delete_operator_pod=True,
-        in_cluster=True,
         task_id="scihub_s2_acquisitions",
-        get_logs=True,
+        xcom=True,
         task_concurrency=1,
-        do_xcom_push=True,
+        cmds=SCIHUB_ACQS_TASK,
         env_vars={
-            "SCIHUB_CREDENTIALS": SCIHUB_CREDENTIALS_STR,
-            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
-            "DB_CREDS": REP_CONN_STR,
             "ACQUISITION_DAYS": "{{ dag_run.conf['acquisition_days'] | default(3) }}",
             "DATA_INTERVAL_END": "{{  dag_run.data_interval_end | ts  }}",
         },
+        secrets=k8s_secrets.scihub_secrets
+        + k8s_secrets.s3_secrets
+        + k8s_secrets.db_secrets(ENV),
     )
 
     INSERT_ACQS_TASK = [
         "echo Insert S2 acquisitions: $(date)",
+        "parse-uri ${REP_DB_URI} /tmp/env; source /tmp/env",
         "esa-inserts",
     ]
-    insert_s2_acquisitions = KubernetesPodOperator(
-        namespace="processing",
+    insert_s2_acquisitions = utilities.k8s_operator(
+        dag=dag,
         image=ETL_IMAGE,
-        arguments=["bash", "-c", " &&\n".join(INSERT_ACQS_TASK)],
-        name="insert_s2_acquisitions",
-        is_delete_operator_pod=True,
-        in_cluster=True,
         task_id="insert_s2_acquisitions",
-        get_logs=True,
+        cmds=INSERT_ACQS_TASK,
         env_vars={
-            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
-            "DB_CREDS": REP_CONN_STR,
             "S2_ACQ_XCOM": "{{ task_instance.xcom_pull(task_ids='scihub_s2_acquisitions', key='return_value') }}",
         },
+        secrets=k8s_secrets.s3_secrets + k8s_secrets.db_secrets(ENV),
     )
 
     L1_CONFIG = {
@@ -159,59 +125,46 @@ with dag:
 
     COMPUTE_COMPLETENESS_TASK = [
         "echo Compute S2 L1 Completeness: $(date)",
+        "parse-uri ${REP_DB_URI} /tmp/env; source /tmp/env",
         "esa-completeness",
     ]
-    compute_s2_l1_completeness = KubernetesPodOperator(
-        namespace="processing",
+
+    compute_s2_l1_completeness = utilities.k8s_operator(
+        dag=dag,
         image=ETL_IMAGE,
-        arguments=["bash", "-c", " &&\n".join(COMPUTE_COMPLETENESS_TASK)],
-        name="compute_s2_l1_completeness",
-        is_delete_operator_pod=True,
-        in_cluster=True,
         task_id="compute_s2_l1_completeness",
-        get_logs=True,
+        cmds=COMPUTE_COMPLETENESS_TASK,
         env_vars={
             "COMPLETENESS_CONFIG": json.dumps(L1_CONFIG),
-            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
-            "DB_CREDS": REP_CONN_STR,
-            "ODC_CREDS": ODC_CONN_STR,
             "DATA_INTERVAL_END": "{{  dag_run.data_interval_end | ts  }}",
         },
+        secrets=k8s_secrets.aws_odc_secrets + k8s_secrets.db_secrets(ENV),
     )
-    compute_s2_ard_completeness = KubernetesPodOperator(
-        namespace="processing",
+
+    compute_s2_ard_completeness = utilities.k8s_operator(
+        dag=dag,
         image=ETL_IMAGE,
-        arguments=["bash", "-c", " &&\n".join(COMPUTE_COMPLETENESS_TASK)],
-        name="compute_s2_ard_completeness",
-        is_delete_operator_pod=True,
-        in_cluster=True,
         task_id="compute_s2_ard_completeness",
-        get_logs=True,
+        cmds=COMPUTE_COMPLETENESS_TASK,
         env_vars={
             "COMPLETENESS_CONFIG": json.dumps(ARD_CONFIG),
-            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
-            "DB_CREDS": REP_CONN_STR,
-            "ODC_CREDS": ODC_CONN_STR,
             "DATA_INTERVAL_END": "{{  dag_run.data_interval_end | ts  }}",
         },
+        secrets=k8s_secrets.aws_odc_secrets + k8s_secrets.db_secrets(ENV),
     )
-    compute_s2_ardp_completeness = KubernetesPodOperator(
-        namespace="processing",
+
+    compute_s2_ardp_completeness = utilities.k8s_operator(
+        dag=dag,
         image=ETL_IMAGE,
-        arguments=["bash", "-c", " &&\n".join(COMPUTE_COMPLETENESS_TASK)],
-        name="compute_s2_ardp_completeness",
-        is_delete_operator_pod=True,
-        in_cluster=True,
         task_id="compute_s2_ardp_completeness",
-        get_logs=True,
+        cmds=COMPUTE_COMPLETENESS_TASK,
         env_vars={
             "COMPLETENESS_CONFIG": json.dumps(ARDP_CONFIG),
-            "S3_CREDENTIALS": S3_CREDENTIALS_STR,
-            "DB_CREDS": REP_CONN_STR,
-            "ODC_CREDS": ODC_CONN_STR,
             "DATA_INTERVAL_END": "{{  dag_run.data_interval_end | ts  }}",
         },
+        secrets=k8s_secrets.aws_odc_secrets + k8s_secrets.db_secrets(ENV),
     )
+
     syn_l1_nrt_download = utilities.k8s_operator(
         dag=dag,
         image=ETL_IMAGE,
@@ -227,6 +180,7 @@ with dag:
         },
         secrets=k8s_secrets.sqs_secrets,
     )
+
     syn_l1_nrt_ingestion = utilities.k8s_operator(
         dag=dag,
         image=ETL_IMAGE,
@@ -241,6 +195,7 @@ with dag:
         },
         secrets=k8s_secrets.db_secrets(ENV),
     )
+
     syn_l1_nrt_download >> syn_l1_nrt_ingestion
     (
         scihub_s2_acquisitions
