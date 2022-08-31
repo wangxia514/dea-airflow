29dc8a6de4947a46baf04fed8ba5b813d2222b52
Duncan Gray <duncan.gray@ga.gov.au>
2022-08-31 11:28:12 +1000

    setting interim-days-wait to 18

--- dags/nci_ard.py
+++ dags/nci_ard.py
@@ -23,7 +23,7 @@ params = {
     "wagl_env": "/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env",
     "config_arg": "",
     "scene_limit": "--scene-limit 400",
-    "interim_days_wait": "",
+    "interim_days_wait": "--interim-days-wait 18",
     "products_arg": "",
     "pkgdir_arg": "/g/data/xu18/ga",
     "base_dir": "/g/data/v10/work/c3_ard/",
