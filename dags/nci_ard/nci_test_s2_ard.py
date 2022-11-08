0c4a11dd7c5950207a6975894812c3285081103c
Duncan Gray <duncan.gray@ga.gov.au>
2022-11-09 10:29:57 +1100

    reduce resouce allocation

--- dags/nci_ard/nci_test_s2_ard.py
+++ dags/nci_ard/nci_test_s2_ard.py
@@ -80,7 +80,6 @@ with dag:
         qsub -N ard_scene_select \
               -q  {{ params.queue }}  \
               -W umask=33 \
-              -l wd,walltime=3:00:00,mem=15GB,ncpus=1 -m abe \
               -l storage=gdata/ka08+scratch/ka08+gdata/v10+scratch/v10+gdata/fj7+scratch/fj7+gdata/u46+scratch/u46 \
               -P  {{ params.project }} -o {{ params.base_dir }}{{ log_ext }} -e {{ params.base_dir }}{{ log_ext }}  \
               -- /bin/bash -l -c \
