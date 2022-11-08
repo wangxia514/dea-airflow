81de5aadc6815c5f33f8b1edf7a390bea2203f98
Duncan Gray <duncan.gray@ga.gov.au>
2022-11-08 11:47:08 +1100

    pylint fix

--- dags/nci_ard/nci_s2_metadata.py
+++ dags/nci_ard/nci_s2_metadata.py
@@ -30,7 +30,7 @@ params = {
 
 ssh_conn_id = "lpgs_gadi"
 
-schedule_interval = None #"0 10 * * *"
+schedule_interval = None
 
 # Having the info above as variables and some empty values
 # means I can easily test by adding some test code here
