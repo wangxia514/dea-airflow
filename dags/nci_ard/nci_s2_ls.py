987721fb889aaaa637f14e79f4b23631c8f00f5f
Duncan Gray <duncan.gray@ga.gov.au>
2022-11-09 11:36:19 +1100

    bug fix

--- dags/nci_ard/nci_s2_ls.py
+++ dags/nci_ard/nci_s2_ls.py
@@ -81,6 +81,7 @@ with dag:
                   module use /g/data/v10/private/modules/modulefiles/; \
                   module load {{ params.module_ass }}; \
                   ls {{ params.base_dir }}; \
+                  "
         """,
         timeout=60 * 20,
         do_xcom_push=True,
