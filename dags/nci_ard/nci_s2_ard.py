694e68b63479d3eb11589283fe09d74ce33aab43
Duncan Gray <duncan.gray@ga.gov.au>
2022-11-09 16:13:37 +1100

    pylint

--- dags/nci_ard/nci_s2_ard.py
+++ dags/nci_ard/nci_s2_ard.py
@@ -34,7 +34,7 @@ params = {
 
 ssh_conn_id = "lpgs_gadi"
 
-#schedule_interval = "0 10 * * *"
+# schedule_interval = "0 10 * * *"
 schedule_interval = None
 
 # Having the info above as variables and some empty values
