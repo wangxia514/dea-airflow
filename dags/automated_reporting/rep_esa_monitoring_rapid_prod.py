c298f60226cb79a795140840d1dfa03a570c7d2a
Tom McAdam <tom.mcadam@ga.gov.au>
2022-11-29 12:02:03 +0000

    automated-reporting: esa monitoring etl version bump

--- dags/automated_reporting/rep_esa_monitoring_rapid_prod.py
+++ dags/automated_reporting/rep_esa_monitoring_rapid_prod.py
@@ -15,7 +15,7 @@ from automated_reporting import k8s_secrets, utilities
 
 ENV = "prod"
 ETL_IMAGE = (
-    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/ga-reporting-etls:v2.17.11"
+    "538673716275.dkr.ecr.ap-southeast-2.amazonaws.com/ga-reporting-etls:v2.17.12"
 )
 
 default_args = {
