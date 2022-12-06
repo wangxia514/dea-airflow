66e6cefc2b8f5c639b2d516ccfc6e6a691fc287e
James Miller <49048036+JM-GA@users.noreply.github.com>
2022-12-06 11:24:31 +1100

    update month (#984)

--- dags/collection3/nci_c3_s2_sync.py
+++ dags/collection3/nci_c3_s2_sync.py
@@ -44,7 +44,7 @@ COMMON = dedent(
 default_args = {
     "owner": "James Miller",
     "start_date": datetime(
-        2022, 11, 6, tzinfo=local_tz
+        2022, 12, 6, tzinfo=local_tz
     ),  # earliest date in nci DB is 2016-06-29
     "retries": 0,
     "retry_delay": timedelta(minutes=5),
