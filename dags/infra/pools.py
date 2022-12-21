c764b86b07023b59697a8c38c137da9efbbf6825
James Miller <james.miller@ga.gov.au>
2022-12-21 03:10:25 +0000

    removing decommissioned dags sentinel 2 collection 1

--- dags/infra/pools.py
+++ dags/infra/pools.py
@@ -9,4 +9,3 @@ DEA_NEWDATA_PROCESSING_POOL = Variable.get(
     "newdata_indexing_pool", default_var="NewDeaData_indexing_pool"
 )
 C3_INDEXING_POOL = Variable.get("c3_indexing_pool", default_var="c3_indexing_pool")
-NBART_INDEXING_POOL = Variable.get("nbart_indexing_pool", default_var="nbart_indexing_pool")
