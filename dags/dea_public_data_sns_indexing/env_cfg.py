be7513d4d3c9ef687d69fef051333996cc33a730
James Miller <james.miller@ga.gov.au>
2022-12-21 00:11:02 +0000

    remove decommissioned dags

--- dags/dea_public_data_sns_indexing/env_cfg.py
+++ dags/dea_public_data_sns_indexing/env_cfg.py
@@ -3,17 +3,13 @@
 """
 
 # TODO: The archive condition is for sentinel_2_nrt, set different condition for different products
-ARCHIVE_PRODUCTS = "s2a_nrt_granule s2b_nrt_granule ga_s2am_ard_provisional_3 ga_s2bm_ard_provisional_3 ga_ls8c_ard_provisional_3 ga_ls7e_ard_provisional_3"
+ARCHIVE_PRODUCTS = "ga_ls8c_ard_provisional_3 ga_ls7e_ard_provisional_3"
 ARCHIVE_CONDITION = "[$(date -d '-365 day' +%F), $(date -d '-93 day' +%F)]"
 
 
 # products to be indexed
 # TODO: This list need to be split when multiple SQS queues are setup for different products
 INDEXING_PRODUCTS = [
-    "s2a_nrt_granule",
-    "s2b_nrt_granule",
-    "ga_s2am_ard_provisional_3",
-    "ga_s2bm_ard_provisional_3",
     "ga_ls8c_ard_provisional_3",
     "ga_ls7e_ard_provisional_3",
 ]
@@ -23,9 +19,6 @@ NRT_PRODUCTS = INDEXING_PRODUCTS
 # S3 Record list for indexing products
 # TODO: This list need to be split when multiple SQS queues are setup for different products
 PRODUCT_RECORD_PATHS = (
-    "L2/sentinel-2-nrt/S2MSIARD/*/*/ARD-METADATA.yaml",
-    "baseline/ga_s2am_ard_provisional_3/*/*/*/*/*/*/*.odc-metadata.yaml",
-    "baseline/ga_s2bm_ard_provisional_3/*/*/*/*/*/*/*.odc-metadata.yaml",
     "baseline/ga_ls8c_ard_provisional_3/*/*/*/*/*/*.odc-metadata.yaml",
     "baseline/ga_ls7e_ard_provisional_3/*/*/*/*/*/*.odc-metadata.yaml",
 )
