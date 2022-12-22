c573403466c11f4b7e79f0afc90223fb046e8c0f
Duncan Gray <duncan.gray@ga.gov.au>
2022-12-23 07:21:48 +1100

    drop scene limit for an intial prod run

--- dags/nci_ard/nci_s2_ard.py
+++ dags/nci_ard/nci_s2_ard.py
@@ -38,7 +38,7 @@ params = {
     "base_dir": "/g/data/v10/work/s2_c3_ard/",
     "walltime": "10:00:00",
     "days_to_exclude_arg": """--days-to-exclude '["2015-01-01:2022-08-31"]'""",
-    "scene_limit": "--scene-limit 5000",
+    "scene_limit": "--scene-limit 100",
     "run_ard_arg": "--run-ard",
     "yamldir": " --yamls-dir /g/data/ka08/ga/l1c_metadata",
 }
