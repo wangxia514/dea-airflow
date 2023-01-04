e697ab3c3b3ff1039402bf450c29b16491a53647
James Miller <james.miller@ga.gov.au>
2023-01-04 02:08:49 +0000

    remove decommission products from explorer/ows updates

--- dags/webapp_update/update_list.py
+++ dags/webapp_update/update_list.py
@@ -3,8 +3,6 @@ Lists of default products to update in the OWS and Explorer Web Application
 """
 # Default OWS product layers to be updated
 OWS_UPDATE_LIST = (
-    "s2_nrt_granule_nbar_t",
-    "s2_ard_granule_nbar_t",
     "ga_ls_wo_3",
     "ga_ls_fc_3",
     "ga_ls5t_ard_3",
@@ -12,17 +10,12 @@ OWS_UPDATE_LIST = (
     "ga_ls8c_ard_3",
     "ga_ls_ard_3",
     "ga_ls_ard_provisional_3",
-    "s2_nrt_provisional_granule_nbar_t",
     "ga_s2_ba_provisional_3",
     "ga_s2m_ard_3",
 )
 
 # Default Datacube Explorer Products to Update
 EXPLORER_UPDATE_LIST = (
-    "s2a_nrt_granule",
-    "s2b_nrt_granule",
-    "s2a_ard_granule",
-    "s2b_ard_granule",
     "ga_ls_wo_3",
     "ga_ls_fc_3",
     "ga_ls5t_ard_3",
@@ -31,8 +24,6 @@ EXPLORER_UPDATE_LIST = (
     "ga_ls9c_ard_3",
     "ga_ls7e_ard_provisional_3",
     "ga_ls8c_ard_provisional_3",
-    "ga_s2am_ard_provisional_3",
-    "ga_s2bm_ard_provisional_3",
     "ga_s2_ba_provisional_3",
     "ga_s2am_ard_3",
     "ga_s2bm_ard_3",
